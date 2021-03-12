67cb4019a8ef85b3c8ff7258608745410c0f68f3
Vesa Luusua <vesa.luusua@gmail.com>
2021-03-12 15:11:01 +0200

    Update import paths for EditListingPricingPanel

--- src/containers/EditListingPage/EditListingWizard/EditListingPricingPanel/EditListingPricingPanel.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingPricingPanel/EditListingPricingPanel.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { FormattedMessage } from '../../util/reactIntl';
-import { LISTING_STATE_DRAFT } from '../../util/types';
-import { ListingLink } from '../../components';
-import { EditListingPricingForm } from '../../forms';
-import { ensureOwnListing } from '../../util/data';
-import { types as sdkTypes } from '../../util/sdkLoader';
-import config from '../../config';
 
+// Import configs and util modules
+import config from '../../../../config';
+import { FormattedMessage } from '../../../../util/reactIntl';
+import { LISTING_STATE_DRAFT } from '../../../../util/types';
+import { ensureOwnListing } from '../../../../util/data';
+import { types as sdkTypes } from '../../../../util/sdkLoader';
+
+// Import shared components
+import { ListingLink } from '../../../../components';
+
+// Import modules from this directory
+import EditListingPricingForm from './EditListingPricingForm';
 import css from './EditListingPricingPanel.module.css';
 
 const { Money } = sdkTypes;
