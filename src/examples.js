35ae0686a092a0b105e065b7ee03cd7adf09f4bc
Kimmo Puputti <git@kimmo.kpuputti.fi>
2021-03-18 13:49:34 +0200

    Update paths to moved EmailVerificationForm

--- src/examples.js
+++ src/examples.js
@@ -82,7 +82,6 @@ import * as UserDisplayName from './components/UserDisplayName/UserDisplayName.e
 
 // forms
 import * as BookingDatesForm from './forms/BookingDatesForm/BookingDatesForm.example';
-import * as EmailVerificationForm from './forms/EmailVerificationForm/EmailVerificationForm.example';
 import * as EnquiryForm from './forms/EnquiryForm/EnquiryForm.example';
 import * as PasswordRecoveryForm from './forms/PasswordRecoveryForm/PasswordRecoveryForm.example';
 import * as PasswordResetForm from './forms/PasswordResetForm/PasswordResetForm.example';
@@ -108,6 +107,7 @@ import * as PriceFilter from './containers/SearchPage/PriceFilter/PriceFilter.ex
 import * as FilterForm from './containers/SearchPage/FilterForm/FilterForm.example';
 import * as FilterPlain from './containers/SearchPage/FilterPlain/FilterPlain.example';
 import * as FilterPopup from './containers/SearchPage/FilterPopup/FilterPopup.example';
+import * as EmailVerificationForm from './containers/EmailVerificationPage/EmailVerificationForm/EmailVerificationForm.example';
 
 // containers
 import * as Colors from './containers/StyleguidePage/Colors.example';
