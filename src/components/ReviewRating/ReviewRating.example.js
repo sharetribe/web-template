7df7f2382608ed08133585f45cdc17531ed5d88a
Vesa Luusua <vesa.luusua@gmail.com>
2021-03-23 19:16:13 +0200

    new group: reviews

--- src/components/ReviewRating/ReviewRating.example.js
+++ src/components/ReviewRating/ReviewRating.example.js
@@ -3,9 +3,11 @@ import ReviewRating from './ReviewRating';
 export const ReviewRatingThree = {
   component: ReviewRating,
   props: { rating: 3 },
+  group: 'reviews',
 };
 
 export const ReviewRatingFive = {
   component: ReviewRating,
   props: { rating: 5 },
+  group: 'reviews',
 };
