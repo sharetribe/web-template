ead9fd5cd307654a87362f3c4eec4f7d3491c58e
Vesa Luusua <vesa.luusua@gmail.com>
2021-04-07 20:13:01 +0300

    Web app doesn't use nightsbetween (separate fn used on server)

--- src/util/dates.js
+++ src/util/dates.js
@@ -134,23 +134,6 @@ export const subtractTime = (date, offset, unit, timeZone) => {
 ///////////////
 
 /**
- * Calculate the number of nights between the given dates
- *
- * @param {Date} startDate start of the time period
- * @param {Date} endDate end of the time period
- *
- * @throws Will throw if the end date is before the start date
- * @returns {Number} number of nights between the given dates
- */
-export const nightsBetween = (startDate, endDate) => {
-  const nights = moment(endDate).diff(startDate, 'days');
-  if (nights < 0) {
-    throw new Error('End date cannot be before start date');
-  }
-  return nights;
-};
-
-/**
  * Calculate the number of days between the given dates
  *
  * @param {Date} startDate start of the time period
--- src/util/dates.test.js
+++ src/util/dates.test.js
@@ -2,7 +2,6 @@ import { fakeIntl } from './test-data';
 import {
   isDate,
   isSameDate,
-  nightsBetween,
   daysBetween,
   minutesBetween,
   formatDate,
@@ -34,28 +33,6 @@ describe('date utils', () => {
     });
     it('should be truthy if parameters match', () => {
       expect(isSameDate(new Date(2019, 0, 1), new Date(2019, 0, 1))).toBeTruthy();
-    });
-  });
-
-  describe('nightsBetween()', () => {
-    it('should fail if end date is before start date', () => {
-      const start = new Date(2017, 0, 2);
-      const end = new Date(2017, 0, 1);
-      expect(() => nightsBetween(start, end)).toThrow('End date cannot be before start date');
-    });
-    it('should handle equal start and end dates', () => {
-      const d = new Date(2017, 0, 1);
-      expect(nightsBetween(d, d)).toEqual(0);
-    });
-    it('should calculate night count for a single night', () => {
-      const start = new Date(2017, 0, 1);
-      const end = new Date(2017, 0, 2);
-      expect(nightsBetween(start, end)).toEqual(1);
-    });
-    it('should calculate night count', () => {
-      const start = new Date(2017, 0, 1);
-      const end = new Date(2017, 0, 3);
-      expect(nightsBetween(start, end)).toEqual(2);
     });
   });
 
