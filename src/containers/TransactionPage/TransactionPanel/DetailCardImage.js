f828a73270f84f4fcb5454560b305ce3fd3e15cd
Vesa Luusua <vesa.luusua@gmail.com>
2021-07-27 18:02:00 +0300

    TransactionPage: use custom image variant for listing

--- src/containers/TransactionPage/TransactionPanel/DetailCardImage.js
+++ src/containers/TransactionPage/TransactionPanel/DetailCardImage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import { AvatarMedium, ResponsiveImage } from '../../../components';
+// Import configs and util modules
+import config from '../../../config';
+
+import { AvatarMedium, AspectRatioWrapper, ResponsiveImage } from '../../../components';
 
 import css from './TransactionPanel.module.css';
 
@@ -17,18 +20,21 @@ const DetailCardImage = props => {
     isCustomer,
   } = props;
   const classes = classNames(rootClassName || css.detailCardImageWrapper, className);
+  const { aspectWidth = 1, aspectHeight = 1, variantPrefix = 'listing-card' } = config.listing;
+  const variants = image
+    ? Object.keys(image?.attributes?.variants).filter(k => k.startsWith(variantPrefix))
+    : [];
+
   return (
     <React.Fragment>
-      <div className={classes}>
-        <div className={css.aspectWrapper}>
-          <ResponsiveImage
-            rootClassName={css.rootForImage}
-            alt={listingTitle}
-            image={image}
-            variants={['landscape-crop', 'landscape-crop2x']}
-          />
-        </div>
-      </div>
+      <AspectRatioWrapper width={aspectWidth} height={aspectHeight} className={classes}>
+        <ResponsiveImage
+          rootClassName={css.rootForImage}
+          alt={listingTitle}
+          image={image}
+          variants={variants}
+        />
+      </AspectRatioWrapper>
       {isCustomer ? (
         <div className={avatarWrapperClassName || css.avatarWrapper}>
           <AvatarMedium user={provider} />
