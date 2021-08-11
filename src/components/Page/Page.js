c92d2be3342432423cb953be9d8f39f2feef88a4
Thomas Malbaux <thomas.malbaux@gmail.com>
2021-08-11 17:42:27 +0300

    Update social media images

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -12,8 +12,8 @@ import { canonicalRoutePath } from '../../util/routes';
 
 import { CookieConsent } from '../../components';
 
-import facebookImage from '../../assets/saunatimeFacebook-1200x630.jpg';
-import twitterImage from '../../assets/saunatimeTwitter-600x314.jpg';
+import facebookImage from '../../assets/sneakertimeFacebook-1200x630.jpg';
+import twitterImage from '../../assets/sneakertimeTwitter-600x314.jpg';
 import css from './Page.module.css';
 
 const preventDefault = e => {
