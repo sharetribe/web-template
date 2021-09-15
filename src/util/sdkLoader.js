55bd0ec300d0b6a832ef7d8e10d2a021dfc9c310
Vesa Luusua <vesa.luusua@gmail.com>
2021-09-15 16:47:24 +0300

    Throw understandable error when dimensions of a custom image variant are too big.

--- src/util/sdkLoader.js
+++ src/util/sdkLoader.js
@@ -16,7 +16,12 @@ const { createInstance, types, transit, util } = exportSdk;
 
 // create image variant from variant name, desired width and aspectRatio
 const createImageVariantConfig = (name, width, aspectRatio) => {
-  //const aspectRatio = aspectHeight / aspectWidth;
+  const height = aspectRatio * width;
+  if (width > 3072 || height > 3072) {
+    throw new Error(`Dimensions of custom image variant (${name}) are too high (w:${width}, h:${height}).
+    Reduce them to max 3072px. https://www.sharetribe.com/api-reference/marketplace.html#custom-image-variants`);
+  }
+
   return {
     [`imageVariant.${name}`]: util.objectQueryString({
       w: width,
