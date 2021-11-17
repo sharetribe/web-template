57ab41f5b950eee7314762f8bfdcf6b0d19f0b13
Vesa Luusua <vesa.luusua@gmail.com>
2021-11-17 21:37:04 +0200

    TransactionPanel: update tests

--- src/containers/TransactionPage/TransactionPanel/TransactionPanel.test.js
+++ src/containers/TransactionPage/TransactionPanel/TransactionPanel.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 
 import { types as sdkTypes } from '../../../util/sdkLoader';
 import {
-  createTxTransition,
-  createTransaction,
-  createBooking,
   createListing,
   createUser,
   createCurrentUser,
@@ -13,137 +9,22 @@ import {
 } from '../../../util/test-data';
 import { renderShallow } from '../../../util/test-helpers';
 import { fakeIntl } from '../../../util/test-data';
-import { getProcess } from '../../../util/transaction';
 
-import BreakdownMaybe from './BreakdownMaybe';
 import { TransactionPanelComponent } from './TransactionPanel';
 
 const noop = () => null;
-const transitions = getProcess('flex-product-default-process')?.transitions;
 
-const { Money } = sdkTypes;
+const { UUID } = sdkTypes;
 
 describe('TransactionPanel - Sale', () => {
   const providerId = 'provider';
   const customerId = 'customer';
-  const start = new Date(Date.UTC(2017, 5, 10));
-  const end = new Date(Date.UTC(2017, 5, 13));
-
-  const baseTxAttrs = {
-    total: new Money(16500, 'USD'),
-    commission: new Money(1000, 'USD'),
-    listing: createListing('listing1'),
-    customer: createUser(customerId),
-    provider: createUser(providerId),
-    lastTransitionedAt: new Date(Date.UTC(2017, 5, 10)),
-  };
-
-  const txEnquired = createTransaction({
-    id: 'sale-enquired',
-    lastTransition: transitions.ENQUIRE,
-    ...baseTxAttrs,
-  });
-
-  const txPurchased = createTransaction({
-    id: 'sale-purchased',
-    lastTransition: transitions.CONFIRM_PAYMENT,
-    ...baseTxAttrs,
-  });
-
-  const txDelivered = createTransaction({
-    id: 'sale-delivered',
-    lastTransition: transitions.MARK_DELIVERED,
-    transitions: [
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.REQUEST_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.CONFIRM_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 5, 1)),
-        by: 'provider',
-        transition: transitions.MARK_DELIVERED,
-      }),
-    ],
-    ...baseTxAttrs,
-  });
-
-  const txCanceled = createTransaction({
-    id: 'sale-canceled',
-    lastTransition: transitions.CANCEL,
-    ...baseTxAttrs,
-  });
-
-  const txReceived = createTransaction({
-    id: 'sale-received',
-    lastTransition: transitions.MARK_RECEIVED,
-    transitions: [
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.REQUEST_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.CONFIRM_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 5, 1)),
-        by: 'provider',
-        transition: transitions.MARK_DELIVERED,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 5, 1)),
-        by: 'provider',
-        transition: transitions.MARK_RECEIVED,
-      }),
-    ],
-    ...baseTxAttrs,
-  });
 
   const panelBaseProps = {
-    markReceivedFromPurchasedProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'mark received',
-      errorText: 'mark received failed',
-    },
-    markDeliveredProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'mark delivered',
-      errorText: 'mark delivered failed',
-    },
-    markReceivedProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'mark received',
-      errorText: 'mark received failed',
-    },
-    disputeProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'dispute',
-      errorText: 'dispute failed',
-    },
-    leaveReviewProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'leave review',
-      errorText: 'leave review failed',
-    },
+    transactionId: new UUID('tx-id'),
+    listing: createListing('listing1'),
     currentUser: createCurrentUser(providerId),
+    transactionRole: 'provider',
     totalMessages: 2,
     totalMessagePages: 1,
     oldestMessagePageFetched: 1,
@@ -171,201 +52,22 @@ describe('TransactionPanel - Sale', () => {
   it('enquired matches snapshot', () => {
     const props = {
       ...panelBaseProps,
-      transaction: txEnquired,
     };
     const tree = renderShallow(<TransactionPanelComponent {...props} />);
     expect(tree).toMatchSnapshot();
-  });
-
-  it('purchased matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txPurchased,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('delivered matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txDelivered,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('canceled matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txCanceled,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('received matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txReceived,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-  it('renders correct total price', () => {
-    const start = new Date(Date.UTC(2017, 5, 10));
-    const end = new Date(Date.UTC(2017, 5, 13));
-
-    const transaction = createTransaction({
-      id: 'sale-tx',
-      lastTransition: transitions.REQUEST_PAYMENT,
-      total: new Money(16500, 'USD'),
-      commission: new Money(1000, 'USD'),
-      listing: createListing('listing1'),
-      customer: createUser('customer1'),
-      provider: createUser('provider1'),
-      lastTransitionedAt: new Date(Date.UTC(2017, 5, 10)),
-    });
-    const props = {
-      ...panelBaseProps,
-      transaction,
-    };
-    const panel = shallow(<TransactionPanelComponent {...props} />);
-    const breakdownProps = panel
-      .find(BreakdownMaybe)
-      .first()
-      .props();
-
-    // Total price for the provider should be transaction total minus the commission.
-    expect(breakdownProps.transaction.attributes.payoutTotal).toEqual(new Money(15500, 'USD'));
   });
 });
 
 describe('TransactionPanel - Order', () => {
   const providerId = 'provider';
   const customerId = 'customer';
-  const start = new Date(Date.UTC(2017, 5, 10));
-  const end = new Date(Date.UTC(2017, 5, 13));
-  const baseTxAttrs = {
-    total: new Money(16500, 'USD'),
-    booking: createBooking('booking1', {
-      start,
-      end,
-      displayStart: start,
-      displayEnd: end,
-    }),
-    listing: createListing('listing1'),
-    provider: createUser(providerId),
-    customer: createUser(customerId),
-  };
-
-  const txEnquired = createTransaction({
-    id: 'order-enquired',
-    lastTransition: transitions.ENQUIRE,
-    ...baseTxAttrs,
-  });
-
-  const txPurchased = createTransaction({
-    id: 'order-purchased',
-    lastTransition: transitions.CONFIRM_PAYMENT,
-    ...baseTxAttrs,
-  });
-
-  const txDelivered = createTransaction({
-    id: 'order-delivered',
-    lastTransition: transitions.MARK_DELIVERED,
-    transitions: [
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.REQUEST_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.CONFIRM_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 5, 1)),
-        by: 'provider',
-        transition: transitions.MARK_DELIVERED,
-      }),
-    ],
-    ...baseTxAttrs,
-  });
-
-  const txCanceled = createTransaction({
-    id: 'order-canceled',
-    lastTransition: transitions.CANCEL,
-    ...baseTxAttrs,
-  });
-
-  const txReceived = createTransaction({
-    id: 'order-received',
-    lastTransition: transitions.MARK_RECEIVED,
-    transitions: [
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.REQUEST_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 4, 1)),
-        by: 'customer',
-        transition: transitions.CONFIRM_PAYMENT,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 5, 1)),
-        by: 'provider',
-        transition: transitions.MARK_DELIVERED,
-      }),
-      createTxTransition({
-        createdAt: new Date(Date.UTC(2017, 5, 1)),
-        by: 'provider',
-        transition: transitions.MARK_RECEIVED,
-      }),
-    ],
-    ...baseTxAttrs,
-  });
 
   const panelBaseProps = {
-    markReceivedFromPurchasedProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'mark received',
-      errorText: 'mark received failed',
-    },
-    markDeliveredProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'mark delivered',
-      errorText: 'mark delivered failed',
-    },
-    markReceivedProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'mark received',
-      errorText: 'mark received failed',
-    },
-    disputeProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'dispute',
-      errorText: 'dispute failed',
-    },
-    leaveReviewProps: {
-      inProgress: false,
-      error: null,
-      onAction: noop,
-      buttonText: 'leave review',
-      errorText: 'leave review failed',
-    },
-    intl: fakeIntl,
+    transactionId: new UUID('tx-id'),
+    listing: createListing('listing1'),
     currentUser: createCurrentUser(customerId),
+    transactionRole: 'customer',
+    intl: fakeIntl,
     totalMessages: 2,
     totalMessagePages: 1,
     oldestMessagePageFetched: 1,
@@ -392,75 +94,9 @@ describe('TransactionPanel - Order', () => {
   it('enquired matches snapshot', () => {
     const props = {
       ...panelBaseProps,
-      transaction: txEnquired,
     };
 
     const tree = renderShallow(<TransactionPanelComponent {...props} />);
     expect(tree).toMatchSnapshot();
-  });
-
-  it('purchased matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txPurchased,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('delivered matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txDelivered,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('canceled matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txCanceled,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('received matches snapshot', () => {
-    const props = {
-      ...panelBaseProps,
-      transaction: txReceived,
-    };
-    const tree = renderShallow(<TransactionPanelComponent {...props} />);
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders correct total price', () => {
-    const start = new Date(Date.UTC(2017, 5, 10));
-    const end = new Date(Date.UTC(2017, 5, 13));
-    const tx = createTransaction({
-      id: 'order-tx',
-      lastTransition: transitions.REQUEST_PAYMENT,
-      total: new Money(16500, 'USD'),
-      booking: createBooking('booking1', {
-        start,
-        end,
-        displayStart: start,
-        displayEnd: end,
-      }),
-      listing: createListing('listing1'),
-      provider: createUser(providerId),
-      customer: createUser(customerId),
-    });
-    const props = {
-      ...panelBaseProps,
-      transaction: tx,
-    };
-    const panel = shallow(<TransactionPanelComponent {...props} />);
-    const breakdownProps = panel
-      .find(BreakdownMaybe)
-      .first()
-      .props();
-    expect(breakdownProps.transaction.attributes.payinTotal).toEqual(new Money(16500, 'USD'));
   });
 });
