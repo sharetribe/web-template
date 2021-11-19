2606d4a5b677acc42f238646a6b2e3094afb50fe
Vesa Luusua <vesa.luusua@gmail.com>
2021-11-19 12:37:20 +0200

    InboxPage: support multiple processes

--- src/containers/InboxPage/InboxPage.example.js
+++ src/containers/InboxPage/InboxPage.example.js
@@ -5,7 +5,7 @@ import { createUser, createTransaction } from '../../util/test-data';
 import { getProcess } from '../../util/transaction';
 import { LINE_ITEM_UNITS } from '../../util/types';
 
-import { InboxItem, txState } from './InboxPage';
+import { InboxItem, getStateData } from './InboxPage';
 
 const noop = () => null;
 const transitions = getProcess('flex-product-default-process')?.transitions;
@@ -35,16 +35,25 @@ const tx = lastTransition =>
 
 const TranslatedInboxItem = props => {
   const { intl, tx, type, ...rest } = props;
-  const stateData = txState(intl, tx, type);
+  const transactionRole = isOrders ? TX_TRANSITION_ACTOR_CUSTOMER : TX_TRANSITION_ACTOR_PROVIDER;
+  const stateData = getStateData({ transaction: tx, transactionRole });
 
-  return <InboxItem intl={intl} tx={tx} stateData={stateData} type={type} {...rest} />;
+  return (
+    <InboxItem
+      intl={intl}
+      tx={tx}
+      stateData={stateData}
+      transactionRole={transactionRole}
+      {...rest}
+    />
+  );
 };
 
 export const EnquiryPayment = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.ENQUIRE),
   },
   group: 'page:InboxPage',
@@ -58,7 +67,7 @@ export const PendingPayment_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.REQUEST_PAYMENT),
   },
   group: 'page:InboxPage',
@@ -68,7 +77,7 @@ export const Purchased_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.CONFIRM_PAYMENT),
   },
   group: 'page:InboxPage',
@@ -78,7 +87,7 @@ export const Canceled_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.CANCEL),
   },
   group: 'page:InboxPage',
@@ -88,7 +97,7 @@ export const Delivered_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.MARK_DELIVERED),
   },
   group: 'page:InboxPage',
@@ -98,7 +107,7 @@ export const Disputed_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.DISPUTE),
   },
   group: 'page:InboxPage',
@@ -108,7 +117,7 @@ export const Received_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.MARK_RECEIVED),
   },
   group: 'page:InboxPage',
@@ -118,7 +127,7 @@ export const ReviewedByCustomer_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.REVIEW_1_BY_CUSTOMER),
   },
   group: 'page:InboxPage',
@@ -128,7 +137,7 @@ export const ReviewedByProvider_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.REVIEW_1_BY_PROVIDER),
   },
   group: 'page:InboxPage',
@@ -138,7 +147,7 @@ export const Reviewed_Order = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'order',
+    transactionRole: 'customer',
     tx: tx(transitions.EXPIRE_REVIEW_PERIOD),
   },
   group: 'page:InboxPage',
@@ -152,7 +161,7 @@ export const PendingPayment_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.REQUEST_PAYMENT),
   },
   group: 'page:InboxPage',
@@ -162,7 +171,7 @@ export const Purchased_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.CONFIRM_PAYMENT),
   },
   group: 'page:InboxPage',
@@ -172,7 +181,7 @@ export const Canceled_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.CANCEL),
   },
   group: 'page:InboxPage',
@@ -182,7 +191,7 @@ export const Delivered_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.MARK_DELIVERED),
   },
   group: 'page:InboxPage',
@@ -192,7 +201,7 @@ export const Disputed_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.DISPUTE),
   },
   group: 'page:InboxPage',
@@ -202,7 +211,7 @@ export const Received_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.MARK_RECEIVED),
   },
   group: 'page:InboxPage',
@@ -212,7 +221,7 @@ export const ReviewedByCustomer_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.REVIEW_1_BY_CUSTOMER),
   },
   group: 'page:InboxPage',
@@ -222,7 +231,7 @@ export const ReviewedByProvider_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.REVIEW_1_BY_PROVIDER),
   },
   group: 'page:InboxPage',
@@ -232,7 +241,7 @@ export const Reviewed_Sale = {
   component: injectIntl(TranslatedInboxItem),
   props: {
     unitType: LINE_ITEM_UNITS,
-    type: 'sale',
+    transactionRole: 'provider',
     tx: tx(transitions.EXPIRE_REVIEW_PERIOD),
   },
   group: 'page:InboxPage',
