b2f6cc5bf442bd1d87610c1b89e447aaa48cae76
Vesa Luusua <vesa.luusua@gmail.com>
2021-11-29 15:42:40 +0200

    OrderPanel: rename unitType as lineItemUnitType

--- src/components/OrderPanel/BookingDatesForm/BookingDatesForm.js
+++ src/components/OrderPanel/BookingDatesForm/BookingDatesForm.js
@@ -104,7 +104,7 @@ export class BookingDatesFormComponent extends Component {
             intl,
             isOwnListing,
             submitButtonWrapperClassName,
-            unitType,
+            lineItemUnitType,
             values,
             timeSlots,
             fetchTimeSlotsError,
@@ -202,7 +202,7 @@ export class BookingDatesFormComponent extends Component {
               <FieldDateRangeInput
                 className={css.bookingDates}
                 name="bookingDates"
-                unitType={unitType}
+                unitType={lineItemUnitType}
                 startDateId={`${formId}.bookingStartDate`}
                 startDateLabel={bookingStartLabel}
                 startDatePlaceholderText={startDatePlaceholderText}
@@ -265,7 +265,7 @@ BookingDatesFormComponent.propTypes = {
   className: string,
   submitButtonWrapperClassName: string,
 
-  unitType: propTypes.lineItemUnitType.isRequired,
+  lineItemUnitType: propTypes.lineItemUnitType.isRequired,
   price: propTypes.money,
   isOwnListing: bool,
   timeSlots: arrayOf(propTypes.timeSlot),
