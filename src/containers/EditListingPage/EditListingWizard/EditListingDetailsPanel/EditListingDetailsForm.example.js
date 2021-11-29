d0c69501751ef6bb547004a1eecc488251714b1b
Vesa Luusua <vesa.luusua@gmail.com>
2021-11-29 15:47:15 +0200

    EditListingDetailsForm.example.js: processInfo was missing

--- src/containers/EditListingPage/EditListingWizard/EditListingDetailsPanel/EditListingDetailsForm.example.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingDetailsPanel/EditListingDetailsForm.example.js
@@ -1,5 +1,14 @@
 /* eslint-disable no-console */
 import EditListingDetailsForm from './EditListingDetailsForm';
+import config from '../../../../config';
+
+const processInfos = [
+  {
+    name: 'flex-product-default-process',
+    alias: 'release-1',
+    unitTypes: ['item'],
+  },
+];
 
 export const Empty = {
   component: EditListingDetailsForm,
@@ -12,6 +21,8 @@ export const Empty = {
     updateInProgress: false,
     disabled: false,
     ready: false,
+    processInfos: processInfos,
+    filterConfigs: config.custom.filters,
   },
   group: 'page:EditListingPage',
 };
