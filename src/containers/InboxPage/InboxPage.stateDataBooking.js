26ccf71a2e9b70f6dcf4be7f836e859bef1ab19a
Vesa Luusua <vesa.luusua@gmail.com>
2021-12-07 17:57:44 +0200

    InboxPage: extract getStateData functionality to own helper files

---
+++ src/containers/InboxPage/InboxPage.stateDataBooking.js
@@ -0,0 +1,50 @@
+import {
+  TX_TRANSITION_ACTOR_CUSTOMER as CUSTOMER,
+  TX_TRANSITION_ACTOR_PROVIDER as PROVIDER,
+  CONDITIONAL_RESOLVER_WILDCARD,
+  ConditionalResolver,
+} from '../../util/transaction';
+
+// Get UI data mapped to specific transaction state & role
+export const getStateDataForBookingProcess = (txInfo, processInfo) => {
+  const { transactionRole } = txInfo;
+  const { processName, processState, states } = processInfo;
+  const _ = CONDITIONAL_RESOLVER_WILDCARD;
+
+  return new ConditionalResolver([processState, transactionRole])
+    .cond([states.ENQUIRY, _], () => {
+      return { processName, processState, actionNeeded: true, emphasizeTransitionMoment: true };
+    })
+    .cond([states.PENDING_PAYMENT, CUSTOMER], () => {
+      return { processName, processState, actionNeeded: true };
+    })
+    .cond([states.CANCELED, _], () => {
+      return { processName, processState, isFinal: true };
+    })
+    .cond([states.PREAUTHORIZED, PROVIDER], () => {
+      return { processName, processState, actionNeeded: true };
+    })
+    .cond([states.ACCEPTED, _], () => {
+      return { processName, processState, actionNeeded: true, emphasizeTransitionMoment: true };
+    })
+    .cond([states.DECLINED, _], () => {
+      return { processName, processState, isFinal: true };
+    })
+    .cond([states.DELIVERED, _], () => {
+      return { processName, processState, actionNeeded: true };
+    })
+    .cond([states.REVIEWED_BY_PROVIDER, CUSTOMER], () => {
+      return { processName, processState, actionNeeded: true };
+    })
+    .cond([states.REVIEWED_BY_CUSTOMER, PROVIDER], () => {
+      return { processName, processState, actionNeeded: true };
+    })
+    .cond([states.REVIEWED, _], () => {
+      return { processName, processState, isFinal: true };
+    })
+    .default(() => {
+      // Default values for other states
+      return { processName, processState };
+    })
+    .resolve();
+};
