49c781db092744a5a966ed2f798425707586ae5d
Vesa Luusua <vesa.luusua@gmail.com>
2021-12-07 17:18:18 +0200

    TransactionPage: extract getStateData functionality to own helper files

---
+++ src/containers/TransactionPage/TransactionPage.stateData.js
@@ -0,0 +1,139 @@
+import { bool, func, oneOf, shape, string } from 'prop-types';
+import { getStateDataForBookingProcess } from './TransactionPage.stateDataBooking.js';
+import { getStateDataForProductProcess } from './TransactionPage.stateDataProduct.js';
+
+const FLEX_PRODUCT_DEFAULT_PROCESS = 'flex-product-default-process';
+const FLEX_DAILY_DEFAULT_PROCESS = 'flex-default-process';
+
+const errorShape = shape({
+  type: oneOf(['error']).isRequired,
+  name: string.isRequired,
+  message: string,
+});
+
+const actionButtonsShape = shape({
+  inProgress: bool,
+  error: errorShape,
+  onAction: func.isRequired,
+  buttonText: string,
+  errorText: string,
+});
+
+export const stateDataShape = shape({
+  processName: string.isRequired,
+  processState: string.isRequired,
+  primaryButtonProps: actionButtonsShape,
+  secondaryButtonProps: actionButtonsShape,
+  showActionButtons: bool,
+  showDetailCardHeadings: bool,
+  showDispute: bool,
+  showOrderPanel: bool,
+  showReviewAsFirstLink: bool,
+  showReviewAsSecondLink: bool,
+  showReviews: bool,
+});
+
+// Transitions are following process.edn format: "transition/my-transtion-name"
+// This extracts the 'my-transtion-name' string if namespace exists
+const getTransitionKey = transitionName => {
+  const [nameSpace, transitionKey] = transitionName.split('/');
+  return transitionKey || transitionName;
+};
+
+// Action button prop for the TransactionPanel
+const getActionButtonPropsMaybe = (params, onlyForRole = 'both') => {
+  const {
+    processName,
+    transitionName,
+    inProgress,
+    transitionError,
+    onAction,
+    transactionRole,
+    actionButtonTranslationId,
+    actionButtonTranslationErrorId,
+    intl,
+  } = params;
+  const transitionKey = getTransitionKey(transitionName);
+
+  const actionButtonTrId =
+    actionButtonTranslationId ||
+    `TransactionPage.${processName}.${transactionRole}.transition-${transitionKey}.actionButton`;
+  const actionButtonTrErrorId =
+    actionButtonTranslationErrorId ||
+    `TransactionPage.${processName}.${transactionRole}.transition-${transitionKey}.actionError`;
+
+  return onlyForRole === 'both' || onlyForRole === transactionRole
+    ? {
+        inProgress,
+        error: transitionError,
+        onAction,
+        buttonText: intl.formatMessage({ id: actionButtonTrId }),
+        errorText: intl.formatMessage({ id: actionButtonTrErrorId }),
+      }
+    : {};
+};
+
+export const getStateData = (params, process) => {
+  const {
+    transaction,
+    transactionRole,
+    intl,
+    transitionInProgress,
+    transitionError,
+    onTransition,
+    sendReviewInProgress,
+    sendReviewError,
+    onOpenReviewModal,
+  } = params;
+  const isCustomer = transactionRole === 'customer';
+  const processName = transaction?.attributes?.processName;
+
+  const getActionButtonProps = (transitionName, forRole, extra = {}) =>
+    getActionButtonPropsMaybe(
+      {
+        processName,
+        transitionName,
+        transactionRole,
+        intl,
+        inProgress: transitionInProgress === transitionName,
+        transitionError,
+        onAction: () => onTransition(transaction?.id, transitionName, {}),
+        ...extra,
+      },
+      forRole
+    );
+
+  const getLeaveReviewProps = getActionButtonPropsMaybe({
+    processName,
+    transitionName: 'leaveReview',
+    transactionRole,
+    intl,
+    inProgress: sendReviewInProgress,
+    transitionError: sendReviewError,
+    onAction: onOpenReviewModal,
+    actionButtonTranslationId: 'TransactionPage.leaveReview.actionButton',
+    actionButtonTranslationErrorId: 'TransactionPage.leaveReview.actionError',
+  });
+
+  const processInfo = () => {
+    const { getState, states, transitions } = process;
+    const processState = getState(transaction);
+    return {
+      processName,
+      processState,
+      states,
+      transitions,
+      isCustomer,
+      actionButtonProps: getActionButtonProps,
+      leaveReviewProps: getLeaveReviewProps,
+    };
+  };
+
+  if (processName === FLEX_PRODUCT_DEFAULT_PROCESS) {
+    return getStateDataForProductProcess(params, processInfo());
+  } else if (processName === FLEX_DAILY_DEFAULT_PROCESS) {
+    return getStateDataForBookingProcess(params, processInfo());
+  } else {
+    return {};
+  }
+};
---
+++ src/containers/TransactionPage/TransactionPage.stateDataBooking.js
@@ -0,0 +1,89 @@
+import {
+  TX_TRANSITION_ACTOR_CUSTOMER as CUSTOMER,
+  TX_TRANSITION_ACTOR_PROVIDER as PROVIDER,
+  CONDITIONAL_RESOLVER_WILDCARD,
+  ConditionalResolver,
+} from '../../util/transaction';
+
+/**
+ * Get state data against booking process for TransactionPage's UI.
+ * I.e. info about showing action buttons, current state etc.
+ *
+ * @param {*} txInfo detials about transaction
+ * @param {*} processInfo  details about process
+ */
+export const getStateDataForBookingProcess = (txInfo, processInfo) => {
+  const { transaction, transactionRole, nextTransitions } = txInfo;
+  const isProviderBanned = transaction?.provider?.attributes?.banned;
+  const isCustomerBanned = transaction?.provider?.attributes?.banned;
+  const _ = CONDITIONAL_RESOLVER_WILDCARD;
+
+  const {
+    processName,
+    processState,
+    states,
+    transitions,
+    isCustomer,
+    actionButtonProps,
+    leaveReviewProps,
+  } = processInfo;
+
+  return new ConditionalResolver([processState, transactionRole])
+    .cond([states.ENQUIRY, CUSTOMER], () => {
+      const transitionNames = Array.isArray(nextTransitions)
+        ? nextTransitions.map(t => t.attributes.name)
+        : [];
+      const requestAfterEnquiry = transitions.REQUEST_PAYMENT_AFTER_ENQUIRY;
+      const hasCorrectNextTransition = transitionNames.includes(requestAfterEnquiry);
+      const showOrderPanel = !isProviderBanned && hasCorrectNextTransition;
+      return { processName, processState, showOrderPanel };
+    })
+    .cond([states.PREAUTHORIZED, PROVIDER], () => {
+      const primary = isCustomerBanned ? null : actionButtonProps(transitions.ACCEPT, PROVIDER);
+      const secondary = isCustomerBanned ? null : actionButtonProps(transitions.DECLINE, PROVIDER);
+      return {
+        processName,
+        processState,
+        showActionButtons: true,
+        primaryButtonProps: primary,
+        secondaryButtonProps: secondary,
+      };
+    })
+    .cond([states.DELIVERED, _], () => {
+      return {
+        processName,
+        processState,
+        showDetailCardHeadings: isCustomer,
+        showReviewAsFirstLink: true,
+        showActionButtons: true,
+        primaryButtonProps: leaveReviewProps,
+      };
+    })
+    .cond([states.REVIEWED_BY_PROVIDER, CUSTOMER], () => {
+      return {
+        processName,
+        processState,
+        showDetailCardHeadings: true,
+        showReviewAsSecondLink: true,
+        showActionButtons: true,
+        primaryButtonProps: leaveReviewProps,
+      };
+    })
+    .cond([states.REVIEWED_BY_CUSTOMER, PROVIDER], () => {
+      return {
+        processName,
+        processState,
+        showReviewAsSecondLink: true,
+        showActionButtons: true,
+        primaryButtonProps: leaveReviewProps,
+      };
+    })
+    .cond([states.REVIEWED, _], () => {
+      return { processName, processState, showDetailCardHeadings: isCustomer, showReviews: true };
+    })
+    .default(() => {
+      // Default values for other states
+      return { processName, processState, showDetailCardHeadings: isCustomer };
+    })
+    .resolve();
+};
