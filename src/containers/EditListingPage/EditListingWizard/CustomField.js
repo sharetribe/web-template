1bac8460e32b0b2c21c1653ba0abbfdd3b20d83d
Vesa Luusua <vesa.luusua@gmail.com>
2021-12-09 19:47:31 +0200

    EditListingWizard/CustomField: add more custom field for supported schema types

--- src/containers/EditListingPage/EditListingWizard/CustomField.js
+++ src/containers/EditListingPage/EditListingWizard/CustomField.js
@@ -3,58 +3,141 @@ import React from 'react';
 // Import config and utils
 import { required } from '../../../util/validators';
 // Import shared components
-import { FieldCheckboxGroup, FieldSelect } from '../../../components';
+import { FieldCheckboxGroup, FieldSelect, FieldTextInput, FieldBoolean } from '../../../components';
 // Import modules from this directory
 import css from './EditListingWizard.module.css';
 
 const CustomFieldEnum = props => {
-  const { id, filterConfig } = props;
-  const { config, label, wizardPlaceholder, wizardRequired } = filterConfig || {};
-  const { options = [], schemaType } = config;
+  const { name, fieldConfig } = props;
+  const { schemaOptions = [], editListingPageConfig } = fieldConfig || {};
+  const { label, placeholder, requiredMessage } = editListingPageConfig;
+  const validateMaybe = requiredMessage ? { validate: required(requiredMessage) } : {};
 
-  return options && schemaType === 'enum' ? (
-    <FieldSelect
-      className={css.detailsSelect}
-      name={id}
-      id={id}
-      label={label}
-      validate={required(wizardRequired)}
-    >
+  return schemaOptions ? (
+    <FieldSelect className={css.customField} name={name} id={name} label={label} {...validateMaybe}>
       <option disabled value="">
-        {wizardPlaceholder}
+        {placeholder}
       </option>
-      {options.map(c => (
-        <option key={c.key} value={c.key}>
-          {c.label}
-        </option>
-      ))}
+      {schemaOptions.map(option => {
+        // Key is used in URL on SearchPage, when making listing queries.
+        // We turn it to more readable form by avoiding encoded space characters
+        // I.e. "My Option" => "my_option"
+        const key = `${option}`.toLowerCase().replaceAll(' ', '_');
+        return (
+          <option key={key} value={key}>
+            {option}
+          </option>
+        );
+      })}
     </FieldSelect>
   ) : null;
 };
 
 const CustomFieldMultiEnum = props => {
-  const { id, filterConfig } = props;
-  const { config, label } = filterConfig || {};
-  const { options = [], schemaType } = config;
+  const { name, fieldConfig } = props;
+  const { schemaOptions = [], editListingPageConfig } = fieldConfig || {};
+  const options = schemaOptions.map(option => {
+    // Key is used in URL on SearchPage, when making listing queries.
+    // We turn it to more readable form by avoiding encoded space characters
+    // I.e. "My Option" => "my_option"
+    const key = option.toLowerCase().replaceAll(' ', '_');
+    return { key, label: option };
+  });
 
-  return options && schemaType === 'multi-enum' ? (
+  return options ? (
     <FieldCheckboxGroup
-      className={css.multiEnum}
-      id={id}
-      name={id}
-      label={label}
+      className={css.customField}
+      id={name}
+      name={name}
+      label={editListingPageConfig?.label}
       options={options}
     />
   ) : null;
 };
 
-const CustomField = props => {
-  const { options = [], schemaType } = props?.filterConfig?.config || {};
-  return options && schemaType === 'enum' ? (
-    <CustomFieldEnum {...props} />
-  ) : options && schemaType === 'multi-enum' ? (
-    <CustomFieldMultiEnum {...props} />
-  ) : null;
+const CustomFieldText = props => {
+  const { name, fieldConfig } = props;
+  const { label, placeholder, requiredMessage } = fieldConfig?.editListingPageConfig || {};
+  const validateMaybe = requiredMessage ? { validate: required(requiredMessage) } : {};
+
+  return (
+    <FieldTextInput
+      className={css.customField}
+      id={name}
+      name={name}
+      type="textarea"
+      label={label}
+      placeholder={placeholder}
+      {...validateMaybe}
+    />
+  );
 };
 
-export default CustomField;
+const CustomFieldLong = props => {
+  const { name, fieldConfig } = props;
+  const { label, placeholder, requiredMessage } = fieldConfig?.editListingPageConfig || {};
+  const validateMaybe = requiredMessage ? { validate: required(requiredMessage) } : {};
+
+  return (
+    <FieldTextInput
+      className={css.customField}
+      id={name}
+      name={name}
+      type="number"
+      step="1"
+      parse={value => {
+        const parsed = Number.parseInt(value, 10);
+        return Number.isNaN(parsed) ? null : parsed;
+      }}
+      label={label}
+      placeholder={placeholder}
+      {...validateMaybe}
+    />
+  );
+};
+
+const CustomFieldBoolean = props => {
+  const { name, fieldConfig } = props;
+  const { label, placeholder, requiredMessage } = fieldConfig?.editListingPageConfig || {};
+  const validateMaybe = requiredMessage ? { validate: required(requiredMessage) } : {};
+
+  return (
+    <FieldBoolean
+      className={css.customField}
+      id={name}
+      name={name}
+      label={label}
+      placeholder={placeholder}
+      {...validateMaybe}
+    />
+  );
+};
+
+/**
+ * Return Final Form field for each configuration according to schema type.
+ *
+ * These custom extended data fields are for generating input fields from configuration defined
+ * in marketplace-custom-config.js. Other panels in EditListingWizard might add more extended data
+ * fields (e.g. shipping fee), but these are independently customizable.
+ *
+ * @param {Object} props should contain fieldConfig that defines schemaType, schemaOptions?, and
+ * editListingPageConfig for the field.
+ */
+const CustomExtendedDataField = props => {
+  const { schemaOptions = [], schemaType } = props?.fieldConfig || {};
+  const renderFieldComponent = (FieldComponent, props) => <FieldComponent {...props} />;
+
+  return schemaType === 'enum' && schemaOptions
+    ? renderFieldComponent(CustomFieldEnum, props)
+    : schemaType === 'multi-enum' && schemaOptions
+    ? renderFieldComponent(CustomFieldMultiEnum, props)
+    : schemaType === 'text'
+    ? renderFieldComponent(CustomFieldText, props)
+    : schemaType === 'long'
+    ? renderFieldComponent(CustomFieldLong, props)
+    : schemaType === 'boolean'
+    ? renderFieldComponent(CustomFieldBoolean, props)
+    : null;
+};
+
+export default CustomExtendedDataField;
