ff1badca8cbfa8514ed7c719c0e90867836e6f7c
Vesa Luusua <vesa.luusua@gmail.com>
2021-12-09 19:50:07 +0200

    EditListingDetailsForm: handle custom fields based on listingExtendedData config and process

--- src/containers/EditListingPage/EditListingWizard/EditListingDetailsPanel/EditListingDetailsForm.example.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingDetailsPanel/EditListingDetailsForm.example.js
@@ -2,7 +2,7 @@
 import EditListingDetailsForm from './EditListingDetailsForm';
 import config from '../../../../config';
 
-const processInfos = [
+const oneProcessInfo = [
   {
     name: 'flex-product-default-process',
     alias: 'release-1',
@@ -10,19 +10,55 @@ const processInfos = [
   },
 ];
 
-export const Empty = {
+export const WithInitialValues = {
   component: EditListingDetailsForm,
   props: {
     onSubmit: values => {
       console.log('Submit EditListingDetailsForm with (unformatted) values:', values);
     },
     saveActionMsg: 'Save description',
-    updated: false,
-    updateInProgress: false,
     disabled: false,
     ready: false,
+    updated: false,
+    updateInProgress: false,
+    processInfos: oneProcessInfo,
+    customConfig: config.custom,
+    initialValues: {
+      title: 'Listing',
+      description: 'Lorem ipsum',
+      transactionProcessAlias: 'flex-product-default-process/release-1',
+      unitType: 'item',
+    },
+  },
+  group: 'page:EditListingPage',
+};
+
+const processInfos = [
+  {
+    name: 'flex-product-default-process',
+    alias: 'release-1',
+    unitTypes: ['item'],
+  },
+  {
+    name: 'flex-default-process',
+    alias: 'release-1',
+    unitTypes: ['day', 'night'],
+  },
+];
+
+export const MultipleProcessInfos = {
+  component: EditListingDetailsForm,
+  props: {
+    onSubmit: values => {
+      console.log('Submit EditListingDetailsForm with (unformatted) values:', values);
+    },
+    saveActionMsg: 'Save description',
+    disabled: false,
+    ready: false,
+    updated: false,
+    updateInProgress: false,
     processInfos: processInfos,
-    filterConfigs: config.custom.filters,
+    customConfig: config.custom,
   },
   group: 'page:EditListingPage',
 };
