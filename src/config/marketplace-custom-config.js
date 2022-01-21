a23f7580d6052b593ae66b3585b3dd2ce9e6d903
Vesa Luusua <vesa.luusua@gmail.com>
2022-01-21 12:42:52 +0200

    marketplace-custom-config.js: update listingExtendedData and remove filters and listing confs

--- src/config/marketplace-custom-config.js
+++ src/config/marketplace-custom-config.js
@@ -58,6 +58,7 @@ export const listingExtendedData = [
     },
     listingPageConfig: {
       label: 'Category',
+      isDetail: true,
     },
     editListingPageConfig: {
       label: 'Select category',
@@ -79,6 +80,7 @@ export const listingExtendedData = [
     },
     listingPageConfig: {
       label: 'Size (US)',
+      isDetail: true,
     },
     editListingPageConfig: {
       label: 'Select size (US)',
@@ -110,6 +112,7 @@ export const listingExtendedData = [
     },
     listingPageConfig: {
       label: 'Brand',
+      isDetail: true,
     },
     editListingPageConfig: {
       label: 'Select brand',
@@ -195,6 +198,7 @@ export const listingExtendedData = [
     },
     listingPageConfig: {
       label: 'Gears',
+      isDetail: true,
     },
     editListingPageConfig: {
       label: 'Gears',
@@ -214,6 +218,7 @@ export const listingExtendedData = [
     },
     listingPageConfig: {
       label: 'Has lights',
+      isDetail: true,
     },
     editListingPageConfig: {
       label: 'Has lights',
@@ -243,192 +248,6 @@ export const defaultFilters = [
     schemaType: 'text',
     label: 'Keyword',
   },
-];
-
-/**
- * Every filter needs to have following keys:
- * - id:     Unique id of the filter.
- * - label:  The default label of the filter.
- * - type:   String that represents one of the existing filter components:
- *           BookingDateRangeFilter, KeywordFilter, PriceFilter,
- *           SelectSingleFilter, SelectMultipleFilter.
- * - group:  Is this 'primary' or 'secondary' filter?
- *           Primary filters are visible on desktop layout by default.
- *           Secondary filters are behind "More filters" button.
- *           Read more from src/containers/SearchPage/README.md
- * - queryParamNames: Describes parameters to be used with queries
- *                    (e.g. 'price' or 'pub_amenities'). Most of these are
- *                    the same between webapp URLs and API query params.
- *                    You can't change 'dates', 'price', or 'keywords'
- *                    since those filters are fixed to a specific attribute.
- * - config: Extra configuration that the filter component needs.
- *
- * Note 1: Labels could be tied to translation file
- *         by importing FormattedMessage:
- *         <FormattedMessage id="some.translation.key.here" />
- *
- * Note 2: If you need to add new custom filter components,
- *         you need to take those into use in:
- *         src/containers/SearchPage/FilterComponent.js
- *
- * Note 3: If you just want to create more enum filters
- *         (i.e. SelectSingleFilter, SelectMultipleFilter),
- *         you can just add more configurations with those filter types
- *         and tie them with correct extended data key
- *         (i.e. pub_<key> or meta_<key>).
- */
-// TODO This filters setup should be transformed to listing's extended data setup
-export const filters = [
-  {
-    id: 'category',
-    label: 'Category',
-    wizardPlaceholder: 'Choose…',
-    wizardRequired: 'You need to select a category.',
-    type: 'SelectSingleFilter',
-    group: 'primary',
-    queryParamNames: ['pub_category'],
-    config: {
-      // Schema type is enum for SelectSingleFilter
-      schemaType: 'enum',
-
-      // "key" is the option you see in Flex Console.
-      // "label" is set here for the UI only.
-      // Note: label is not added through the translation files
-      // to make filter customizations a bit easier.
-      options: [
-        { key: 'men', label: 'Men' },
-        { key: 'women', label: 'Women' },
-        { key: 'kids', label: 'Kids' },
-      ],
-    },
-  },
-  {
-    id: 'size',
-    label: 'Size (US)',
-    wizardPlaceholder: 'Choose…',
-    wizardRequired: 'You need to select a size.',
-    type: 'SelectMultipleFilter',
-    group: 'primary',
-    queryParamNames: ['pub_size'],
-    config: {
-      // Schema type options: 'enum', 'multi-enum'
-      // Both types can work so that user selects multiple values when filtering search results.
-      // With "enum" the functionality will be OR-semantics (Nike OR Adidas OR Salomon)
-      // With "multi-enum" it's possible to use both AND and OR semantics with searchMode config.
-      schemaType: 'enum',
-
-      // "key" is the option you see in Flex Console.
-      // "label" is set here for the UI only.
-      // Note: label is not added through the translation files
-      // to make filter customizations a bit easier.
-      options: [
-        { key: '4', label: '4' },
-        { key: '5', label: '5' },
-        { key: '6', label: '6' },
-        { key: '7', label: '7' },
-        { key: '8', label: '8' },
-        { key: '9', label: '9' },
-        { key: '10', label: '10' },
-        { key: '11', label: '11' },
-        { key: '12', label: '12' },
-      ],
-    },
-  },
-  {
-    id: 'brand',
-    label: 'Brand',
-    wizardPlaceholder: 'Choose…',
-    wizardRequired: 'You need to select a brand.',
-    type: 'SelectMultipleFilter',
-    group: 'primary',
-    queryParamNames: ['pub_brand'],
-    config: {
-      // Schema type options: 'enum', 'multi-enum'
-      // Both types can work so that user selects multiple values when filtering search results.
-      // With "enum" the functionality will be OR-semantics (Nike OR Adidas OR Salomon)
-      // With "multi-enum" it's possible to use both AND and OR semantics with searchMode config.
-      schemaType: 'enum',
-
-      // "key" is the option you see in Flex Console.
-      // "label" is set here for the UI only.
-      // Note: label is not added through the translation files
-      // to make filter customizations a bit easier.
-      options: [
-        { key: 'adidas', label: 'Adidas' },
-        { key: 'air_jordan', label: 'Air Jordan' },
-        { key: 'converse', label: 'Converse' },
-        { key: 'new_balance', label: 'New Balance' },
-        { key: 'nike', label: 'Nike' },
-        { key: 'puma', label: 'Puma' },
-        { key: 'ultraboost', label: 'Ultraboost' },
-        { key: 'vans', label: 'Vans' },
-        { key: 'yeezy', label: 'Yeezy' },
-        { key: 'other', label: 'Other' },
-      ],
-    },
-  },
-  {
-    id: 'price',
-    label: 'Price',
-    type: 'PriceFilter',
-    group: 'primary',
-    // Note: PriceFilter is fixed filter,
-    // you can't change "queryParamNames: ['price'],"
-    queryParamNames: ['price'],
-    // Price filter configuration
-    // Note: unlike most prices this is not handled in subunits
-    config: {
-      min: 0,
-      max: 1000,
-      step: 5,
-    },
-  },
-  {
-    id: 'keyword',
-    label: 'Keyword',
-    type: 'KeywordFilter',
-    group: 'primary',
-    // Note: KeywordFilter is fixed filter,
-    // you can't change "queryParamNames: ['keywords'],"
-    queryParamNames: ['keywords'],
-    // NOTE: If you are ordering search results by distance
-    // the keyword search can't be used at the same time.
-    // You can turn on/off ordering by distance from config.js file.
-    config: {},
-  },
-
-  // Here is an example of multi-enum search filter.
-  //
-  // {
-  //   id: 'amenities',
-  //   label: 'Amenities',
-  //   type: 'SelectMultipleFilter',
-  //   group: 'secondary',
-  //   queryParamNames: ['pub_amenities'],
-  //   config: {
-  //     // Schema type options: 'enum', 'multi-enum'
-  //     // Both types can work so that user selects multiple values when filtering search results.
-  //     // With "enum" the functionality will be OR-semantics (Nike OR Adidas OR Salomon)
-  //     // With "multi-enum" it's possible to use both AND and OR semantics with searchMode config.
-  //     schemaType: 'multi-enum',
-
-  //     // Optional modes: 'has_all', 'has_any'
-  //     // Note: this is relevant only for schema type 'multi-enum'
-  //     // https://www.sharetribe.com/api-reference/marketplace.html#extended-data-filtering
-  //     searchMode: 'has_all',
-
-  //     // "key" is the option you see in Flex Console.
-  //     // "label" is set here for this web app's UI only.
-  //     // Note: label is not added through the translation files
-  //     // to make filter customizations a bit easier.
-  //     options: [
-  //       { key: 'towels', label: 'Towels' },
-  //       { key: 'bathroom', label: 'Bathroom' },
-  //       { key: 'swimming_pool', label: 'Swimming pool' },
-  //       { key: 'barbeque', label: 'Barbeque' },
-  //     ],
-  //   },
-  // },
 ];
 
 export const sortConfig = {
@@ -462,10 +281,4 @@ export const sortConfig = {
     // for handling the internal state of the sorting dropdown.
     { key: 'relevance', label: 'Relevance', longLabel: 'Relevance (Keyword search)' },
   ],
-};
-
-export const listing = {
-  // These should be listing details from public data with schema type: enum
-  // SectionDetailsMaybe component shows these on listing page.
-  enumFieldDetails: ['size', 'brand', 'category'],
 };
