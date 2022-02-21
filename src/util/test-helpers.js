64da6d1f5729cd7fff4e1d5b5998ce78b77baf9a
Jenni Laakso <jenni@sharetribe.com>
2022-02-21 10:48:46 +0200

    Try react-testing-library on AuthenticationPage

--- src/util/test-helpers.js
+++ src/util/test-helpers.js
@@ -3,8 +3,13 @@ import mapValues from 'lodash/mapValues';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
-import { BrowserRouter } from 'react-router-dom';
+
+import * as reactTestingLibrary from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 
 import configureStore from '../store';
 import { IntlProvider } from '../util/reactIntl';
@@ -22,6 +27,8 @@ Enzyme.configure({ adapter: new Adapter() });
 
 let undefined;
 export const getDefaultConfiguration = () => {
+  // TODO: add more relevant data for tests
+  // TODO: make it possible to overwrite configuration for tests
   return {
     ...defaultConfig,
     currency: 'USD',
@@ -188,20 +195,43 @@ const testMessages = mapValues(messages, (val, key) => key);
 
 // Provide all the context for components that connect to the Redux
 // store, i18n, router, etc.
-export const TestProvider = props => {
+export const TestProvider = ({ children }) => {
   const store = configureStore();
   return (
     <ConfigurationProvider value={getDefaultConfiguration()}>
       <RouteConfigurationProvider value={getRouteConfiguration()}>
         <IntlProvider locale="en" messages={testMessages} textComponent="span">
-          <BrowserRouter>
-            <Provider store={store}>{props.children}</Provider>
-          </BrowserRouter>
+          <Provider store={store}>
+            <HelmetProvider>
+              <MemoryRouter>{children}</MemoryRouter>
+            </HelmetProvider>
+          </Provider>
         </IntlProvider>
       </RouteConfigurationProvider>
     </ConfigurationProvider>
   );
 };
+
+// With react-testing-library we don't have option to shallow render
+// so we would need a custom render function to add required wrappers
+// in order to test the whole Page component.
+// Example for creating custom render function
+// for using React Intl with RTL:
+// https://testing-library.com/docs/example-react-intl#creating-a-custom-render-function
+// function render(ui, { preloadedState, store = configureStore(), ...renderOptions } = {}) {
+//   function Wrapper({ children }) {
+//     return (<TestProvider>{children}</TestProvider>);
+//   }
+//   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+// }
+
+export const renderWithProviders = (ui, renderOptions = {}) => {
+  const Wrapper = ({ children }) => {
+    return <TestProvider>{children}</TestProvider>;
+  };
+  return reactTestingLibrary.render(ui, { wrapper: Wrapper, ...renderOptions });
+};
+export const testingLibrary = { ...reactTestingLibrary, userEvent };
 
 // Use Enzyme's shallow rendering to render the given component to a
 // JSON structure that can be used in snapshot tests. This doesn't
