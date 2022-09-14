d179587d094aeb2c290ceb97010ab45ee97906fb
Vesa Luusua <vesa.luusua@gmail.com>
2022-09-14 19:41:14 +0300

    Remove deprecated config: listingManagementType

--- src/util/search.js
+++ src/util/search.js
@@ -90,4 +90,11 @@ export const isOriginInUse = config =>
 /**
  * Check if the stock management is currently active.
  */
-export const isStockInUse = config => config.search?.listingManagementType === 'stock';
+export const isStockInUse = config => {
+  const transactionTypes = config.transaction.transactionTypes;
+  const hasItems = !transactionTypes.find(conf => conf.unitType === 'item');
+
+  // TODO: if there are multiple processes with both products and bookings,
+  // sdk.listings.query needs more thinking on SearchPage. (bookings have stock=0)
+  return hasItems && transactionTypes.length === 1;
+};
