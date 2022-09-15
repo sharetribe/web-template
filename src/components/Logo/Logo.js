b88f0f1f69c11e22be766823b984dd67a83582a9
Vesa Luusua <vesa.luusua@gmail.com>
2022-09-15 19:13:33 +0300

    Create brandingConfig.js and layoutConfig.js Note: marketplaceColor is handled in brandingConfig.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { useConfiguration } from '../../context/configurationContext';
-import LogoImageMobile from './sneakertime-logo-mobile.png';
-import LogoImage from './sneakertime-logo.png';
 
 const Logo = props => {
   const config = useConfiguration();
   const { className, format, ...rest } = props;
+  // NOTE: logo images are set in src/config/brandingConfig.js
+  const { logoImageDesktopURL, logoImageMobileURL } = config.branding;
 
   if (format === 'desktop') {
-    return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
+    return <img className={className} src={logoImageDesktopURL} alt={config.siteTitle} {...rest} />;
   }
 
-  return <img className={className} src={LogoImageMobile} alt={config.siteTitle} {...rest} />;
+  return <img className={className} src={logoImageMobileURL} alt={config.siteTitle} {...rest} />;
 };
 
 const { oneOf, string } = PropTypes;
---
+++ src/config/layoutConfig.js
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////
+// This file contains configs that affect layout     //
+// NOTE: these are just some of the relevant configs //
+// Most of the work happens in components.           //
+///////////////////////////////////////////////////////
+
+// There are 2 SearchPage variants that can be used:
+// 'map' & 'list'
+export const searchPageVariant = 'list';
+
+// ListingPage has 2 layout options: 'hero-image' and 'full-image'.
+// - 'hero-image' means a layout where there's a hero section with cropped image in the beginning of the page
+// - 'full-image' shows image carousel, where listing images are shown with the original aspect ratio
+export const listingPageVariant = 'full-image';
+
+export const listingImage = {
+  // Aspect ratio for listing image variants
+  aspectWidth: 400,
+  aspectHeight: 400,
+  // Listings have custom image variants, which are named here.
+  variantPrefix: 'listing-card',
+};
