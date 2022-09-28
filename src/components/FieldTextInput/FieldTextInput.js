fb106e8b1196ec3aa03ba7ad58a7dee0aaf28ba2
Vesa Luusua <vesa.luusua@gmail.com>
2022-09-28 13:16:49 +0300

    FieldTextInput: class > functional

--- src/components/FieldTextInput/FieldTextInput.js
+++ src/components/FieldTextInput/FieldTextInput.js
@@ -8,88 +8,84 @@ import css from './FieldTextInput.module.css';
 
 const CONTENT_MAX_LENGTH = 5000;
 
-class FieldTextInputComponent extends Component {
-  render() {
-    /* eslint-disable no-unused-vars */
-    const {
-      rootClassName,
-      className,
-      inputRootClass,
-      customErrorText,
-      id,
-      label,
-      input,
-      meta,
-      onUnmount,
-      isUncontrolled,
-      inputRef,
-      hideErrorMessage,
-      ...rest
-    } = this.props;
-    /* eslint-enable no-unused-vars */
+const FieldTextInputComponent = props => {
+  const {
+    rootClassName,
+    className,
+    inputRootClass,
+    customErrorText,
+    id,
+    label,
+    input,
+    meta,
+    onUnmount,
+    isUncontrolled,
+    inputRef,
+    hideErrorMessage,
+    ...rest
+  } = props;
 
-    if (label && !id) {
-      throw new Error('id required when a label is given');
-    }
-
-    const { valid, invalid, touched, error } = meta;
-    const isTextarea = input.type === 'textarea';
-
-    const errorText = customErrorText || error;
-
-    // Error message and input error styles are only shown if the
-    // field has been touched and the validation has failed.
-    const hasError = !!customErrorText || !!(touched && invalid && error);
-
-    const fieldMeta = { touched: hasError, error: errorText };
-
-    // Textarea doesn't need type.
-    const { type, ...inputWithoutType } = input;
-    // Uncontrolled input uses defaultValue instead of value.
-    const { value: defaultValue, ...inputWithoutValue } = input;
-    // Use inputRef if it is passed as prop.
-    const refMaybe = inputRef ? { ref: inputRef } : {};
-
-    const inputClasses =
-      inputRootClass ||
-      classNames(css.input, {
-        [css.inputSuccess]: valid,
-        [css.inputError]: hasError,
-        [css.textarea]: isTextarea,
-      });
-    const maxLength = CONTENT_MAX_LENGTH;
-    const inputProps = isTextarea
-      ? {
-          className: inputClasses,
-          id,
-          rows: 1,
-          maxLength,
-          ...refMaybe,
-          ...inputWithoutType,
-          ...rest,
-        }
-      : isUncontrolled
-      ? {
-          className: inputClasses,
-          id,
-          type,
-          defaultValue,
-          ...refMaybe,
-          ...inputWithoutValue,
-          ...rest,
-        }
-      : { className: inputClasses, id, type, ...refMaybe, ...input, ...rest };
-
-    const classes = classNames(rootClassName || css.root, className);
-    return (
-      <div className={classes}>
-        {label ? <label htmlFor={id}>{label}</label> : null}
-        {isTextarea ? <ExpandingTextarea {...inputProps} /> : <input {...inputProps} />}
-        {hideErrorMessage ? null : <ValidationError fieldMeta={fieldMeta} />}
-      </div>
-    );
+  if (label && !id) {
+    throw new Error('id required when a label is given');
   }
-}
+
+  const { valid, invalid, touched, error } = meta;
+  const isTextarea = input.type === 'textarea';
+
+  const errorText = customErrorText || error;
+
+  // Error message and input error styles are only shown if the
+  // field has been touched and the validation has failed.
+  const hasError = !!customErrorText || !!(touched && invalid && error);
+
+  const fieldMeta = { touched: hasError, error: errorText };
+
+  // Textarea doesn't need type.
+  const { type, ...inputWithoutType } = input;
+  // Uncontrolled input uses defaultValue instead of value.
+  const { value: defaultValue, ...inputWithoutValue } = input;
+  // Use inputRef if it is passed as prop.
+  const refMaybe = inputRef ? { ref: inputRef } : {};
+
+  const inputClasses =
+    inputRootClass ||
+    classNames(css.input, {
+      [css.inputSuccess]: valid,
+      [css.inputError]: hasError,
+      [css.textarea]: isTextarea,
+    });
+  const maxLength = CONTENT_MAX_LENGTH;
+  const inputProps = isTextarea
+    ? {
+        className: inputClasses,
+        id,
+        rows: 1,
+        maxLength,
+        ...refMaybe,
+        ...inputWithoutType,
+        ...rest,
+      }
+    : isUncontrolled
+    ? {
+        className: inputClasses,
+        id,
+        type,
+        defaultValue,
+        ...refMaybe,
+        ...inputWithoutValue,
+        ...rest,
+      }
+    : { className: inputClasses, id, type, ...refMaybe, ...input, ...rest };
+
+  const classes = classNames(rootClassName || css.root, className);
+  return (
+    <div className={classes}>
+      {label ? <label htmlFor={id}>{label}</label> : null}
+      {isTextarea ? <ExpandingTextarea {...inputProps} /> : <input {...inputProps} />}
+      {hideErrorMessage ? null : <ValidationError fieldMeta={fieldMeta} />}
+    </div>
+  );
+};
 
 FieldTextInputComponent.defaultProps = {
   rootClassName: null,
