069116ce221daf13c2e4676c7e6a6e250111b890
Vesa Luusua <vesa.luusua@gmail.com>
2022-09-29 18:41:09 +0300

    Add comment about defaultLocationSearches

--- src/config/configDefaultLocationSearches.js
+++ src/config/configDefaultLocationSearches.js
@@ -8,8 +8,8 @@ const { LatLng, LatLngBounds } = sdkTypes;
 // Each item in the array should be an object with a unique `id` (String) and a
 // `predictionPlace` (util.types.place) properties.
 //
-// NOTE: these are highly recommended, since they 
-//       1) help customers to find relevant locations, and 
+// NOTE: these are highly recommended, since they
+//       1) help customers to find relevant locations, and
 //       2) reduce the cost of using map providers geocoding API
 const defaultLocations = [
   // {
