3e5e67513bf3f54b00fa21698c43c17c446a4b2e
Vesa Luusua <vesa.luusua@gmail.com>
2022-10-14 12:59:31 +0300

    Remove isBookingUnitType. Avoid tying unitType with process type.

--- src/containers/InboxPage/InboxPage.duck.js
+++ src/containers/InboxPage/InboxPage.duck.js
@@ -110,12 +110,7 @@ export const loadData = (params, search) => (dispatch, getState, sdk) => {
       'payoutTotal',
       'lineItems',
     ],
-    'fields.listing': [
-      'title',
-      'availabilityPlan',
-      'publicData.transactionType',
-      'publicData.unitType',
-    ],
+    'fields.listing': ['title', 'availabilityPlan', 'publicData.transactionProcessAlias'],
     'fields.user': ['profile.displayName', 'profile.abbreviatedName'],
     'fields.image': ['variants.square-small', 'variants.square-small2x'],
     page,
