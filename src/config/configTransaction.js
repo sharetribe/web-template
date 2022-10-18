762ad59a1812dede755891184e7a4edcf0e0340a
Vesa Luusua <vesa.luusua@gmail.com>
2022-10-18 13:30:58 +0300

    Reduce transaction types to only 1

--- src/config/configTransaction.js
+++ src/config/configTransaction.js
@@ -32,34 +32,36 @@
 
 export const transactionTypes = [
   {
-    type: 'product-selling',
-    label: 'Sell bicycles',
-    process: 'flex-product-default-process',
-    alias: 'release-1',
-    unitType: 'item',
-    showStock: false,
-  },
-  {
     type: 'daily-booking',
     label: 'Daily booking',
     process: 'flex-booking-default-process',
     alias: 'release-1',
     unitType: 'day',
   },
-  {
-    type: 'nightly-booking',
-    label: 'Nightly booking',
-    process: 'flex-booking-default-process',
-    alias: 'release-1',
-    unitType: 'night',
-  },
-  {
-    type: 'hourly-booking',
-    label: 'Hourly booking',
-    process: 'flex-booking-default-process',
-    alias: 'release-1',
-    unitType: 'hour',
-  },
+  // // Here are some examples for other transactionTypes
+  // // TODO: SearchPage does not work well if both booking and product selling are used at the same time
+  // {
+  //   type: 'nightly-booking',
+  //   label: 'Nightly booking',
+  //   process: 'flex-booking-default-process',
+  //   alias: 'release-1',
+  //   unitType: 'night',
+  // },
+  // {
+  //   type: 'hourly-booking',
+  //   label: 'Hourly booking',
+  //   process: 'flex-booking-default-process',
+  //   alias: 'release-1',
+  //   unitType: 'hour',
+  // },
+  // {
+  //   type: 'product-selling',
+  //   label: 'Sell bicycles',
+  //   process: 'flex-product-default-process',
+  //   alias: 'release-1',
+  //   unitType: 'item',
+  //   showStock: false,
+  // },
 ];
 
 // SearchPage can enforce listing query to only those listings with valid transactionType
