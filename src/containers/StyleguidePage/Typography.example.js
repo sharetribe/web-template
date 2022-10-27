569bd2492ac723cdd6182074a6018c1c09f94b8b
Vesa Luusua <vesa.luusua@gmail.com>
2022-10-27 20:58:10 +0300

    Update styleguide>typography cards

--- src/containers/StyleguidePage/Typography.example.js
+++ src/containers/StyleguidePage/Typography.example.js
@@ -11,7 +11,7 @@ const Font = props => {
         <TextComponent />
       </div>
       <div className={css.description}>
-        <p>{description}</p>
+        <p className={css.descriptionInfo}>{description}</p>
         <pre className={css.tinyFont}>{styling}</pre>
       </div>
     </div>
@@ -27,111 +27,145 @@ Font.propTypes = {
 };
 
 const Fonts = () => {
-  const heroTitleFontStyling = `Mobile styles:
-  font-size: 48px;
-  line-height: 54px;
-  margin-top: 25px;
-  margin-bottom: 29px;
-
-  Desktop styles:
-  font-size: 90px;
-  line-height: 96px;
-  margin-top: 25px;
-  margin-bottom: 31px;`;
-
   const h1FontStyling = `Mobile styles:
   font-size: 30px;
-  line-height: 36px;
+  font-weight: var(--fontWeightSemiBold);
+  line-height: 30px;
+  letter-spacing: -0.5px;
   margin-top: 18px;
   margin-bottom: 18px;
 
   Desktop styles:
-  font-size: 48px;
-  line-height: 56px;
+  font-size: 40px;
+  font-weight: var(--fontWeightSemiBold);
+  line-height: 48px;
+  letter-spacing: -1px;
   margin-top: 24px;
   margin-bottom: 24px;`;
 
   const h2FontStyling = `Mobile styles:
-  font-size: 24px;
-  line-height: 30px;
-  margin-top: 21px;
-  margin-bottom: 17px;
+  font-size: 21px;
+  font-weight: var(--fontWeightSemiBold);
+  line-height: 24px;
+  padding: 4px 0 2px 0;
+  margin-top: 24px;
+  margin-bottom: 18px;
 
   Desktop styles:
-  font-size: 24px;
+  font-size: 21px;
+  font-weight: var(--fontWeightSemiBold);
   line-height: 32px;
-  margin-top: 21px;
-  margin-bottom: 19px;`;
+  padding: 0;
+  margin-top: 24px;
+  margin-bottom: 16px;`;
 
   const h3FontStyling = `Mobile styles:
-  font-size: 20px;
-  line-height: 24px;
-  margin-top: 16px;
-  margin-bottom: 14px;
-
-  Desktop styles:
-  font-size: 20px;
-  line-height: 24px;
-  margin-top: 16px;
-  margin-bottom: 16px;`;
-
-  const h4FontStyling = `Mobile styles:
-  font-size: 16px;
-  line-height: 24px;
-  margin-top: 12px;
-  margin-bottom: 12px;
-
-  Desktop styles:
-  font-size: 16px;
-  line-height: 24px;
-  margin-top: 17px;
-  margin-bottom: 15px;`;
-
-  const h5FontStyling = `Mobile styles:
-  font-size: 14px;
-  line-height: 18px;
-  margin-top: 10px;
-  margin-bottom: 8px;
-
-  Desktop styles:
-  font-size: 14px;
-  line-height: 16px;
-  margin-top: 10px;
-  margin-bottom: 14px;`;
-
-  const h6FontStyling = `Mobile styles:
-  font-size: 12px;
-  line-height: 18px;
-  margin-top: 10px;
-  margin-bottom: 8px;
-
-  Desktop styles:
-  font-size: 12px;
-  line-height: 16px;
-  margin-top: 10px;
-  margin-bottom: 6px;`;
-
-  const bodyFontStyling = `Mobile styles:
   font-size: 18px;
+  font-weight: var(--fontWeightSemiBold);
+  line-height: 24px;
+  margin-top: 18px;
+  margin-bottom: 12px;
+
+  Desktop styles:
+  font-size: 18px;
+  font-weight: var(--fontWeightSemiBold);
+  line-height: 24px;
+  padding: 5px 0 3px 0;
+  margin-top: 16px;
+  margin-bottom: 16px;`;
+
+  const h4FontStyling = `
+  Can be used as a subtitle and also as
+  a label font for form inputs.
+
+  Mobile styles:
+  font-size: 15px;
+  font-weight: var(--fontWeightMedium);
   line-height: 24px;
   margin-top: 12px;
   margin-bottom: 12px;
 
   Desktop styles:
-  font-size: 20px;
-  line-height: 32px;
+  font-size: 15px;
+  font-weight: var(--fontWeightMedium);
+  line-height: 24px;
+  padding: 6px 0 2px 0;
   margin-top: 16px;
   margin-bottom: 16px;`;
 
-  const tinyFontStyling = `Mobile styles:
+  const h5FontStyling = `
+  Can be used as a fine print text.
+
+  Mobile styles:
+  font-size: 14px;
+  font-weight: var(--fontWeightMedium);
+  line-height: 18px;
+  padding: 5px 0 1px 0;
+  margin-top: 12px;
+  margin-bottom: 6px;
+
+  Desktop styles:
+  font-size: 14px;
+  font-weight: var(--fontWeightMedium);
+  line-height: 16px;
+  padding: 2px 0 6px 0;
+  margin-top: 8px;
+  margin-bottom: 16px;`;
+
+  const h6FontStyling = `
+  Can be used as a 'close' button text.
+
+  Mobile styles:
+  font-size: 12px;
+  font-weight: var(--fontWeightBold);
+  line-height: 18px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  padding: 2px 0 4px 0;
+  margin-top: 6px;
+  margin-bottom: 6px;
+
+  Desktop styles:
+  font-size: 12px;
+  font-weight: var(--fontWeightBold);
+  line-height: 16px;
+  padding: 3px 0 5px 0;
+  margin-top: 8px;
+  margin-bottom: 8px;`;
+
+  const bodyFontStyling = `
+  Paragraphs and other body texts.
+
+  Mobile styles:
+  font-size: 14px;
+  font-weight: var(--fontWeightMedium);
+  line-height: 24px;
+  letter-spacing: -0.1px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+
+  Desktop styles:
+  font-size: 16px;
+  font-weight: var(--fontWeightMedium);
+  line-height: 32px;
+  letter-spacing: -0.1px;
+  margin-top: 16px;
+  margin-bottom: 16px;`;
+
+  const tinyFontStyling = `
+  Very small print.
+
+  Mobile styles:
   font-size: 13px;
+  font-weight: var(--fontWeightRegular);
   line-height: 18px;
   margin-top: 9.5px;
   margin-bottom: 8.5px;
 
   Desktop styles:
   font-size: 13px;
-  line-height: 18px;
+  font-weight: var(--fontWeightRegular);
+  line-height: 16px;
   margin-top: 10.5px;
   margin-bottom: 13.5px;`;
 
@@ -150,40 +184,35 @@ const Fonts = () => {
       </p>
       <div className={fontsContainerClasses}>
         <Font
-          component={() => <p className={css.heroTitle}>Hello beautiful world.</p>}
-          description="--marketplaceHeroTitleFontStyles: Biggest font style. Used in the hero in landingPage. Works as a site title."
-          styling={heroTitleFontStyling}
-        />
-        <Font
-          component={() => <h1>Listing + Page title</h1>}
-          description="H1 / --marketplaceH1FontStyles: Works as a page title. Inline component using --marketplaceH1FontStyles can be aligned with .h1Font to the same baseline by dropping it 18px (e.g. with margin-top)."
+          component={() => <h1>H1 heading</h1>}
+          description="<h1>/composes: h1 from global;"
           styling={h1FontStyling}
         />
         <Font
-          component={() => <h2>This many listings found in here</h2>}
-          description="H2 / --marketplaceH2FontStyles: Works as a page subtitle and in sidebar menu link."
+          component={() => <h2>H2 heading</h2>}
+          description="<h2>/composes: h2 from global;"
           styling={h2FontStyling}
         />
         <Font
-          component={() => <h3>About this listing</h3>}
-          description="H3 / --marketplaceH3FontStyles: Works as a paragraph subtitle."
+          component={() => <h3>H3 heading</h3>}
+          description="<h3>/composes: h3 from global;"
           styling={h3FontStyling}
         />
         <Font
-          component={() => <h4>H4 (--marketplaceH4FontStyles): Lorem ipsum dolor sit amet</h4>}
-          description="Can be used as a subtitle and also as a label font for form inputs."
+          component={() => <h4>H4: Lorem ipsum dolor sit amet</h4>}
+          description="<h4>/composes: h4 from global;"
           styling={h4FontStyling}
         />
         <Font
           component={() => (
-            <h5>You will only be charged if your request is accepted by the provider.</h5>
+            <h5>H5: You will only be charged if your request is accepted by the provider.</h5>
           )}
-          description="H5 / --marketplaceH5FontStyles: Can be used as a fine print text."
+          description="<h5>/composes: h5 from global;"
           styling={h5FontStyling}
         />
         <Font
-          component={() => <h6>Close</h6>}
-          description="H6 / --marketplaceH6FontStyles: Works as a close text."
+          component={() => <h6>H6: Close</h6>}
+          description="<h6>/composes: h6 from global;"
           styling={h6FontStyling}
         />
         <Font
@@ -195,12 +224,12 @@ const Fonts = () => {
               elit.
             </p>
           )}
-          description="--marketplaceBodyFontStyles, p, button, etc: Paragraphs and other body texts."
+          description="<p>, <button>, etc. / composes: marketplaceBodyFontStyles from global;"
           styling={bodyFontStyling}
         />
         <Font
           component={() => <p className={css.tinyFont}>Hosted by user</p>}
-          description="--marketplaceTinyFontStyles: Very small print."
+          description="composes: marketplaceTinyFontStyles from global;"
           styling={tinyFontStyling}
         />
       </div>
