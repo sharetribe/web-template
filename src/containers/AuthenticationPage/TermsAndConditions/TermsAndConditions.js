679f4b92c8a3c2c6025734330b2c06d0039ecd5d
Vesa Luusua <vesa.luusua@gmail.com>
2023-02-22 20:43:43 +0200

    AuthenticationPage: add TermsAndConditions subcomponent

---
+++ src/containers/AuthenticationPage/TermsAndConditions/TermsAndConditions.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { func } from 'prop-types';
+
+import { requiredFieldArrayCheckbox } from '../../../util/validators';
+import { FieldCheckboxGroup } from '../../../components';
+
+import { FormattedMessage, injectIntl, intlShape } from '../../../util/reactIntl';
+
+import css from './TermsAndConditions.module.css';
+
+const KEY_CODE_ENTER = 13;
+
+const TermsAndConditions = props => {
+  const { onOpenTermsOfService, onOpenPrivacyPolicy, intl } = props;
+
+  const handleClick = callback => e => {
+    e.preventDefault();
+    callback(e);
+  };
+  const handleKeyUp = callback => e => {
+    // Allow click action with keyboard like with normal links
+    if (e.keyCode === KEY_CODE_ENTER) {
+      callback();
+    }
+  };
+
+  const termsLink = (
+    <span
+      className={css.termsLink}
+      onClick={handleClick(onOpenTermsOfService)}
+      role="button"
+      tabIndex="0"
+      onKeyUp={handleKeyUp(onOpenTermsOfService)}
+    >
+      <FormattedMessage id="AuthenticationPage.termsAndConditionsTermsLinkText" />
+    </span>
+  );
+
+  const privacyLink = (
+    <span
+      className={css.privacyLink}
+      onClick={handleClick(onOpenPrivacyPolicy)}
+      role="button"
+      tabIndex="0"
+      onKeyUp={handleKeyUp(onOpenPrivacyPolicy)}
+    >
+      <FormattedMessage id="AuthenticationPage.termsAndConditionsPrivacyLinkText" />
+    </span>
+  );
+
+  return (
+    <div className={css.root}>
+      <FieldCheckboxGroup
+        name="terms-of-service"
+        id={`tos-accepted`}
+        optionLabelClassName={css.finePrint}
+        options={[
+          {
+            key: 'tos',
+            label: intl.formatMessage(
+              { id: 'AuthenticationPage.termsAndConditionsAcceptText' },
+              { termsLink, privacyLink }
+            ),
+          },
+        ]}
+        validate={requiredFieldArrayCheckbox('You need to accept Terms of Service')}
+      />
+    </div>
+  );
+};
+
+// TermsAndConditions.defaultProps = {};
+
+TermsAndConditions.propTypes = {
+  onOpenTermsOfService: func.isRequired,
+  onOpenPrivacyPolicy: func.isRequired,
+};
+
+export default TermsAndConditions;
