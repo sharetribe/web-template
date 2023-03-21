ddbf428e3e02297654c4b1a3245d9adc37ee2cd7
Vesa Luusua <vesa.luusua@gmail.com>
2023-03-21 13:30:59 +0200

    PaginationLinks: update test

--- src/components/PaginationLinks/PaginationLinks.test.js
+++ src/components/PaginationLinks/PaginationLinks.test.js
@@ -1,75 +1,73 @@
 import React from 'react';
+import '@testing-library/jest-dom';
+
+import { renderWithProviders as render, testingLibrary } from '../../util/test-helpers';
 import { fakeIntl } from '../../util/test-data';
-import { renderShallow } from '../../util/test-helpers';
-import { PaginationLinksComponent as PaginationLinks } from './PaginationLinks';
+
+import PaginationLinks from './PaginationLinks';
+
+const { screen } = testingLibrary;
 
 describe('PaginationLinks', () => {
-  it('should match snapshot with both links enabled', () => {
+  const props = pagination => ({
+    pageName: 'SearchPage',
+    pagePathParams: {},
+    pageSearchParams: { param: 'foobar' },
+    pagination,
+    intl: fakeIntl,
+  });
+
+  // This is quite small component what comes to rendered HTML
+  // For now, we rely on snapshot-testing and checking the existence of next & prev buttons.
+  test('should match snapshot with both links enabled', () => {
     const pagination = {
       page: 2,
       perPage: 10,
       totalItems: 30,
       totalPages: 3,
     };
-    const props = {
-      pageName: 'SomePage',
-      pagePathParams: { id: 'some-page-id' },
-      pageSearchParams: { param: 'foobar' },
-      pagination,
-      intl: fakeIntl,
-    };
-    const tree = renderShallow(<PaginationLinks {...props} />);
-    expect(tree).toMatchSnapshot();
+    const tree = render(<PaginationLinks {...props(pagination)} />);
+    expect(screen.queryByTitle('PaginationLinks.previous')).toBeInTheDocument();
+    expect(screen.queryByTitle('PaginationLinks.next')).toBeInTheDocument();
+    expect(tree.asFragment().firstChild).toMatchSnapshot();
   });
-  it('should match snapshot with both links disabled', () => {
+
+  test('should match snapshot with both links disabled', () => {
     const pagination = {
       page: 1,
       perPage: 10,
       totalItems: 10,
       totalPages: 1,
     };
-    const props = {
-      pageName: 'SomePage',
-      pagePathParams: { id: 'some-page-id' },
-      pageSearchParams: { param: 'foobar' },
-      pagination,
-      intl: fakeIntl,
-    };
-    const tree = renderShallow(<PaginationLinks {...props} />);
-    expect(tree).toMatchSnapshot();
+    const tree = render(<PaginationLinks {...props(pagination)} />);
+    expect(screen.queryByTitle('PaginationLinks.previous')).not.toBeInTheDocument();
+    expect(screen.queryByTitle('PaginationLinks.next')).not.toBeInTheDocument();
+    expect(tree.asFragment().firstChild).toMatchSnapshot();
   });
-  it('should match snapshot with prev disabled and next enabled', () => {
+
+  test('should match snapshot with prev disabled and next enabled', () => {
     const pagination = {
       page: 1,
       perPage: 10,
       totalItems: 30,
       totalPages: 3,
     };
-    const props = {
-      pageName: 'SomePage',
-      pagePathParams: { id: 'some-page-id' },
-      pageSearchParams: { param: 'foobar' },
-      pagination,
-      intl: fakeIntl,
-    };
-    const tree = renderShallow(<PaginationLinks {...props} />);
-    expect(tree).toMatchSnapshot();
+    const tree = render(<PaginationLinks {...props(pagination)} />);
+    expect(screen.queryByTitle('PaginationLinks.previous')).not.toBeInTheDocument();
+    expect(screen.queryByTitle('PaginationLinks.next')).toBeInTheDocument();
+    expect(tree.asFragment().firstChild).toMatchSnapshot();
   });
-  it('should match snapshot with prev enabled and next disabled', () => {
+
+  test('should match snapshot with prev enabled and next disabled', () => {
     const pagination = {
       page: 3,
       perPage: 10,
       totalItems: 30,
       totalPages: 3,
     };
-    const props = {
-      pageName: 'SomePage',
-      pagePathParams: { id: 'some-page-id' },
-      pageSearchParams: { param: 'foobar' },
-      pagination,
-      intl: fakeIntl,
-    };
-    const tree = renderShallow(<PaginationLinks {...props} />);
-    expect(tree).toMatchSnapshot();
+    const tree = render(<PaginationLinks {...props(pagination)} />);
+    expect(screen.queryByTitle('PaginationLinks.previous')).toBeInTheDocument();
+    expect(screen.queryByTitle('PaginationLinks.next')).not.toBeInTheDocument();
+    expect(tree.asFragment().firstChild).toMatchSnapshot();
   });
 });
