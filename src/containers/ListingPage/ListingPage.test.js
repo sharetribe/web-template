99c61b4ea6968f1516678c15a1368c44408bb16a
Vesa Luusua <vesa.luusua@gmail.com>
2023-03-21 13:47:40 +0200

    ListingPage: update test

--- src/containers/ListingPage/ListingPage.test.js
+++ src/containers/ListingPage/ListingPage.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
-import { FormattedMessage } from '../../util/reactIntl';
 import { types as sdkTypes } from '../../util/sdkLoader';
 import {
   createUser,
@@ -10,24 +9,30 @@ import {
   createOwnListing,
   fakeIntl,
 } from '../../util/test-data';
-import { storableError } from '../../util/errors';
 import {
-  renderShallow,
+  renderWithProviders as render,
+  testingLibrary,
   getRouteConfiguration,
   getDefaultConfiguration,
 } from '../../util/test-helpers';
+
+import { storableError } from '../../util/errors';
+
 import {
   LISTING_STATE_PENDING_APPROVAL,
   LISTING_STATE_PUBLISHED,
   LISTING_STATE_CLOSED,
 } from '../../util/types';
+
 import { addMarketplaceEntities } from '../../ducks/marketplaceData.duck';
 import { showListingRequest, showListingError, showListing } from './ListingPage.duck';
 
-import { ListingPageComponent } from './ListingPageHeroImage';
+import { ListingPageComponent as ListingPageHeroImageComponent } from './ListingPageHeroImage';
+import { ListingPageComponent as ListingPageFullImageComponent } from './ListingPageFullImage';
 import ActionBarMaybe from './ActionBarMaybe';
 
 const { UUID } = sdkTypes;
+const { screen } = testingLibrary;
 const noop = () => null;
 
 const listingConfig = {
@@ -67,176 +72,229 @@ const listingConfig = {
     },
   ],
 };
-describe('ListingPage', () => {
-  it('matches snapshot', () => {
-    const currentUser = createCurrentUser('user-2');
-    const id = 'listing1';
-    const slug = 'listing1-title';
-    const listing1 = createListing(id, {}, { author: createUser('user-1') });
-    const listing1Own = createOwnListing(id, {}, { author: createCurrentUser('user-1') });
-    const getListing = () => listing1;
-    const getOwnListing = () => listing1Own;
 
-    const props = {
-      location: {
-        pathname: `/l/${slug}/${id}`,
-        search: '',
-        hash: '',
-      },
-      history: {
-        push: () => console.log('HistoryPush called'),
-      },
-      params: { id, slug },
-      currentUser,
-      getListing,
-      getOwnListing,
-      intl: fakeIntl,
-      authInProgress: false,
-      currentUserHasListings: false,
-      isAuthenticated: false,
-      onLogout: noop,
-      onLoadListing: noop,
-      onManageDisableScrolling: noop,
-      scrollingDisabled: false,
-      callSetInitialValues: noop,
-      sendVerificationEmailInProgress: false,
-      onResendVerificationEmail: noop,
-      onInitializeCardPaymentData: noop,
-      sendInquiryInProgress: false,
-      onSendInquiry: noop,
-      listingConfig,
-      fetchLineItemsInProgress: false,
-      onFetchTransactionLineItems: () => null,
-      onFetchTimeSlots: () => null,
-      config: getDefaultConfiguration(),
-      routeConfiguration: getRouteConfiguration(),
-    };
+describe('ListingPage variants', () => {
+  const currentUser = createCurrentUser('user-2');
+  const id = 'listing1';
+  const slug = 'listing1-title';
+  const publicData = {
+    listingType: 'sell-bicycles',
+    transactionProcessAlias: 'default-purchase/release-1',
+    unitType: 'item',
+    category: 'cat_1',
+  };
+  const listing1 = createListing(id, { publicData }, { author: createUser('user-1') });
+  const listing1Own = createOwnListing(id, {}, { author: createCurrentUser('user-1') });
+  const getListing = () => listing1;
+  const getOwnListing = () => listing1Own;
 
-    const tree = renderShallow(<ListingPageComponent {...props} />);
-    expect(tree).toMatchSnapshot();
+  const props = {
+    location: {
+      pathname: `/l/${slug}/${id}`,
+      search: '',
+      hash: '',
+    },
+    history: {
+      push: () => console.log('HistoryPush called'),
+    },
+    params: { id, slug },
+    currentUser,
+    getListing,
+    getOwnListing,
+    intl: fakeIntl,
+    authInProgress: false,
+    currentUserHasListings: false,
+    isAuthenticated: false,
+    onLogout: noop,
+    onLoadListing: noop,
+    onManageDisableScrolling: noop,
+    scrollingDisabled: false,
+    callSetInitialValues: noop,
+    sendVerificationEmailInProgress: false,
+    onResendVerificationEmail: noop,
+    onInitializeCardPaymentData: noop,
+    sendInquiryInProgress: false,
+    onSendInquiry: noop,
+    listingConfig,
+    fetchLineItemsInProgress: false,
+    onFetchTransactionLineItems: () => null,
+    onFetchTimeSlots: () => null,
+    config: getDefaultConfiguration(),
+    routeConfiguration: getRouteConfiguration(),
+  };
+
+  test('ListingPageHeroImage has hero section', () => {
+    render(<ListingPageHeroImageComponent {...props} />);
+    expect(screen.getByTestId('hero')).toBeInTheDocument();
+
+    const orderTitle = screen.queryAllByRole('heading', { name: 'ListingPage.orderTitle' });
+    expect(orderTitle).toHaveLength(3);
+    expect(screen.getByRole('heading', { name: 'ListingPage.detailsTitle' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'ListingPage.locationTitle' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'ListingPage.reviewsTitle' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: 'ListingPage.aboutProviderTitle' })
+    ).toBeInTheDocument();
   });
 
-  describe('Duck', () => {
-    const listingExtendedData = [];
-    const config = {
-      layout: {
-        listingImage: {
-          aspectWidth: 400,
-          aspectHeight: 400,
-          variantPrefix: 'listing-card',
-        },
+  test('ListingPageFullImage has no hero section', () => {
+    render(<ListingPageFullImageComponent {...props} />);
+    expect(screen.queryByTestId('hero')).not.toBeInTheDocument();
+
+    const orderTitle = screen.queryAllByRole('heading', { name: 'ListingPage.orderTitle' });
+    expect(orderTitle).toHaveLength(3);
+    expect(screen.getByRole('heading', { name: 'ListingPage.detailsTitle' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'ListingPage.locationTitle' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'ListingPage.reviewsTitle' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: 'ListingPage.aboutProviderTitle' })
+    ).toBeInTheDocument();
+  });
+});
+
+describe('Duck', () => {
+  const listingExtendedData = [];
+  const config = {
+    layout: {
+      listingImage: {
+        aspectWidth: 400,
+        aspectHeight: 400,
+        variantPrefix: 'listing-card',
       },
-      listing: {
-        listingExtendedData,
-      },
-    };
+    },
+    listing: {
+      listingExtendedData,
+    },
+  };
 
-    it('showListing() success', () => {
-      const id = new UUID('00000000-0000-0000-0000-000000000000');
-      const dispatch = jest.fn(action => action);
-      const response = { status: 200 };
-      const show = jest.fn(() => Promise.resolve(response));
-      const sdk = { listings: { show }, currentUser: { show } };
+  test('showListing() success', () => {
+    const id = new UUID('00000000-0000-0000-0000-000000000000');
+    const dispatch = jest.fn(action => action);
+    const response = { status: 200 };
+    const show = jest.fn(() => Promise.resolve(response));
+    const sdk = { listings: { show }, currentUser: { show } };
 
-      return showListing(id, config)(dispatch, null, sdk).then(data => {
-        expect(data).toEqual(response);
-        expect(show.mock.calls).toEqual([
-          [
-            expect.objectContaining({
-              id,
-              'imageVariant.listing-card': 'w:400;h:400;fit:crop',
-              'imageVariant.listing-card-2x': 'w:800;h:800;fit:crop',
-              include: ['author', 'author.profileImage', 'images', 'currentStock'],
-            }),
-          ],
-        ]);
-        expect(dispatch.mock.calls).toEqual([
-          [showListingRequest(id)],
-          [expect.anything()], // fetchCurrentUser() call
-          [addMarketplaceEntities(data, { listingExtendedData })],
-        ]);
-      });
-    });
-
-    it('showListing() error', () => {
-      const id = new UUID('00000000-0000-0000-0000-000000000000');
-      const dispatch = jest.fn(action => action);
-      const error = new Error('fail');
-      const show = jest.fn(() => Promise.reject(error));
-      const sdk = { listings: { show } };
-
-      // Calling sdk.listings.show is expected to fail now
-
-      return showListing(id, config)(dispatch, null, sdk).then(data => {
-        expect(show.mock.calls).toEqual([
-          [
-            expect.objectContaining({
-              id,
-              'imageVariant.listing-card': 'w:400;h:400;fit:crop',
-              'imageVariant.listing-card-2x': 'w:800;h:800;fit:crop',
-              include: ['author', 'author.profileImage', 'images', 'currentStock'],
-            }),
-          ],
-        ]);
-        expect(dispatch.mock.calls).toEqual([
-          [showListingRequest(id)],
-          [expect.anything()], // fetchCurrentUser() call
-          [showListingError(storableError(error))],
-        ]);
-      });
+    return showListing(id, config)(dispatch, null, sdk).then(data => {
+      expect(data).toEqual(response);
+      expect(show.mock.calls).toEqual([
+        [
+          expect.objectContaining({
+            id,
+            'imageVariant.listing-card': 'w:400;h:400;fit:crop',
+            'imageVariant.listing-card-2x': 'w:800;h:800;fit:crop',
+            include: ['author', 'author.profileImage', 'images', 'currentStock'],
+          }),
+        ],
+      ]);
+      expect(dispatch.mock.calls).toEqual([
+        [showListingRequest(id)],
+        [expect.anything()], // fetchCurrentUser() call
+        [addMarketplaceEntities(data, { listingExtendedData })],
+      ]);
     });
   });
 
-  describe('ActionBarMaybe', () => {
-    it('shows users own listing status', () => {
-      const listing = createListing('listing-published', {
-        state: LISTING_STATE_PUBLISHED,
-      });
-      const actionBar = shallow(<ActionBarMaybe isOwnListing listing={listing} editParams={{}} />);
-      const formattedMessages = actionBar.find(FormattedMessage);
-      expect(formattedMessages.length).toEqual(2);
-      expect(formattedMessages.at(0).props().id).toEqual('ListingPage.ownListing');
-      expect(formattedMessages.at(1).props().id).toEqual('ListingPage.editListing');
+  test('showListing() error', () => {
+    const id = new UUID('00000000-0000-0000-0000-000000000000');
+    const dispatch = jest.fn(action => action);
+    const error = new Error('fail');
+    const show = jest.fn(() => Promise.reject(error));
+    const sdk = { listings: { show } };
+
+    // Calling sdk.listings.show is expected to fail now
+
+    return showListing(id, config)(dispatch, null, sdk).then(data => {
+      expect(show.mock.calls).toEqual([
+        [
+          expect.objectContaining({
+            id,
+            'imageVariant.listing-card': 'w:400;h:400;fit:crop',
+            'imageVariant.listing-card-2x': 'w:800;h:800;fit:crop',
+            include: ['author', 'author.profileImage', 'images', 'currentStock'],
+          }),
+        ],
+      ]);
+      expect(dispatch.mock.calls).toEqual([
+        [showListingRequest(id)],
+        [expect.anything()], // fetchCurrentUser() call
+        [showListingError(storableError(error))],
+      ]);
     });
-    it('shows users own pending listing status', () => {
-      const listing = createListing('listing-published', {
-        state: LISTING_STATE_PENDING_APPROVAL,
-      });
-      const actionBar = shallow(<ActionBarMaybe isOwnListing listing={listing} editParams={{}} />);
-      const formattedMessages = actionBar.find(FormattedMessage);
-      expect(formattedMessages.length).toEqual(2);
-      expect(formattedMessages.at(0).props().id).toEqual('ListingPage.ownListingPendingApproval');
-      expect(formattedMessages.at(1).props().id).toEqual('ListingPage.editListing');
+  });
+});
+
+describe('ActionBarMaybe', () => {
+  test('shows users own listing status', () => {
+    const listing = createListing('listing-published', {
+      state: LISTING_STATE_PUBLISHED,
     });
-    it('shows users own closed listing status', () => {
-      const listing = createListing('listing-closed', {
-        state: LISTING_STATE_CLOSED,
-      });
-      const actionBar = shallow(<ActionBarMaybe isOwnListing listing={listing} editParams={{}} />);
-      const formattedMessages = actionBar.find(FormattedMessage);
-      expect(formattedMessages.length).toEqual(2);
-      expect(formattedMessages.at(0).props().id).toEqual('ListingPage.ownClosedListing');
-      expect(formattedMessages.at(1).props().id).toEqual('ListingPage.editListing');
+    render(
+      <ActionBarMaybe
+        isOwnListing
+        listing={listing}
+        editParams={{ id: 'id1', slug: 'asdf', type: 'edit', tab: 'details' }}
+      />
+    );
+
+    expect(screen.getByText('ListingPage.ownListing')).toBeInTheDocument();
+    expect(screen.getByText('ListingPage.editListing')).toBeInTheDocument();
+  });
+
+  test('shows users own pending listing status', () => {
+    const listing = createListing('listing-published', {
+      state: LISTING_STATE_PENDING_APPROVAL,
     });
-    it('shows closed listing status', () => {
-      const listing = createListing('listing-closed', {
-        state: LISTING_STATE_CLOSED,
-      });
-      const actionBar = shallow(
-        <ActionBarMaybe isOwnListing={false} listing={listing} editParams={{}} />
-      );
-      const formattedMessages = actionBar.find(FormattedMessage);
-      expect(formattedMessages.length).toEqual(1);
-      expect(formattedMessages.at(0).props().id).toEqual('ListingPage.closedListing');
+    render(
+      <ActionBarMaybe
+        isOwnListing
+        listing={listing}
+        editParams={{ id: 'id1', slug: 'asdf', type: 'edit', tab: 'details' }}
+      />
+    );
+    expect(screen.getByText('ListingPage.ownListingPendingApproval')).toBeInTheDocument();
+    expect(screen.getByText('ListingPage.editListing')).toBeInTheDocument();
+  });
+
+  test('shows users own closed listing status', () => {
+    const listing = createListing('listing-closed', {
+      state: LISTING_STATE_CLOSED,
     });
-    it("is missing if listing is not closed or user's own", () => {
-      const listing = createListing('listing-published', {
-        state: LISTING_STATE_PUBLISHED,
-      });
-      const actionBar = shallow(
-        <ActionBarMaybe isOwnListing={false} listing={listing} editParams={{}} />
-      );
-      expect(actionBar.getElement()).toBeNull();
+    render(
+      <ActionBarMaybe
+        isOwnListing
+        listing={listing}
+        editParams={{ id: 'id1', slug: 'asdf', type: 'edit', tab: 'details' }}
+      />
+    );
+    expect(screen.getByText('ListingPage.ownClosedListing')).toBeInTheDocument();
+    expect(screen.getByText('ListingPage.editListing')).toBeInTheDocument();
+  });
+
+  test('shows closed listing status', () => {
+    const listing = createListing('listing-closed', {
+      state: LISTING_STATE_CLOSED,
     });
+    render(
+      <ActionBarMaybe
+        isOwnListing={false}
+        listing={listing}
+        editParams={{ id: 'id1', slug: 'asdf', type: 'edit', tab: 'details' }}
+      />
+    );
+    expect(screen.getByText('ListingPage.closedListing')).toBeInTheDocument();
+  });
+
+  test("is missing if listing is not closed or user's own", () => {
+    const listing = createListing('listing-published', {
+      state: LISTING_STATE_PUBLISHED,
+    });
+    const actionBar = render(
+      <ActionBarMaybe
+        isOwnListing={false}
+        listing={listing}
+        editParams={{ id: 'id1', slug: 'asdf', type: 'edit', tab: 'details' }}
+      />
+    );
+    expect(actionBar.asFragment().firstChild).toBeNull();
   });
 });
