932d52b356affd8a72a2291ee6eae953a0de1266
Vesa Luusua <vesa.luusua@gmail.com>
2023-03-21 13:35:44 +0200

    CheckoutPage: update test (todo: needs more tests)

--- src/containers/CheckoutPage/CheckoutPage.test.js
+++ src/containers/CheckoutPage/CheckoutPage.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import '@testing-library/jest-dom';
+import Decimal from 'decimal.js';
 
-import { renderShallow, getDefaultConfiguration } from '../../util/test-helpers';
-import { createUser, createCurrentUser, createListing, fakeIntl } from '../../util/test-data';
+import { types as sdkTypes } from '../../util/sdkLoader';
+import {
+  createUser,
+  createCurrentUser,
+  createListing,
+  fakeIntl,
+  createTransaction,
+} from '../../util/test-data';
+import {
+  renderWithProviders as render,
+  testingLibrary,
+  getDefaultConfiguration,
+} from '../../util/test-helpers';
+
 import { CheckoutPageComponent } from './CheckoutPage';
 import checkoutPageReducer, { SET_INITIAL_VALUES, setInitialValues } from './CheckoutPage.duck';
 
+const { Money } = sdkTypes;
+const { screen } = testingLibrary;
 const noop = () => null;
 
 const routeConfiguration = [
@@ -21,38 +37,188 @@ const routeConfiguration = [
 ];
 
 describe('CheckoutPage', () => {
-  it('matches snapshot', () => {
+  beforeEach(() => {
+    // This is not defined by default on test env. AuthenticationPage needs it.
+    window.matchMedia = jest.fn(() => ({
+      matches: true,
+      addEventListener: noop,
+      removeEventListener: noop,
+    }));
+
+    window.Stripe = jest.fn(() => ({
+      elements: () => ({
+        create: () => ({
+          mount: noop,
+          unmount: noop,
+          update: noop,
+          addEventListener: noop,
+          removeEventListener: noop,
+        }),
+      }),
+    }));
+  });
+
+  afterAll(() => {
+    // Remove window.scrollTo
+    jest.clearAllMocks();
+  });
+
+  const lineItems = [
+    {
+      code: 'line-item/item',
+      includeFor: ['customer', 'provider'],
+      quantity: new Decimal(2),
+      unitPrice: new Money(1000, 'USD'),
+      lineTotal: new Money(2000, 'USD'),
+      reversal: false,
+    },
+    {
+      code: 'line-item/provider-commission',
+      includeFor: ['provider'],
+      unitPrice: new Money(-100, 'USD'),
+      lineTotal: new Money(-100, 'USD'),
+      reversal: false,
+    },
+  ];
+  const commonProps = {
+    dispatch: noop,
+    history: { push: noop, action: 'PUSH' },
+    intl: fakeIntl,
+    currentUser: createCurrentUser('currentUser'),
+    params: { id: 'listing1', slug: 'listing1' },
+    sendOrderRequest: noop,
+    fetchStripeCustomer: noop,
+    stripeCustomerFetched: true,
+    fetchSpeculatedTransaction: noop,
+    speculateTransactionInProgress: false,
+    scrollingDisabled: false,
+    onConfirmPayment: noop,
+    onConfirmCardPayment: noop,
+    onInitiateOrder: noop,
+    onRetrievePaymentIntent: noop,
+    onSavePaymentMethod: noop,
+    onSendMessage: noop,
+    confirmCardPaymentInProgress: false,
+    config: getDefaultConfiguration(),
+    routeConfiguration,
+  };
+
+  test('Check that purchase has relevant info', () => {
     const listing = createListing(
       'listing1',
       { publicData: { transactionProcessAlias: 'default-purchase', unitType: 'item' } },
       { author: createUser('author') }
     );
+
+    const shippingFeeLineItem = [
+      {
+        code: 'line-item/shipping-fee',
+        includeFor: ['customer', 'provider'],
+        unitPrice: new Money(500, 'USD'),
+        quantity: new Decimal(1),
+        lineTotal: new Money(500, 'USD'),
+        reversal: false,
+      },
+    ];
     const props = {
-      orderData: { quantity: 3, deliveryMethod: 'shipping' },
-      dispatch: noop,
-      history: { push: noop, action: 'PUSH' },
-      intl: fakeIntl,
+      ...commonProps,
+      orderData: { quantity: 2, deliveryMethod: 'shipping' },
       listing,
-      currentUser: createCurrentUser('currentUser'),
-      params: { id: 'listing1', slug: 'listing1' },
-      sendOrderRequest: noop,
-      fetchStripeCustomer: noop,
-      stripeCustomerFetched: false,
-      fetchSpeculatedTransaction: noop,
-      speculateTransactionInProgress: false,
-      scrollingDisabled: false,
-      onConfirmPayment: noop,
-      onConfirmCardPayment: noop,
-      onInitiateOrder: noop,
-      onRetrievePaymentIntent: noop,
-      onSavePaymentMethod: noop,
-      onSendMessage: noop,
-      confirmCardPaymentInProgress: false,
-      config: getDefaultConfiguration(),
-      routeConfiguration,
+      speculatedTransaction: createTransaction({
+        id: 'tx1',
+        lineItems: lineItems.concat(shippingFeeLineItem),
+        total: new Money(2500, 'USD'),
+      }),
     };
-    const tree = renderShallow(<CheckoutPageComponent {...props} />);
-    expect(tree).toMatchSnapshot();
+
+    render(<CheckoutPageComponent {...props} />);
+
+    const purchaseTitle = 'CheckoutPage.default-purchase.title';
+    expect(screen.getByRole('heading', { name: purchaseTitle })).toBeInTheDocument();
+    const orderBreakdownTitle = 'CheckoutPage.default-purchase.orderBreakdown';
+    expect(screen.getByRole('heading', { name: orderBreakdownTitle })).toBeInTheDocument();
+    // These are rendered twice (mobile & desktop)
+    expect(screen.getAllByText('OrderBreakdown.baseUnitQuantity')).toHaveLength(2);
+    expect(screen.getAllByText('OrderBreakdown.shippingFee')).toHaveLength(2);
+    expect(screen.getAllByText('OrderBreakdown.total')).toHaveLength(2);
+
+    const getTextbox = name => screen.getByRole('textbox', { name });
+
+    const shippingHeading = 'ShippingDetails.title';
+    expect(screen.getByRole('heading', { name: shippingHeading })).toBeInTheDocument();
+    expect(getTextbox('ShippingDetails.recipientNameLabel')).toBeInTheDocument();
+    expect(getTextbox('ShippingDetails.recipientPhoneNumberLabel')).toBeInTheDocument();
+    expect(getTextbox('ShippingDetails.addressLine1Label')).toBeInTheDocument();
+    expect(getTextbox('ShippingDetails.addressLine2Label')).toBeInTheDocument();
+    expect(getTextbox('ShippingDetails.postalCodeLabel')).toBeInTheDocument();
+    expect(getTextbox('ShippingDetails.cityLabel')).toBeInTheDocument();
+    expect(getTextbox('ShippingDetails.stateLabel')).toBeInTheDocument();
+
+    const paymentHeading = 'StripePaymentForm.paymentHeading';
+    expect(screen.getByRole('heading', { name: paymentHeading })).toBeInTheDocument();
+
+    const billingDetails = 'StripePaymentForm.billingDetails';
+    expect(screen.getByRole('heading', { name: billingDetails })).toBeInTheDocument();
+    expect(getTextbox('StripePaymentForm.billingDetailsNameLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.addressLine1Label')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.addressLine2Label')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.postalCodeLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.cityLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.stateLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentForm.messageLabel')).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('button', { name: 'StripePaymentForm.submitPaymentInfo' })
+    ).toBeInTheDocument();
+  });
+
+  test('Check that booking has relevant info', () => {
+    const listing = createListing(
+      'listing1',
+      { publicData: { transactionProcessAlias: 'default-booking', unitType: 'day' } },
+      { author: createUser('author') }
+    );
+
+    const props = {
+      ...commonProps,
+      orderData: { quantity: 2 },
+      listing,
+      speculatedTransaction: createTransaction({
+        id: 'tx1',
+        lineItems,
+        total: new Money(2500, 'USD'),
+      }),
+    };
+    render(<CheckoutPageComponent {...props} />);
+
+    const bookingTitle = 'CheckoutPage.default-booking.title';
+    expect(screen.getByRole('heading', { name: bookingTitle })).toBeInTheDocument();
+    const orderBreakdownTitle = 'CheckoutPage.default-booking.orderBreakdown';
+    expect(screen.getByRole('heading', { name: orderBreakdownTitle })).toBeInTheDocument();
+
+    // These are rendered twice (mobile & desktop)
+    expect(screen.getAllByText('OrderBreakdown.baseUnitQuantity')).toHaveLength(2);
+    expect(screen.queryAllByText('OrderBreakdown.shippingFee')).toHaveLength(0);
+    expect(screen.getAllByText('OrderBreakdown.total')).toHaveLength(2);
+
+    const getTextbox = name => screen.getByRole('textbox', { name });
+
+    const paymentHeading = 'StripePaymentForm.paymentHeading';
+    expect(screen.getByRole('heading', { name: paymentHeading })).toBeInTheDocument();
+
+    const billingDetails = 'StripePaymentForm.billingDetails';
+    expect(screen.getByRole('heading', { name: billingDetails })).toBeInTheDocument();
+    expect(getTextbox('StripePaymentForm.billingDetailsNameLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.addressLine1Label')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.addressLine2Label')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.postalCodeLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.cityLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentAddress.stateLabel')).toBeInTheDocument();
+    expect(getTextbox('StripePaymentForm.messageLabel')).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('button', { name: 'StripePaymentForm.submitPaymentInfo' })
+    ).toBeInTheDocument();
   });
 
   describe('Duck', () => {
