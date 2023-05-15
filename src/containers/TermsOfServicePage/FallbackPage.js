5f46bc70b2ba9318981ce4ea6492f63fb580ab4b
Sari Saariaho <sari@sharetribe.com>
2023-05-15 17:08:10 +0300

    Add code splitting to PageBuilder imports

--- src/containers/TermsOfServicePage/FallbackPage.js
+++ src/containers/TermsOfServicePage/FallbackPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import loadable from '@loadable/component';
 
-import PageBuilder from '../PageBuilder/PageBuilder';
+const PageBuilder = loadable(() => import ('../../containers/PageBuilder/PageBuilder'))
 
 // NOTE: You could add the actual Terms of Service here as a fallback
 //       instead of showing this error message.
