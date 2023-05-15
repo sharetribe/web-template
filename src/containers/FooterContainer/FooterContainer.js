880e38a2c8cb1d770d4d4f4a58e981df6ac3f0ae
Sari Saariaho <sari@sharetribe.com>
2023-05-15 15:37:01 +0300

    Use hosted footer in FooterContainer

--- src/containers/FooterContainer/FooterContainer.js
+++ src/containers/FooterContainer/FooterContainer.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import footerAsset from './footerasset.json';
 import { SectionBuilder } from '../PageBuilder/PageBuilder';
+import { useConfiguration } from '../../context/configurationContext';
 
-const FooterContainer = props => {
-  // TODO fetch footer asset from context
-  return <SectionBuilder sections={[footerAsset]} />;
+const FooterContainer = () => {
+  const { footer } = useConfiguration();
+
+  // The footer asset does not specify sectionId or sectionType. However, the SectionBuilder
+  // expects sectionId and sectionType in order to identify the section. I suggest that we
+  // add those attributes here before passing the asset to SectionBuilder.
+  const footerSection = {
+    ...footer,
+    sectionId: 'footer',
+    sectionType: 'footer',
+  };
+
+  return <SectionBuilder sections={[footerSection]} />;
 };
 
 export default FooterContainer;
+
