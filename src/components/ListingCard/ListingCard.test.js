7a84c9e99c85cff38f5e2c8966ef28a2e296fe6b
Vesa Luusua <vesa.luusua@gmail.com>
2023-08-02 19:44:44 +0300

    ListingCard: support listings without price

--- src/components/ListingCard/ListingCard.test.js
+++ src/components/ListingCard/ListingCard.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 
-import { renderWithProviders as render } from '../../util/testHelpers';
+import { getHostedConfiguration, renderWithProviders as render } from '../../util/testHelpers';
 import { createUser, createListing, fakeIntl } from '../../util/testData';
 
 import { ListingCardComponent } from './ListingCard';
+
+const getConfig = () => {
+  const hostedConfig = getHostedConfiguration();
+  return {
+    ...hostedConfig,
+    listingTypes: {
+      listingTypes: [
+        {
+          id: 'free-inquiry',
+          transactionProcess: {
+            name: 'default-inquiry',
+            alias: 'default-inquiry/release-1',
+          },
+          unitType: 'inquiry',
+          defaultListingFields: {
+            price: false,
+          },
+        },
+      ],
+    },
+  };
+};
 
 describe('ListingCard', () => {
   it('matches snapshot', () => {
@@ -12,6 +34,17 @@ describe('ListingCard', () => {
     // For now, we rely on snapshot-testing.
     const listing = createListing('listing1', {}, { author: createUser('user1') });
     const tree = render(<ListingCardComponent listing={listing} intl={fakeIntl} />);
+    expect(tree.asFragment().firstChild).toMatchSnapshot();
+  });
+
+  it('matches snapshot without price', () => {
+    const config = getConfig();
+    const listing = createListing(
+      'listing1',
+      { publicData: { listingType: 'free-inquiry' } },
+      { author: createUser('user1') }
+    );
+    const tree = render(<ListingCardComponent listing={listing} intl={fakeIntl} />, { config });
     expect(tree.asFragment().firstChild).toMatchSnapshot();
   });
 });
