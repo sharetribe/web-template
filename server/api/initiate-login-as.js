4eb98ea03015073f5bafe47bb734f1b68d8aaa04
Vesa Luusua <vesa.luusua@gmail.com>
2023-10-12 18:17:22 +0300

    Change Console URL

--- server/api/initiate-login-as.js
+++ server/api/initiate-login-as.js
@@ -2,8 +2,7 @@ const crypto = require('crypto');
 
 const CLIENT_ID = process.env.REACT_APP_SHARETRIBE_SDK_CLIENT_ID;
 const ROOT_URL = process.env.REACT_APP_MARKETPLACE_ROOT_URL;
-const CONSOLE_URL =
-  process.env.SERVER_SHARETRIBE_CONSOLE_URL || 'https://flex-console.sharetribe.com';
+const CONSOLE_URL = process.env.SERVER_SHARETRIBE_CONSOLE_URL || 'https://console.sharetribe.com';
 const USING_SSL = process.env.REACT_APP_SHARETRIBE_USING_SSL === 'true';
 
 // redirect_uri param used when initiating a login as authentication flow and
