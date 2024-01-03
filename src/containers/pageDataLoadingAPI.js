4f78fef234aaafffc6ef1cc96c069757ba655169
inepicone <inespicone@hotmail.com>
2024-01-03 15:45:27 -0300

    chau stripe 10

--- src/containers/pageDataLoadingAPI.js
+++ src/containers/pageDataLoadingAPI.js
@@ -11,6 +11,7 @@ import { loadData as EmailVerificationPageLoader } from './EmailVerificationPage
 import { loadData as InboxPageLoader } from './InboxPage/InboxPage.duck';
 import { loadData as ListingPageLoader } from './ListingPage/ListingPage.duck';
 import { loadData as ManageListingsPageLoader } from './ManageListingsPage/ManageListingsPage.duck';
+import { loadData as PaymentMethodsPageLoader } from './PaymentMethodsPage/PaymentMethodsPage.duck';
 import { loadData as PrivacyPolicyPageLoader } from './PrivacyPolicyPage/PrivacyPolicyPage.duck';
 import { loadData as ProfilePageLoader } from './ProfilePage/ProfilePage.duck';
 import { loadData as SearchPageLoader } from './SearchPage/SearchPage.duck';
