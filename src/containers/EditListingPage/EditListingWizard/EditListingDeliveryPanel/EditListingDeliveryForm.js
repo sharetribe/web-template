3eb135a9187958d397499828ab77624d426788a7
Vesa Luusua <vesa.luusua@gmail.com>
2024-01-09 17:26:12 +0200

    EditListingDeliveryPanel: refactor for the setup where only one method is allowed

--- src/containers/EditListingPage/EditListingWizard/EditListingDeliveryPanel/EditListingDeliveryForm.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingDeliveryPanel/EditListingDeliveryForm.js
@@ -8,6 +8,7 @@ import classNames from 'classnames';
 import appSettings from '../../../../config/settings';
 import { intlShape, injectIntl, FormattedMessage } from '../../../../util/reactIntl';
 import { propTypes } from '../../../../util/types';
+import { displayDeliveryPickup, displayDeliveryShipping } from '../../../../util/configHelpers';
 import {
   autocompleteSearchRequired,
   autocompletePlaceSelected,
@@ -45,6 +46,7 @@ export const EditListingDeliveryFormComponent = props => (
         intl,
         pristine,
         invalid,
+        listingTypeConfig,
         marketplaceCurrency,
         hasStockInUse,
         saveActionMsg,
@@ -68,8 +70,11 @@ export const EditListingDeliveryFormComponent = props => (
       pauseValidation(false);
       useEffect(() => resumeValidation(), [values]);
 
-      const shippingEnabled = values.deliveryOptions?.includes('shipping');
-      const pickupEnabled = values.deliveryOptions?.includes('pickup');
+      const displayShipping = displayDeliveryShipping(listingTypeConfig);
+      const displayPickup = displayDeliveryPickup(listingTypeConfig);
+      const displayMultipleDelivery = displayShipping && displayPickup;
+      const shippingEnabled = displayShipping && values.deliveryOptions?.includes('shipping');
+      const pickupEnabled = displayPickup && values.deliveryOptions?.includes('pickup');
 
       const addressRequiredMessage = intl.formatMessage({
         id: 'EditListingDeliveryForm.addressRequired',
@@ -93,18 +98,23 @@ export const EditListingDeliveryFormComponent = props => (
       const shippingLabel = intl.formatMessage({ id: 'EditListingDeliveryForm.shippingLabel' });
       const pickupLabel = intl.formatMessage({ id: 'EditListingDeliveryForm.pickupLabel' });
 
-      const pickupClasses = classNames(css.deliveryOption, !pickupEnabled ? css.disabled : null);
-      const shippingClasses = classNames(
-        css.deliveryOption,
-        !shippingEnabled ? css.disabled : null
-      );
+      const pickupClasses = classNames({
+        [css.deliveryOption]: displayMultipleDelivery,
+        [css.disabled]: !pickupEnabled,
+        [css.hidden]: !displayPickup,
+      });
+      const shippingClasses = classNames({
+        [css.deliveryOption]: displayMultipleDelivery,
+        [css.disabled]: !shippingEnabled,
+        [css.hidden]: !displayShipping,
+      });
       const currencyConfig = appSettings.getCurrencyFormatting(marketplaceCurrency);
 
       return (
         <Form className={classes} onSubmit={handleSubmit}>
           <FieldCheckbox
             id="pickup"
-            className={css.deliveryCheckbox}
+            className={classNames(css.deliveryCheckbox, { [css.hidden]: !displayMultipleDelivery })}
             name="deliveryOptions"
             label={pickupLabel}
             value="pickup"
@@ -171,9 +181,10 @@ export const EditListingDeliveryFormComponent = props => (
               disabled={!pickupEnabled}
             />
           </div>
+
           <FieldCheckbox
             id="shipping"
-            className={css.deliveryCheckbox}
+            className={classNames(css.deliveryCheckbox, { [css.hidden]: !displayMultipleDelivery })}
             name="deliveryOptions"
             label={shippingLabel}
             value="shipping"
