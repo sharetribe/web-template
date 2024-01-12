2d5ac856519945b2c239e989951d631dbf2fd279
inepicone <inespicone@hotmail.com>
2024-01-12 11:15:37 -0300

    creo

--- src/transactions/transaction.js
+++ src/transactions/transaction.js
@@ -3,9 +3,7 @@ import { ensureTransaction } from '../util/data';
 import * as purchaseProcess from './transactionProcessPurchase';
 import * as bookingProcess from './transactionProcessBooking';
 import * as inquiryProcess from './transactionProcessInquiry';
-//ine
-import * as instantProcess from './transactionProcessInstantBooking';
-// fin ine
+
 
 // Supported unit types
 // Note: These are passed to translations/microcopy in certain cases.
@@ -20,9 +18,7 @@ export const INQUIRY = 'inquiry';
 export const PURCHASE_PROCESS_NAME = 'default-purchase';
 export const BOOKING_PROCESS_NAME = 'default-booking';
 export const INQUIRY_PROCESS_NAME = 'default-inquiry';
-//ine
-export const INSTANT_PROCESS_NAME = 'biketribe-instant-booking';
-//fin ine
+
 
 /**
  * A process should export:
