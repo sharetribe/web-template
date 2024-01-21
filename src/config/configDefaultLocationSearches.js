03b4487d9b14f7ec4da4bf2c4a3c1eb963964260
alvaroamor <aamor3@alu.ucam.edu>
2024-01-21 23:09:43 +0100

    ds
    as

--- src/config/configDefaultLocationSearches.js
+++ src/config/configDefaultLocationSearches.js
@@ -26,13 +26,13 @@ const defaultLocations = [
       bounds: new LatLngBounds(new LatLng(60.53045, 22.38197), new LatLng(60.33361, 22.06644)),
      },
    },
-  // {
-  //   id: 'default-tampere',
-  //   predictionPlace: {
-  //     address: 'Tampere, Finland',
-  //     bounds: new LatLngBounds(new LatLng(61.83657, 24.11838), new LatLng(61.42728, 23.5422)),
-  //   },
-  // },
+   {
+     id: 'default-tampere',
+     predictionPlace: {
+       address: 'Tampere, Finland',
+       bounds: new LatLngBounds(new LatLng(61.83657, 24.11838), new LatLng(61.42728, 23.5422)),
+     },
+   },
   // {
   //   id: 'default-oulu',
   //   predictionPlace: {
