7bda521a1951bfb594f197a38a603ed64288d9c8
Ihor Huk <lsmredfilter@gmail.com>
2024-01-24 01:46:01 +0300

    updated logo

--- src/config/configBranding.js
+++ src/config/configBranding.js
@@ -1,5 +1,5 @@
-import logoImageDesktop from '../assets/89e8e49c-5a59-463b-8633-4290f17afe83.jpg';
-import logoImageMobile from '../assets/89e8e49c-5a59-463b-8633-4290f17afe83.jpg';
+import logoImageDesktop from '../assets/8ad209b0-aaf7-46f9-a094-70a25b65de9c.jpg';
+import logoImageMobile from '../assets/8ad209b0-aaf7-46f9-a094-70a25b65de9c.jpg';
 import brandImage from '../assets/CreMemories-background-1440.png';
 import facebookImage from '../assets/CreMemories-Facebook-1200x630.jpg';
 import twitterImage from '../assets/CreMemoriesTwitter-600x314.png';
