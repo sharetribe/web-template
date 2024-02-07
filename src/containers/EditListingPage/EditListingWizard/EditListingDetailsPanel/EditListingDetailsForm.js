62cdb4defcb7692b207a437f63e1ad11f13d56f9
Aleksi O'Connor <aleksi@sharetribe.com>
2024-02-07 17:51:20 +0200

    Add a new field that renders category fields in the editListingDetailForm

--- src/containers/EditListingPage/EditListingWizard/EditListingDetailsPanel/EditListingDetailsForm.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingDetailsPanel/EditListingDetailsForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { arrayOf, bool, func, shape, string } from 'prop-types';
 import { compose } from 'redux';
 import { Field, Form as FinalForm } from 'react-final-form';
@@ -121,6 +121,108 @@ const FieldSelectListingType = props => {
   );
 };
 
+// Finds the correct subcategory within the given categories array based on the provided categoryIdToFind.
+const findCategoryConfig = (categories, categoryIdToFind) => {
+  return categories?.find(category => category.id === categoryIdToFind);
+};
+
+/**
+ * Recursively render subcategory field inputs if there are subcategories available.
+ * This function calls itself with updated props to render nested category fields.
+ * The select field is used for choosing a category or subcategory.
+ */
+const CategoryField = props => {
+  const { currentCategoryOptions, level, values, prefix, handleCategoryChange, intl } = props;
+
+  const currentCategoryKey = `${prefix}${level}`;
+
+  const categoryConfig = findCategoryConfig(currentCategoryOptions, values[`${prefix}${level}`]);
+
+  return (
+    <>
+      {currentCategoryOptions ? (
+        <FieldSelect
+          key={currentCategoryKey}
+          id={currentCategoryKey}
+          name={currentCategoryKey}
+          className={css.listingTypeSelect}
+          onChange={event => handleCategoryChange(event, level, currentCategoryOptions)}
+          label={
+            level == 1
+              ? intl.formatMessage({ id: 'EditListingDetailsForm.categoryLabel' })
+              : intl.formatMessage({ id: 'EditListingDetailsForm.subCategoryLabel' })
+          }
+          validate={required(intl.formatMessage({ id: 'EditListingDetailsForm.categoryRequired' }))}
+        >
+          <option disabled value="">
+            {intl.formatMessage({ id: 'EditListingDetailsForm.categoryPlaceholder' })}
+          </option>
+
+          {currentCategoryOptions.map(option => (
+            <option key={option.id} value={option.id}>
+              {option.name}
+            </option>
+          ))}
+        </FieldSelect>
+      ) : null}
+
+      {categoryConfig?.subcategories?.length > 0 ? (
+        <CategoryField
+          currentCategoryOptions={categoryConfig.subcategories}
+          level={level + 1}
+          values={values}
+          prefix={prefix}
+          handleCategoryChange={handleCategoryChange}
+          intl={intl}
+        />
+      ) : null}
+    </>
+  );
+};
+
+const FieldSelectCategory = props => {
+  useEffect(() => {
+    checkIfInitialValuesExist();
+  }, []);
+
+  const { prefix, listingCategories, formApi, intl, setAllCategoriesChosen, values } = props;
+
+  // Counts the number of selected categories in the form values based on the given prefix.
+  const countSelectedCategories = () => {
+    return Object.keys(values).filter(key => key.startsWith(prefix)).length;
+  };
+
+  // Checks if initial values exist for categories and sets the state accordingly.
+  // If initial values exist, it sets `allCategoriesChosen` state to true; otherwise, it sets it to false
+  const checkIfInitialValuesExist = () => {
+    const count = countSelectedCategories(values, prefix);
+    setAllCategoriesChosen(count > 0);
+  };
+
+  // If a parent category changes, clear all child category values
+  const handleCategoryChange = (category, level, currentCategoryOptions) => {
+    const selectedCatLenght = countSelectedCategories();
+    if (level < selectedCatLenght) {
+      for (let i = selectedCatLenght; i > level; i--) {
+        formApi.change(`${prefix}${i}`, null);
+      }
+    }
+    const categoryConfig = findCategoryConfig(currentCategoryOptions, category).subcategories;
+    setAllCategoriesChosen(!categoryConfig || categoryConfig.length === 0);
+  };
+
+  return (
+    <CategoryField
+      currentCategoryOptions={listingCategories}
+      level={1}
+      values={values}
+      prefix={prefix}
+      handleCategoryChange={handleCategoryChange}
+      intl={intl}
+    />
+  );
+};
+
 // Add collect data for listing fields (both publicData and privateData) based on configuration
 const AddListingFields = props => {
   const { listingType, listingFieldsConfig, intl } = props;
@@ -171,6 +273,7 @@ const EditListingDetailsFormComponent = props => (
         invalid,
         pristine,
         selectableListingTypes,
+        selectableCategories,
         hasExistingListingType,
         saveActionMsg,
         updated,
@@ -181,6 +284,7 @@ const EditListingDetailsFormComponent = props => (
       } = formRenderProps;
 
       const { listingType } = values;
+      const [allCategoriesChosen, setAllCategoriesChosen] = useState(false);
 
       const titleRequiredMessage = intl.formatMessage({
         id: 'EditListingDetailsForm.titleRequired',
@@ -193,9 +297,12 @@ const EditListingDetailsFormComponent = props => (
       );
       const maxLength60Message = maxLength(maxLengthMessage, TITLE_MAX_LENGTH);
 
-      // Show title and description only after listing type is selected
-      const showTitle = listingType;
-      const showDescription = listingType;
+      const hasCategories = selectableCategories && selectableCategories.length > 0;
+      const showCategories = listingType && hasCategories;
+
+      const showTitle = hasCategories ? allCategoriesChosen : listingType;
+      const showDescription = hasCategories ? allCategoriesChosen : listingType;
+      const showListingFields = hasCategories ? allCategoriesChosen : listingType;
 
       const classes = classNames(css.root, className);
       const submitReady = (updated && pristine) || ready;
@@ -214,6 +321,18 @@ const EditListingDetailsFormComponent = props => (
             formApi={formApi}
             intl={intl}
           />
+
+          {showCategories ? (
+            <FieldSelectCategory
+              values={values}
+              prefix="categoryLevel"
+              listingCategories={selectableCategories}
+              formApi={formApi}
+              intl={intl}
+              allCategoriesChosen={allCategoriesChosen}
+              setAllCategoriesChosen={setAllCategoriesChosen}
+            />
+          ) : null}
 
           {showTitle ? (
             <FieldTextInput
@@ -247,11 +366,13 @@ const EditListingDetailsFormComponent = props => (
             />
           ) : null}
 
-          <AddListingFields
-            listingType={listingType}
-            listingFieldsConfig={listingFieldsConfig}
-            intl={intl}
-          />
+          {showListingFields ? (
+            <AddListingFields
+              listingType={listingType}
+              listingFieldsConfig={listingFieldsConfig}
+              intl={intl}
+            />
+          ) : null}
 
           <Button
             className={css.submitButton}
