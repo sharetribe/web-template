a3e6aab5172ac08441e50b39008632dd2cda552e
Patricio <patricio_cristaldo@outlook.com>
2024-02-23 10:41:12 -0300

    Se agrega botón de wsp a las vistas de -1024px

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -229,6 +229,13 @@ class TopbarComponent extends Component {
             {notificationDot}
           </Button>
           <LinkedLogo layout={'mobile'} alt={intl.formatMessage({ id: 'Topbar.logoIcon' })} />
+                        <a style={{width: '20%', height: '100%'}}>
+        <a style={{ height: '100%', display: 'flex', alignItems: 'center'}} href="https://wa.me/5492944232664" target="_blank" >
+        <p style={{color: "green",textAlign: "center", display: "flex",
+        color: "#07bc60",alignItems: "center"}}> 
+        <img src="/static/icons/whatsapp.png" alt="WhatsApp" style={{marginLeft: "10%", width: "36.5px", height: "35.5px"}}/></p>
+        </a>
+      </a>
           <Button
             rootClassName={css.searchMenu}
             onClick={this.handleMobileSearchOpen}
