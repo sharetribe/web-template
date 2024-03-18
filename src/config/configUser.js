af9f010854961f023834b3080c31c5c08ad55a2e
Sari Sharetribe <sari@sharetribe.com>
2024-03-18 10:55:42 +0200

    Update configUser.js comment

--- src/config/configUser.js
+++ src/config/configUser.js
@@ -1,3 +1,38 @@
+/////////////////////////////////////////////////////////
+// Configurations related to user.                     //
+/////////////////////////////////////////////////////////
+
+// Note: The userFields come from userFields asset nowadays by default.
+//       To use this built-in configuration, you need to change the overwrite from configHelper.js
+//       (E.g. use mergeDefaultTypesAndFieldsForDebugging func)
+
+/**
+ * Configuration options for user fields (custom extended data fields):
+ * - key:                           Unique key for the extended data field.
+ * - scope (optional):              Scope of the extended data can be either 'public', 'protected', or 'private'.
+ *                                  Default value: 'public'.
+ * - schemaType (optional):         Schema for this extended data field.
+ *                                  This is relevant when rendering components.
+ *                                  Possible values: 'enum', 'multi-enum', 'text', 'long', 'boolean'.
+ * - enumOptions (optional):        Options shown for 'enum' and 'multi-enum' extended data.
+ *                                  These are used to render options for inputs on
+ *                                  ProfileSettingsPage and AuthenticationPage.
+ * - showConfig:                    Configuration for rendering user information. (How the field should be shown.)
+ *   - label:                         Label for the saved data.
+ *   - displayInProfile               Can be used to hide field content (of type enum, boolean, or long) from profile page.
+ *                                    Default value: true,
+ * - saveConfig:                    Configuration for adding and modifying extended data fields.
+ *   - label:                         Label for the input field.
+ *   - placeholderMessage (optional): Default message for user input.
+ *   - isRequired (optional):         Is the field required for users to fill
+ *   - requiredMessage (optional):    Message for mandatory fields.
+ * - userTypeConfig:                Configuration for limiting user field to specific user types.
+ *   - limitToUserTypeIds:            Can be used to determine whether to limit the field to certain user types. The
+ *                                    Console based asset configurations do not yet support user types, so in hosted configurations
+ *                                    the default value for this is 'false'.
+ *   - userTypeIds:                   An array of user types for which the extended
+ *   (optional)                       data is relevant and should be added.
+ */
 export const userFields = [
   {
     key: 'cuisine',
@@ -78,6 +113,21 @@ export const userFields = [
     },
   },
   {
+    key: 'arrivalInstructions',
+    scope: 'protected',
+    schemaType: 'text',
+    saveConfig: {
+      label: 'How do people arrive at your kitchen?',
+      displayInSignUp: true, // key might change
+      required: true,
+    },
+    userTypeConfig: {
+      label: 'How do people arrive at your kitchen?',
+      limitToUserTypeIds: true,
+      userTypeIds: ['a', 'b', 'c'],
+    },
+  },
+  {
     key: 'dietaryPreferences',
     scope: 'public',
     schemaType: 'multi-enum',
@@ -106,6 +156,21 @@ export const userFields = [
     },
   },
 ];
+
+/////////////////////////////////////
+// Example user type configuration //
+/////////////////////////////////////
+/**
+ * User types are not supported in hosted configuration yet.
+ *
+ * To take user types into use in your
+ * custom code, you can do the following things:
+ * - Add a new user field with key 'userType', scope 'publicData', and schemaType enum
+ *  - Consider whether or not you want to allow your users to change their user type after first creating it
+ * - Set your user types as the available options for the userType field
+ * - Add your user types in the array below
+ * - Update configHelpers.js mergeUserConfig to pass user types to the validUserFields function
+ */
 
 export const userTypes = [
   {
