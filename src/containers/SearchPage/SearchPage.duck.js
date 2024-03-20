9a17e8eaf05791036c48a9b1a2c2045b371aa9bc
Vesa Luusua <vesa.luusua@gmail.com>
2024-03-20 14:56:20 +0200

    SearchPage.duck.js: omit invalid nested categoryLevel params Nested params could return bad data without clear indication why that happens.

--- src/containers/SearchPage/SearchPage.duck.js
+++ src/containers/SearchPage/SearchPage.duck.js
@@ -9,8 +9,9 @@ import {
   getStartOf,
 } from '../../util/dates';
 import { createImageVariantConfig } from '../../util/sdkLoader';
-import { isOriginInUse, isStockInUse } from '../../util/search';
+import { constructQueryParamName, isOriginInUse, isStockInUse } from '../../util/search';
 import { parse } from '../../util/urlHelpers';
+
 import { addMarketplaceEntities } from '../../ducks/marketplaceData.duck';
 
 // Pagination page size might need to be dynamic on responsive page layouts
@@ -117,6 +118,36 @@ export const searchListings = (searchParams, config) => (dispatch, getState, sdk
       : {};
   };
 
+  const omitInvalidCategoryParams = params => {
+    const categoryConfig = config.search.defaultFilters?.find(f => f.schemaType === 'category');
+    const categories = config.categories;
+    const { key: prefix, scope } = categoryConfig || {};
+    const categoryParamPrefix = constructQueryParamName(prefix, scope);
+
+    const validURLParamForCategoryData = (prefix, categories, level, params) => {
+      const levelKey = `${categoryParamPrefix}${level}`;
+      const levelValue = params?.[levelKey];
+      const foundCategory = categories.find(cat => cat.id === levelValue);
+      const subcategories = foundCategory?.subcategories || [];
+      return foundCategory && subcategories.length > 0
+        ? {
+            [levelKey]: levelValue,
+            ...validURLParamForCategoryData(prefix, subcategories, level + 1, params),
+          }
+        : foundCategory
+        ? { [levelKey]: levelValue }
+        : {};
+    };
+
+    const categoryKeys = validURLParamForCategoryData(prefix, categories, 1, params);
+    const nonCategoryKeys = Object.entries(params).reduce(
+      (picked, [k, v]) => (k.startsWith(categoryParamPrefix) ? picked : { ...picked, [k]: v }),
+      {}
+    );
+
+    return { ...nonCategoryKeys, ...categoryKeys };
+  };
+
   const priceSearchParams = priceParam => {
     const inSubunits = value => convertUnitToSubUnit(value, unitDivisor(config.currency));
     const values = priceParam ? priceParam.split(',') : [];
@@ -197,14 +228,16 @@ export const searchListings = (searchParams, config) => (dispatch, getState, sdk
     return hasDatesFilterInUse ? {} : { minStock: 1, stockMode: 'match-undefined' };
   };
 
-  const { perPage, price, dates, sort, ...rest } = searchParams;
+  const { perPage, price, dates, sort, mapSearch, ...restOfParams } = searchParams;
   const priceMaybe = priceSearchParams(price);
   const datesMaybe = datesSearchParams(dates);
   const stockMaybe = stockFilters(datesMaybe);
   const sortMaybe = sort === config.search.sortConfig.relevanceKey ? {} : { sort };
 
   const params = {
-    ...rest,
+    // The rest of the params except invalid nested category-related params
+    // Note: invalid independent search params are still passed through
+    ...omitInvalidCategoryParams(restOfParams),
     ...priceMaybe,
     ...datesMaybe,
     ...stockMaybe,
