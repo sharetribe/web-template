4f493ad5a797b387cfe8d270cc8681aa1b569a00
Sari Sharetribe <sari@sharetribe.com>
2024-03-21 15:01:10 +0200

    Save custom fields from signup form

--- src/containers/AuthenticationPage/SignupForm/SignupForm.example.js
+++ src/containers/AuthenticationPage/SignupForm/SignupForm.example.js
@@ -3,10 +3,77 @@ import { fakeIntl } from '../../../util/testData';
 import TermsAndConditions from '../TermsAndConditions/TermsAndConditions';
 import SignupForm from './SignupForm';
 
+const userFields = [
+  {
+    key: 'enumField1',
+    scope: 'public',
+    schemaType: 'enum',
+    enumOptions: [
+      { option: 'o1', label: 'l1' },
+      { option: 'o2', label: 'l2' },
+      { option: 'o3', label: 'l3' },
+    ],
+    saveConfig: {
+      label: 'Enum Field 1',
+      displayInSignUp: true,
+      isRequired: false,
+    },
+    userTypeConfig: {
+      limitToUserTypeIds: false,
+    },
+  },
+  {
+    key: 'enumField2',
+    scope: 'public',
+    schemaType: 'enum',
+    enumOptions: [
+      { option: 'o1', label: 'l1' },
+      { option: 'o2', label: 'l2' },
+      { option: 'o3', label: 'l3' },
+    ],
+    saveConfig: {
+      label: 'Enum Field 2',
+      displayInSignUp: true,
+      isRequired: false,
+    },
+    userTypeConfig: {
+      limitToUserTypeIds: true,
+      userTypeIds: ['c', 'd'],
+    },
+  },
+  {
+    key: 'textField',
+    scope: 'private',
+    schemaType: 'text',
+    saveConfig: {
+      label: 'Text Field',
+      displayInSignUp: true,
+      isRequired: true,
+    },
+    userTypeConfig: {
+      limitToUserTypeIds: false,
+    },
+  },
+  {
+    key: 'booleanField',
+    scope: 'protected',
+    schemaType: 'boolean',
+    saveConfig: {
+      label: 'Boolean Field',
+      displayInSignUp: true,
+      isRequired: false,
+    },
+    userTypeConfig: {
+      limitToUserTypeIds: false,
+    },
+  },
+];
+
 export const Empty = {
   component: SignupForm,
   props: {
     formId: 'SignupFormExample',
+    userFields,
     onSubmit(values) {
       console.log('sign up with form values:', values);
     },
