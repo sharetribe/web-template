e2706f3d3c4c42a923919af133e3e2498b62f310
Vesa Luusua <vesa.luusua@gmail.com>
2024-03-28 10:32:16 +0200

    SearchPage variants: pick listing field filters if there is no category restriction

--- src/containers/SearchPage/SearchPageWithMap.js
+++ src/containers/SearchPage/SearchPageWithMap.js
@@ -35,6 +35,7 @@ import {
   validFilterParams,
   cleanSearchFromConflictingParams,
   createSearchResultSchema,
+  pickListingFieldFilters,
 } from './SearchPage.shared';
 
 import FilterComponent from './FilterComponent';
@@ -257,12 +258,18 @@ export class SearchPageComponent extends Component {
       config,
     } = this.props;
 
-    const { listingFields: listingFieldsConfig } = config?.listing || {};
+    const { listingFields } = config?.listing || {};
     const { defaultFilters: defaultFiltersConfig, sortConfig } = config?.search || {};
 
     const activeListingTypes = config?.listing?.listingTypes.map(config => config.listingType);
     const marketplaceCurrency = config.currency;
-    const listingCategories = config.categoryConfiguration.categories;
+    const categoryConfiguration = config.categoryConfiguration;
+    const listingCategories = categoryConfiguration.categories;
+    const listingFieldsConfig = pickListingFieldFilters({
+      listingFields,
+      locationSearch: location.search,
+      categoryConfiguration,
+    });
     const filterConfigs = {
       listingFieldsConfig,
       defaultFiltersConfig,
