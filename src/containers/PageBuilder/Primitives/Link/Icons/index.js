cbeae1c3049e9273459b2d8070a324d4584b6a46
Vesa Luusua <vesa.luusua@gmail.com>
2024-04-08 16:08:42 +0300

    Change Twitter brand to X

--- src/containers/PageBuilder/Primitives/Link/Icons/index.js
+++ src/containers/PageBuilder/Primitives/Link/Icons/index.js
@@ -3,5 +3,5 @@ export { instagramIcon } from './instagramIcon';
 export { linkedinIcon } from './linkedinIcon';
 export { pinterestIcon } from './pinterestIcon';
 export { tiktokIcon } from './tiktokIcon';
-export { twitterIcon } from './twitterIcon';
+export { xIcon } from './xIcon';
 export { youtubeIcon } from './youtubeIcon';
---
+++ src/containers/PageBuilder/Primitives/Link/Icons/xIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const xIcon = () => {
+  return (
+    <svg width="16.61" height="14.999" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12.641 0h2.453L9.709 6.131 16 14.448h-4.937L7.198 9.394l-4.426 5.054H.32l5.705-6.557L0 0h5.06l3.492 4.617L12.642 0Zm-.858 13.009h1.36L4.344 1.386h-1.46l8.898 11.623Z" />
+    </svg>
+  );
+};
