62ff51bf3c80ab7a3378220fa0a2dbc3fffb4e57
Vesa Luusua <vesa.luusua@gmail.com>
2024-04-25 18:26:20 +0300

    ContactDetailsForm: configure the input for phoneNumber

--- src/containers/ContactDetailsPage/ContactDetailsForm/ContactDetailsForm.js
+++ src/containers/ContactDetailsPage/ContactDetailsForm/ContactDetailsForm.js
@@ -27,6 +27,41 @@ import css from './ContactDetailsForm.module.css';
 
 const SHOW_EMAIL_SENT_TIMEOUT = 2000;
 
+const PhoneNumberMaybe = props => {
+  const { formId, userTypeConfig, intl } = props;
+
+  const isDisabled = userTypeConfig?.defaultUserFields?.phoneNumber === false;
+  if (isDisabled) {
+    return null;
+  }
+
+  const { required } = userTypeConfig?.phoneNumberSettings || {};
+  const isRequired = required === true;
+
+  const validateMaybe = isRequired
+    ? {
+        validate: validators.required(
+          intl.formatMessage({
+            id: 'ContactDetailsForm.phoneRequired',
+          })
+        ),
+      }
+    : {};
+
+  return (
+    <FieldPhoneNumberInput
+      className={css.phone}
+      name="phoneNumber"
+      id={formId ? `${formId}.phoneNumber` : 'phoneNumber'}
+      label={intl.formatMessage({ id: 'ContactDetailsForm.phoneLabel' })}
+      placeholder={intl.formatMessage({
+        id: 'ContactDetailsForm.phonePlaceholder',
+      })}
+      {...validateMaybe}
+    />
+  );
+};
+
 class ContactDetailsFormComponent extends Component {
   constructor(props) {
     super(props);
@@ -78,6 +113,7 @@ class ContactDetailsFormComponent extends Component {
             sendVerificationEmailInProgress,
             resetPasswordInProgress,
             values,
+            userTypeConfig,
           } = fieldRenderProps;
           const { email, phoneNumber } = values;
 
@@ -198,11 +234,6 @@ class ContactDetailsFormComponent extends Component {
           const phoneNumberChanged =
             currentPhoneNumber !== phoneNumber &&
             !(typeof currentPhoneNumber === 'undefined' && phoneNumber === '');
-
-          const phonePlaceholder = intl.formatMessage({
-            id: 'ContactDetailsForm.phonePlaceholder',
-          });
-          const phoneLabel = intl.formatMessage({ id: 'ContactDetailsForm.phoneLabel' });
 
           // password
           const passwordLabel = intl.formatMessage({
@@ -327,13 +358,8 @@ class ContactDetailsFormComponent extends Component {
                   customErrorText={emailTouched ? null : emailTakenErrorText}
                 />
                 {emailVerifiedInfo}
-                <FieldPhoneNumberInput
-                  className={css.phone}
-                  name="phoneNumber"
-                  id={formId ? `${formId}.phoneNumber` : 'phoneNumber'}
-                  label={phoneLabel}
-                  placeholder={phonePlaceholder}
-                />
+
+                <PhoneNumberMaybe formId={formId} userTypeConfig={userTypeConfig} intl={intl} />
               </div>
 
               <div className={confirmClasses}>
