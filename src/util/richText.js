d6231043574e01708f4f5c15c0f78bee7a58ae9b
Vesa Luusua <vesa.luusua@gmail.com>
2024-05-31 13:29:22 +0300

    richText.js: support parenthesis in the middle of the URL

--- src/util/richText.js
+++ src/util/richText.js
@@ -87,7 +87,7 @@ export const linkifyOrWrapLinkSplit = (word, key, options = {}) => {
   // https://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
 
   // eslint-disable-next-line no-useless-escape
-  const urlRegex = /(\bhttps?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  const urlRegex = /(\bhttps?:\/\/[-A-Z0-9+&@#\/%?=~_|\(\)!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
   if (word.match(urlRegex)) {
     // Split strings like "(http://www.example.com)" to ["(","http://www.example.com",")"]
     return word.split(urlRegex).map(w => {
