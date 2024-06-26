c76da24b8df09133ba09f48341042431c75f36ba
Vesa Luusua <vesa.luusua@gmail.com>
2024-06-26 16:18:27 +0300

    util/generators.js: add generator named timeSlotsPerDate

--- src/util/generators.test.js
+++ src/util/generators.test.js
@@ -8,6 +8,7 @@ import {
   generateMonths,
   exceptionFreeSlotsPerDate,
   availabilityPerDate,
+  timeSlotsPerDate,
 } from './generators';
 
 describe('generators and iterators', () => {
@@ -380,6 +381,70 @@ describe('generators and iterators', () => {
       });
 
       const dateInfoHashMap = availabilityPerDate(start, end, plan, exceptions);
+      expect(JSON.stringify(dateInfoHashMap)).toEqual(expectedResult);
+    });
+  });
+
+  describe('timeSlotsPerDate(start, end, timeSlots, timeZone, options)', () => {
+    it('should return dates until end date, and map entries, exceptions to them', () => {
+      const start = parseDateFromISO8601('2023-01-02', 'Europe/Helsinki'); // Monday
+      const end = parseDateFromISO8601('2023-01-05', 'Europe/Helsinki'); // Wednesday
+      const timeZone = 'Europe/Helsinki';
+      const options = { seats: 1 };
+
+      const timeSlots = [
+        {
+          attributes: {
+            start: parseDateFromISO8601('2023-01-02', 'Europe/Helsinki'),
+            end: parseDateFromISO8601('2023-01-03', 'Europe/Helsinki'),
+            seats: 1,
+          },
+        },
+        {
+          attributes: {
+            start: new Date('2023-01-03T08:00:00.000Z'), // 10 am in Europe/Helsinki
+            end: new Date('2023-01-03T11:00:00.000Z'), // 1 pm in Europe/Helsinki
+            seats: 1,
+          },
+        },
+      ];
+
+      // prettier-ignore
+      const expectedResult = JSON.stringify({
+        "2023-01-02": {
+          "id": "2023-01-02",
+          "timeSlots": [
+            {
+              "attributes": {
+                "start": "2023-01-01T22:00:00.000Z",
+                "end": "2023-01-02T22:00:00.000Z",
+                "seats": 1
+              }
+            }
+          ],
+          "hasAvailability": true
+        },
+        "2023-01-03": {
+          "id": "2023-01-03",
+          "timeSlots": [
+            {
+              "attributes": {
+                "start": "2023-01-03T08:00:00.000Z",
+                "end": "2023-01-03T11:00:00.000Z",
+                "seats": 1
+              }
+            }
+          ],
+          "hasAvailability": true
+        },
+        "2023-01-04": {
+          "id": "2023-01-04",
+          "timeSlots": [],
+          "hasAvailability": false
+        }
+      });
+
+      const dateInfoHashMap = timeSlotsPerDate(start, end, timeSlots, timeZone, options);
       expect(JSON.stringify(dateInfoHashMap)).toEqual(expectedResult);
     });
   });
