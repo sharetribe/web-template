ddcc5840d1eda5ec8bbae44f552aa17e0d370203
Patricio <patricio_cristaldo@outlook.com>
2024-07-09 09:57:07 -0300

    Se revierte que COMPLETE_START pase a ACCEPTED, vuelve a depender de DELIVERED (para que no se envíe el mail de complete-start sin haber aceptado la solicitud)

--- src/transactions/transactionProcessBooking.js
+++ src/transactions/transactionProcessBooking.js
@@ -137,8 +137,8 @@ export const graph = {
     [states.ACCEPTED]: {
       on: {
         [transitions.CANCEL]: states.CANCELED,
-        [transitions.COMPLETE_START]: states.ACCEPTED,
-        [transitions.COMPLETE]: states.ACCEPTED,
+        [transitions.COMPLETE_START]: states.DELIVERED,
+        [transitions.COMPLETE]: states.DELIVERED,
         [transitions.REVIEW]: states.DELIVERED,
       },
     },
