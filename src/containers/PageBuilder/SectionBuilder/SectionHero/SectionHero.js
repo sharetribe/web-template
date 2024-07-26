70b6c177fb65f30152b6cf26f85c64acf0d1e4e5
Patricio <patricio_cristaldo@outlook.com>
2024-07-26 14:35:09 -0300

    Subo evento compartido a los botones del Hero en Landing page (Quiero alquilar y Si no encontras...)

--- src/containers/PageBuilder/SectionBuilder/SectionHero/SectionHero.js
+++ src/containers/PageBuilder/SectionBuilder/SectionHero/SectionHero.js
@@ -30,16 +30,14 @@ const SectionHero = props => {
   const hasHeaderFields = hasDataInFields([title, description, callToAction], fieldOptions);
 
   // Define the event handler
-  const handleCtaButtonClick = () => {
+  const handleCtaButtonClick = listingId => {
+    console.log('BtnsLandingHero'); // Log para verificar
     if (typeof fbq !== 'undefined') {
-      fbq('track', 'BtnsHeroSection');
-      setTimeout(() => {
-        window.open('https://example.com', '_blank'); // Reemplaza con tu URL
-      }, 300);
+      fbq('track', 'BtnsLandingHero', { listing_id: listingId });
     } else {
       console.error('Meta Pixel no está definido');
     }
-  };
+  };;
 
   return (
     <SectionContainer
@@ -53,12 +51,16 @@ const SectionHero = props => {
         <header className={defaultClasses.sectionDetails}>
           <Field data={title} className={defaultClasses.title} options={fieldOptions} />
           <Field data={description} className={defaultClasses.description} options={fieldOptions} />
-          <Field 
-            data={callToAction} 
-            className={defaultClasses.ctaButton} 
-            options={fieldOptions}
-            onClick={handleCtaButtonClick} // Añade el evento onClick
-          />
+          <button   onClick={handleCtaButtonClick}
+            style={{ border: 'none', background: 'none', padding: 0, margin: 0, width: '100%' }}
+          >
+            <Field
+              data={callToAction}
+              className={defaultClasses.ctaButton}
+              options={fieldOptions}
+              onClick={handleCtaButtonClick} // Añade el evento onClick
+            />
+          </button>
         </header>
       ) : null}
     </SectionContainer>
