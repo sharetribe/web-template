9a6c60b3dc0884213ddca8c46e08d30873c67641
Patricio <patricio_cristaldo@outlook.com>
2024-08-02 09:59:00 -0300

    Agrego evento meta a los botones del carrousel (BtnsCarrousel)

--- src/containers/PageBuilder/SectionBuilder/SectionCarousel/SectionCarousel.js
+++ src/containers/PageBuilder/SectionBuilder/SectionCarousel/SectionCarousel.js
@@ -102,7 +102,14 @@ const SectionCarousel = props => {
       onSlideRight(e);
     }
   };
-
+  const handleCtaButtonClick = () => {
+    console.log('BtnsCarrousel'); // Log para verificar
+    if (typeof fbq !== 'undefined') {
+      fbq('track', 'BtnsCarrousel');
+    } else {
+      console.error('Meta Pixel no está definido');
+    }
+  };
   return (
     <SectionContainer
       id={sectionId}
@@ -115,7 +122,11 @@ const SectionCarousel = props => {
         <header className={defaultClasses.sectionDetails}>
           <Field data={title} className={defaultClasses.title} options={fieldOptions} />
           <Field data={description} className={defaultClasses.description} options={fieldOptions} />
+          <button onClick={handleCtaButtonClick}
+            style={{ border: 'none', background: 'none', padding: 0, margin: 0, width: '100%' }}
+          >
           <Field data={callToAction} className={defaultClasses.ctaButtonCarrousel} options={fieldOptions} />
+          </button>
         </header>
       ) : null}
       {hasBlocks ? (
