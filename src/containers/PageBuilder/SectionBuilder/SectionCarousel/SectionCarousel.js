4f02cb925b8f3d280f904e957c53492d71623ecd
Patricio <patricio_cristaldo@outlook.com>
2024-08-05 08:44:26 -0300

    Saco handles que no van en el carrousel

--- src/containers/PageBuilder/SectionBuilder/SectionCarousel/SectionCarousel.js
+++ src/containers/PageBuilder/SectionBuilder/SectionCarousel/SectionCarousel.js
@@ -120,14 +120,11 @@ const SectionCarousel = props => {
       options={fieldOptions}
     >
       {hasHeaderFields ? (
-        <header className={defaultClasses.sectionDetails} onClick={handleCtaButtonClick}>
+        <header className={defaultClasses.sectionDetails}>
           <Field data={title} className={defaultClasses.title} options={fieldOptions} />
           <Field data={description} className={defaultClasses.description} options={fieldOptions} />
-          <button onClick={handleCtaButtonClick}
-            style={{ border: 'none', background: 'none', padding: 0, margin: 0, width: '100%' }}
-          >
-          <Field data={callToAction} className={defaultClasses.ctaButtonCarrousel} options={fieldOptions} onClick={handleCtaButtonClick}/>
-          </button>
+          <Field data={callToAction} className={defaultClasses.ctaButtonCarrousel} options={fieldOptions}/>
+
         </header>
       ) : null}
       {hasBlocks ? (
