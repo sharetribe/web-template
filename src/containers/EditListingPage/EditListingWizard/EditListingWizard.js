c7cf8fb432ea9508dd95a4a0f2cddd91c01c50d2
Vesa Luusua <vesa.luusua@gmail.com>
2024-08-05 16:44:14 +0300

    Fix tests

--- src/containers/EditListingPage/EditListingWizard/EditListingWizard.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingWizard.js
@@ -642,7 +642,7 @@ class EditListingWizard extends Component {
                   disabled={formDisabled}
                   inProgress={payoutDetailsSaveInProgress}
                   ready={payoutDetailsSaved}
-                  currentUser={ensuredCurrentUser}
+                  currentUser={currentUser}
                   stripeBankAccountLastDigits={getBankAccountLast4Digits(stripeAccountData)}
                   savedCountry={savedCountry}
                   submitButtonText={intl.formatMessage({
