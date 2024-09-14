c66aab3d37df051261709b159ebb0b5065c2bba1
Marcus V. Ximenes <marcus@marcusvinicius.info>
2024-09-14 20:10:46 -0300

    Reorganize batch product listing components

--- src/containers/BatchEditListingPage/BatchEditListingWizard/BatchEditListingWizardTab.js
+++ src/containers/BatchEditListingPage/BatchEditListingWizard/BatchEditListingWizardTab.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import PropTypes, { arrayOf } from 'prop-types';
+import PropTypes from 'prop-types';
 
 // Import configs and util modules
 import {
   LISTING_PAGE_PARAM_TYPE_DRAFT,
   LISTING_PAGE_PARAM_TYPE_NEW,
-  LISTING_PAGE_PARAM_TYPES,
 } from '../../../util/urlHelpers';
-import { ensureListing } from '../../../util/data';
 import { createResourceLocatorString } from '../../../util/routes';
-import { propTypes } from '../../../util/types';
-
-// Import modules from this directory
 
 import css from './BatchEditListingWizardTab.module.css';
 import EditListingUploaderPanel from './EditListingUploaderPanel/EditListingUploaderPanel';
@@ -19,9 +14,6 @@ import { EditListingBatchProductDetails } from './EditListingBatchProductDetails
 
 export const UPLOAD = 'upload';
 export const PRODUCT_DETAILS = 'product-details';
-
-// EditListingWizardTab component supports these tabs
-export const SUPPORTED_TABS = [UPLOAD, PRODUCT_DETAILS];
 
 const pathParamsToNextTab = (params, tab, marketplaceTabs) => {
   const nextTabIndex = marketplaceTabs.findIndex(s => s === tab) + 1;
@@ -57,86 +49,28 @@ const redirectAfterDraftUpdate = (listingId, params, tab, marketplaceTabs, histo
 const BatchEditListingWizardTab = props => {
   const {
     tab,
-    marketplaceTabs,
     params,
     locationSearch,
     errors,
     fetchInProgress,
     newListingPublished,
-    handleCreateFlowTabScrolling,
-    handlePublishListing,
     history,
     images,
-    listing,
-    weeklyExceptionQueries,
-    monthlyExceptionQueries,
-    allExceptions,
-    onFetchExceptions,
-    onAddAvailabilityException,
-    onDeleteAvailabilityException,
-    onUpdateListing,
-    onCreateListingDraft,
+    listing = null,
     onImageUpload,
     onManageDisableScrolling,
-    onListingTypeChange,
     onRemoveImage,
-    updatedTab,
+    updatedTab = null,
     updateInProgress,
     tabSubmitButtonText,
     config,
     routeConfiguration,
-    currentUser,
+    uppy,
   } = props;
 
-  const { type } = params;
-  const isNewURI = type === LISTING_PAGE_PARAM_TYPE_NEW;
-  const isDraftURI = type === LISTING_PAGE_PARAM_TYPE_DRAFT;
-  const isNewListingFlow = isNewURI || isDraftURI;
-
-  const currentListing = ensureListing(listing);
-
-  // New listing flow has automatic redirects to new tab on the wizard
-  // and the last panel calls publishListing API endpoint.
-  const automaticRedirectsForNewListingFlow = (tab, listingId) => {
-    if (tab !== marketplaceTabs[marketplaceTabs.length - 1]) {
-      // Create listing flow: smooth scrolling polyfill to scroll to correct tab
-      handleCreateFlowTabScrolling(false);
-
-      // After successful saving of draft data, user should be redirected to next tab
-      redirectAfterDraftUpdate(
-        listingId,
-        params,
-        tab,
-        marketplaceTabs,
-        history,
-        routeConfiguration
-      );
-    } else {
-      handlePublishListing(listingId);
-    }
-  };
 
   const onCompleteEditListingWizardTab = (tab, updateValues) => {
-    const onUpdateListingOrCreateListingDraft = isNewURI
-      ? (tab, values) => onCreateListingDraft(values, config)
-      : (tab, values) => onUpdateListing(tab, values, config);
-
-    const updateListingValues = isNewURI
-      ? updateValues
-      : { ...updateValues, id: currentListing.id };
-
-    return onUpdateListingOrCreateListingDraft(tab, updateListingValues)
-      .then(r => {
-        // In Availability tab, the submitted data (plan) is inside a modal
-        // We don't redirect provider immediately after plan is set
-        if (isNewListingFlow && tab !== AVAILABILITY) {
-          const listingId = r.data.data.id;
-          automaticRedirectsForNewListingFlow(tab, listingId);
-        }
-      })
-      .catch(e => {
-        // No need for extra actions
-      });
+    console.log('Save listings');
   };
 
   const panelProps = tab => {
@@ -158,7 +92,7 @@ const BatchEditListingWizardTab = props => {
         if (tab === UPLOAD) {
           const nextTab = { ...params, tab: PRODUCT_DETAILS };
           const to = createResourceLocatorString(
-            'EditListingPage',
+            'BatchEditListingPage',
             routeConfiguration,
             nextTab,
             {}
@@ -180,7 +114,7 @@ const BatchEditListingWizardTab = props => {
           images={images}
           onImageUpload={onImageUpload}
           onRemoveImage={onRemoveImage}
-          currentUser={currentUser}
+          uppy={uppy}
         />
       );
     }
@@ -198,54 +132,5 @@ BatchEditListingWizardTab.defaultProps = {
 };
 
 const { array, bool, func, object, oneOf, shape, string } = PropTypes;
-
-BatchEditListingWizardTab.propTypes = {
-  params: shape({
-    id: string.isRequired,
-    slug: string.isRequired,
-    type: oneOf(LISTING_PAGE_PARAM_TYPES).isRequired,
-    tab: oneOf(SUPPORTED_TABS).isRequired,
-  }).isRequired,
-  locationSearch: object,
-  errors: shape({
-    createListingDraftError: object,
-    publishListingError: object,
-    updateListingError: object,
-    showListingsError: object,
-    uploadImageError: object,
-  }).isRequired,
-  fetchInProgress: bool.isRequired,
-  newListingPublished: bool.isRequired,
-  history: shape({
-    push: func.isRequired,
-    replace: func.isRequired,
-  }).isRequired,
-  images: array.isRequired,
-
-  // We cannot use propTypes.listing since the listing might be a draft.
-  listing: shape({
-    attributes: shape({
-      publicData: object,
-      description: string,
-      geolocation: object,
-      pricing: object,
-      title: string,
-    }),
-    images: array,
-  }),
-
-  handleCreateFlowTabScrolling: func.isRequired,
-  handlePublishListing: func.isRequired,
-  onUpdateListing: func.isRequired,
-  onCreateListingDraft: func.isRequired,
-  onImageUpload: func.isRequired,
-  onRemoveImage: func.isRequired,
-  onListingTypeChange: func.isRequired,
-  updatedTab: string,
-  updateInProgress: bool.isRequired,
-  config: object.isRequired,
-  routeConfiguration: arrayOf(propTypes.route).isRequired,
-  currentUser: propTypes.currentUser,
-};
 
 export default BatchEditListingWizardTab;
--- src/containers/BatchEditListingPage/BatchEditListingWizard/EditListingUploaderPanel/EditListingUploaderPanel.js
+++ src/containers/BatchEditListingPage/BatchEditListingWizard/EditListingUploaderPanel/EditListingUploaderPanel.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { array, bool, func, object, string } from 'prop-types';
 import classNames from 'classnames';
 import { FormattedMessage } from '../../../../util/reactIntl';
-import { propTypes } from '../../../../util/types';
 import { Button, H3 } from '../../../../components';
 import css from './EditListingUploaderPanel.module.css';
 import { Dashboard } from '@uppy/react';
@@ -18,34 +16,18 @@ const getInitialValues = params => {
 
 const EditListingUploaderPanel = props => {
   const {
-    className,
-    rootClassName,
-    ready,
-    listing,
-    onImageUpload,
+    className = null,
+    rootClassName = null,
     submitButtonText,
-    panelUpdated,
-    updateInProgress,
     onSubmit,
-    onRemoveImage,
-    listingImageConfig,
     submitReady,
-    currentUser,
+    uppy
   } = props;
 
   const rootClass = rootClassName || css.root;
   const classes = classNames(rootClass, className);
 
-  const { uuid: userId } = currentUser.id;
-  const uppy = useUppy({ userId });
 
-  uppy.on('complete', info => {
-    console.log('complete', info);
-  });
-
-  uppy.on('file-added', info => {
-    console.log('file-added', uppy.getFiles());
-  });
 
   return (
     <div className={classes}>
@@ -67,36 +49,6 @@ const EditListingUploaderPanel = props => {
       </Button>
     </div>
   );
-};
-
-EditListingUploaderPanel.defaultProps = {
-  className: null,
-  rootClassName: null,
-  errors: null,
-  images: [],
-  listing: null,
-  currentUser: null,
-};
-
-EditListingUploaderPanel.propTypes = {
-  className: string,
-  rootClassName: string,
-  errors: object,
-  disabled: bool.isRequired,
-  ready: bool.isRequired,
-  images: array,
-
-  // We cannot use propTypes.listing since the listing might be a draft.
-  listing: object,
-
-  onImageUpload: func.isRequired,
-  onSubmit: func.isRequired,
-  submitButtonText: string.isRequired,
-  panelUpdated: bool.isRequired,
-  updateInProgress: bool.isRequired,
-  onRemoveImage: func.isRequired,
-  listingImageConfig: object.isRequired,
-  currentUser: propTypes.currentUser,
 };
 
 export default EditListingUploaderPanel;
