7464027670a375f4c91cc048cdb23bf0fa8e8054
Quyen Le <165617740+quyen-le-jh@users.noreply.github.com>
2024-10-09 14:17:08 +0700

    Pre-fill currency by location in Signup Page

--- src/containers/AuthenticationPage/SignupForm/SignupForm.js
+++ src/containers/AuthenticationPage/SignupForm/SignupForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { bool, node, string } from 'prop-types';
 import { compose } from 'redux';
 import { Form as FinalForm } from 'react-final-form';
@@ -16,207 +16,239 @@ import FieldSelectUserType from '../FieldSelectUserType';
 import UserFieldDisplayName from '../UserFieldDisplayName';
 import UserFieldPhoneNumber from '../UserFieldPhoneNumber';
 
+import { getUserCurrency } from '../../../extensions/user-currency/helpers';
+
 import css from './SignupForm.module.css';
 
 const getSoleUserTypeMaybe = userTypes =>
   Array.isArray(userTypes) && userTypes.length === 1 ? userTypes[0].userType : null;
 
-const SignupFormComponent = props => (
-  <FinalForm
-    {...props}
-    mutators={{ ...arrayMutators }}
-    initialValues={{ userType: props.preselectedUserType || getSoleUserTypeMaybe(props.userTypes) }}
-    render={formRenderProps => {
-      const {
-        rootClassName,
-        className,
-        formId,
-        handleSubmit,
-        inProgress,
-        invalid,
-        intl,
-        termsAndConditions,
-        preselectedUserType,
-        userTypes,
-        userFields,
-        values,
-      } = formRenderProps;
+const SignupFormComponent = props => {
+  const [currency, setCurrency] = useState('');
 
-      const { userType } = values || {};
-
-      // email
-      const emailRequired = validators.required(
-        intl.formatMessage({
-          id: 'SignupForm.emailRequired',
-        })
-      );
-      const emailValid = validators.emailFormatValid(
-        intl.formatMessage({
-          id: 'SignupForm.emailInvalid',
-        })
-      );
-
-      // password
-      const passwordRequiredMessage = intl.formatMessage({
-        id: 'SignupForm.passwordRequired',
-      });
-      const passwordMinLengthMessage = intl.formatMessage(
-        {
-          id: 'SignupForm.passwordTooShort',
-        },
-        {
-          minLength: validators.PASSWORD_MIN_LENGTH,
+  useEffect(() => {
+    const fetchCurrency = async position => {
+      try {
+        const userCurrency = await getUserCurrency(position);
+        if (userCurrency) {
+          setCurrency(userCurrency);
         }
-      );
-      const passwordMaxLengthMessage = intl.formatMessage(
-        {
-          id: 'SignupForm.passwordTooLong',
-        },
-        {
-          maxLength: validators.PASSWORD_MAX_LENGTH,
-        }
-      );
-      const passwordMinLength = validators.minLength(
-        passwordMinLengthMessage,
-        validators.PASSWORD_MIN_LENGTH
-      );
-      const passwordMaxLength = validators.maxLength(
-        passwordMaxLengthMessage,
-        validators.PASSWORD_MAX_LENGTH
-      );
-      const passwordRequired = validators.requiredStringNoTrim(passwordRequiredMessage);
-      const passwordValidators = validators.composeValidators(
-        passwordRequired,
-        passwordMinLength,
-        passwordMaxLength
-      );
+      } catch (error) {
+        console.error('Error fetching user currency:', error);
+      }
+    };
 
-      // Custom user fields. Since user types are not supported here,
-      // only fields with no user type id limitation are selected.
-      const userFieldProps = getPropsForCustomUserFieldInputs(userFields, intl, userType);
+    const handleError = error => {
+      console.error(error);
+    };
 
-      const noUserTypes = !userType && !(userTypes?.length > 0);
-      const userTypeConfig = userTypes.find(config => config.userType === userType);
-      const showDefaultUserFields = userType || noUserTypes;
-      const showCustomUserFields = (userType || noUserTypes) && userFieldProps?.length > 0;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(fetchCurrency, handleError);
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  }, []);
 
-      const classes = classNames(rootClassName || css.root, className);
-      const submitInProgress = inProgress;
-      const submitDisabled = invalid || submitInProgress;
+  return (
+    <FinalForm
+      {...props}
+      mutators={{ ...arrayMutators }}
+      initialValues={{
+        userType: props.preselectedUserType || getSoleUserTypeMaybe(props.userTypes),
+        pub_userCurrency: currency,
+      }}
+      render={formRenderProps => {
+        const {
+          rootClassName,
+          className,
+          formId,
+          handleSubmit,
+          inProgress,
+          invalid,
+          intl,
+          termsAndConditions,
+          preselectedUserType,
+          userTypes,
+          userFields,
+          values,
+        } = formRenderProps;
 
-      return (
-        <Form className={classes} onSubmit={handleSubmit}>
-          <FieldSelectUserType
-            name="userType"
-            userTypes={userTypes}
-            hasExistingUserType={!!preselectedUserType}
-            intl={intl}
-          />
+        const { userType } = values || {};
 
-          {showDefaultUserFields ? (
-            <div className={css.defaultUserFields}>
-              <FieldTextInput
-                type="email"
-                id={formId ? `${formId}.email` : 'email'}
-                name="email"
-                autoComplete="email"
-                label={intl.formatMessage({
-                  id: 'SignupForm.emailLabel',
-                })}
-                placeholder={intl.formatMessage({
-                  id: 'SignupForm.emailPlaceholder',
-                })}
-                validate={validators.composeValidators(emailRequired, emailValid)}
-              />
-              <div className={css.name}>
+        // email
+        const emailRequired = validators.required(
+          intl.formatMessage({
+            id: 'SignupForm.emailRequired',
+          })
+        );
+        const emailValid = validators.emailFormatValid(
+          intl.formatMessage({
+            id: 'SignupForm.emailInvalid',
+          })
+        );
+
+        // password
+        const passwordRequiredMessage = intl.formatMessage({
+          id: 'SignupForm.passwordRequired',
+        });
+        const passwordMinLengthMessage = intl.formatMessage(
+          {
+            id: 'SignupForm.passwordTooShort',
+          },
+          {
+            minLength: validators.PASSWORD_MIN_LENGTH,
+          }
+        );
+        const passwordMaxLengthMessage = intl.formatMessage(
+          {
+            id: 'SignupForm.passwordTooLong',
+          },
+          {
+            maxLength: validators.PASSWORD_MAX_LENGTH,
+          }
+        );
+        const passwordMinLength = validators.minLength(
+          passwordMinLengthMessage,
+          validators.PASSWORD_MIN_LENGTH
+        );
+        const passwordMaxLength = validators.maxLength(
+          passwordMaxLengthMessage,
+          validators.PASSWORD_MAX_LENGTH
+        );
+        const passwordRequired = validators.requiredStringNoTrim(passwordRequiredMessage);
+        const passwordValidators = validators.composeValidators(
+          passwordRequired,
+          passwordMinLength,
+          passwordMaxLength
+        );
+
+        // Custom user fields. Since user types are not supported here,
+        // only fields with no user type id limitation are selected.
+        const userFieldProps = getPropsForCustomUserFieldInputs(userFields, intl, userType);
+
+        const noUserTypes = !userType && !(userTypes?.length > 0);
+        const userTypeConfig = userTypes.find(config => config.userType === userType);
+        const showDefaultUserFields = userType || noUserTypes;
+        const showCustomUserFields = (userType || noUserTypes) && userFieldProps?.length > 0;
+
+        const classes = classNames(rootClassName || css.root, className);
+        const submitInProgress = inProgress;
+        const submitDisabled = invalid || submitInProgress;
+
+        return (
+          <Form className={classes} onSubmit={handleSubmit}>
+            <FieldSelectUserType
+              name="userType"
+              userTypes={userTypes}
+              hasExistingUserType={!!preselectedUserType}
+              intl={intl}
+            />
+
+            {showDefaultUserFields ? (
+              <div className={css.defaultUserFields}>
                 <FieldTextInput
-                  className={css.firstNameRoot}
-                  type="text"
-                  id={formId ? `${formId}.fname` : 'fname'}
-                  name="fname"
-                  autoComplete="given-name"
+                  type="email"
+                  id={formId ? `${formId}.email` : 'email'}
+                  name="email"
+                  autoComplete="email"
                   label={intl.formatMessage({
-                    id: 'SignupForm.firstNameLabel',
+                    id: 'SignupForm.emailLabel',
                   })}
                   placeholder={intl.formatMessage({
-                    id: 'SignupForm.firstNamePlaceholder',
+                    id: 'SignupForm.emailPlaceholder',
                   })}
-                  validate={validators.required(
-                    intl.formatMessage({
-                      id: 'SignupForm.firstNameRequired',
-                    })
-                  )}
+                  validate={validators.composeValidators(emailRequired, emailValid)}
                 />
+                <div className={css.name}>
+                  <FieldTextInput
+                    className={css.firstNameRoot}
+                    type="text"
+                    id={formId ? `${formId}.fname` : 'fname'}
+                    name="fname"
+                    autoComplete="given-name"
+                    label={intl.formatMessage({
+                      id: 'SignupForm.firstNameLabel',
+                    })}
+                    placeholder={intl.formatMessage({
+                      id: 'SignupForm.firstNamePlaceholder',
+                    })}
+                    validate={validators.required(
+                      intl.formatMessage({
+                        id: 'SignupForm.firstNameRequired',
+                      })
+                    )}
+                  />
+                  <FieldTextInput
+                    className={css.lastNameRoot}
+                    type="text"
+                    id={formId ? `${formId}.lname` : 'lname'}
+                    name="lname"
+                    autoComplete="family-name"
+                    label={intl.formatMessage({
+                      id: 'SignupForm.lastNameLabel',
+                    })}
+                    placeholder={intl.formatMessage({
+                      id: 'SignupForm.lastNamePlaceholder',
+                    })}
+                    validate={validators.required(
+                      intl.formatMessage({
+                        id: 'SignupForm.lastNameRequired',
+                      })
+                    )}
+                  />
+                </div>
+
+                <UserFieldDisplayName
+                  formName="SignupForm"
+                  className={css.row}
+                  userTypeConfig={userTypeConfig}
+                  intl={intl}
+                />
+
                 <FieldTextInput
-                  className={css.lastNameRoot}
-                  type="text"
-                  id={formId ? `${formId}.lname` : 'lname'}
-                  name="lname"
-                  autoComplete="family-name"
+                  className={css.password}
+                  type="password"
+                  id={formId ? `${formId}.password` : 'password'}
+                  name="password"
+                  autoComplete="new-password"
                   label={intl.formatMessage({
-                    id: 'SignupForm.lastNameLabel',
+                    id: 'SignupForm.passwordLabel',
                   })}
                   placeholder={intl.formatMessage({
-                    id: 'SignupForm.lastNamePlaceholder',
+                    id: 'SignupForm.passwordPlaceholder',
                   })}
-                  validate={validators.required(
-                    intl.formatMessage({
-                      id: 'SignupForm.lastNameRequired',
-                    })
-                  )}
+                  validate={passwordValidators}
+                />
+
+                <UserFieldPhoneNumber
+                  formName="SignupForm"
+                  className={css.row}
+                  userTypeConfig={userTypeConfig}
+                  intl={intl}
                 />
               </div>
+            ) : null}
 
-              <UserFieldDisplayName
-                formName="SignupForm"
-                className={css.row}
-                userTypeConfig={userTypeConfig}
-                intl={intl}
-              />
+            {showCustomUserFields ? (
+              <div className={css.customFields}>
+                {userFieldProps.map(fieldProps => (
+                  <CustomExtendedDataField {...fieldProps} formId={formId} />
+                ))}
+              </div>
+            ) : null}
 
-              <FieldTextInput
-                className={css.password}
-                type="password"
-                id={formId ? `${formId}.password` : 'password'}
-                name="password"
-                autoComplete="new-password"
-                label={intl.formatMessage({
-                  id: 'SignupForm.passwordLabel',
-                })}
-                placeholder={intl.formatMessage({
-                  id: 'SignupForm.passwordPlaceholder',
-                })}
-                validate={passwordValidators}
-              />
-
-              <UserFieldPhoneNumber
-                formName="SignupForm"
-                className={css.row}
-                userTypeConfig={userTypeConfig}
-                intl={intl}
-              />
+            <div className={css.bottomWrapper}>
+              {termsAndConditions}
+              <PrimaryButton type="submit" inProgress={submitInProgress} disabled={submitDisabled}>
+                <FormattedMessage id="SignupForm.signUp" />
+              </PrimaryButton>
             </div>
-          ) : null}
-
-          {showCustomUserFields ? (
-            <div className={css.customFields}>
-              {userFieldProps.map(fieldProps => (
-                <CustomExtendedDataField {...fieldProps} formId={formId} />
-              ))}
-            </div>
-          ) : null}
-
-          <div className={css.bottomWrapper}>
-            {termsAndConditions}
-            <PrimaryButton type="submit" inProgress={submitInProgress} disabled={submitDisabled}>
-              <FormattedMessage id="SignupForm.signUp" />
-            </PrimaryButton>
-          </div>
-        </Form>
-      );
-    }}
-  />
-);
+          </Form>
+        );
+      }}
+    />
+  );
+};
 
 SignupFormComponent.defaultProps = {
   rootClassName: null,
