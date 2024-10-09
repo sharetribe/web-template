c791c2a7fe5ab05f7fca29a02346f24ca385a8be
an-nguyen-jh <an.nguyen@journeyh.io>
2024-10-09 14:31:42 +0700

    [Feat] Show note based on display currency

--- src/extensions/MultipleCurrency/components/CurrencyNote/CurrencyNote.js
+++ src/extensions/MultipleCurrency/components/CurrencyNote/CurrencyNote.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useSelector } from 'react-redux';
+
+import { USD } from '../../../common/config/constants/currency.constants';
 
 import css from './CurrencyNote.module.css';
 
 const CurrencyNote = props => {
   const { componentId } = props;
+  const { currentUserShowInProgress } = useSelector(state => state.user);
+  const { uiCurrency } = useSelector(state => state.ui);
+  if (currentUserShowInProgress || uiCurrency === USD) {
+    return null;
+  }
+
   return (
     <div className={css.note}>
       <FormattedMessage id={`${componentId}.exchangeRateNote`} />
