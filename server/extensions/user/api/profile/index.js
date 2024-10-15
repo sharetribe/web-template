d4362b37a83f033c0bf006546df1b0bd8f22d15d
an-nguyen-jh <an.nguyen@journeyh.io>
2024-10-15 14:31:34 +0700

    [Feat] Change method to update user profile

---
+++ server/extensions/user/api/profile/index.js
@@ -0,0 +1,18 @@
+const { handleError } = require('../../../common/sdk');
+const { getSdk } = require('../../../common/sdk');
+const { finalizeTransitResponse } = require('../../../common/response');
+
+const updateProfile = async (req, res) => {
+  try {
+    const { data, queryParams } = req.body;
+    const sdk = getSdk(req, res);
+
+    const response = await sdk.currentUser.updateProfile(data, queryParams);
+    return finalizeTransitResponse(res)({ data: response.data });
+  } catch (error) {
+    console.error('Update user profile failed', error);
+    handleError(res, error);
+  }
+};
+
+module.exports = updateProfile;
--- src/extensions/common/config/constants/currency.constants.js
+++ src/extensions/common/config/constants/currency.constants.js
@@ -1,2 +1,5 @@
 export const DEFAULT_CURRENCY = 'USD';
-export const SUPPORT_CURRENCIES = ['USD', 'CAD'];
+
+export const SUPPORT_CURRENCIES = process.env.REACT_APP_CURRENCIES_SUPPORT
+  ? process.env.REACT_APP_CURRENCIES_SUPPORT.split(',')
+  : ['USD', 'CAD'];
