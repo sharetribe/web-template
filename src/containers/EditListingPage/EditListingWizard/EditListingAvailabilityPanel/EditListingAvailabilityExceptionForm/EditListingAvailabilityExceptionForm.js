a4d9730d655250a88a1c0e671f80655ac508dc0b
Vesa Luusua <vesa.luusua@gmail.com>
2024-11-14 17:21:28 +0200

    EditListingAvailabilityExceptionForm: improve JsDocs

--- src/containers/EditListingPage/EditListingWizard/EditListingAvailabilityPanel/EditListingAvailabilityExceptionForm/EditListingAvailabilityExceptionForm.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingAvailabilityPanel/EditListingAvailabilityExceptionForm/EditListingAvailabilityExceptionForm.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { array, arrayOf, bool, func, object, oneOf, shape, string } from 'prop-types';
-import { compose } from 'redux';
 import { Form as FinalForm } from 'react-final-form';
 import classNames from 'classnames';
 
-import { intlShape, injectIntl, FormattedMessage } from '../../../../../util/reactIntl';
-import { AVAILABILITY_MULTIPLE_SEATS, propTypes } from '../../../../../util/types';
+import { FormattedMessage, useIntl } from '../../../../../util/reactIntl';
+import { AVAILABILITY_MULTIPLE_SEATS } from '../../../../../util/types';
 import { DAY } from '../../../../../transactions/transaction';
 
 import { Form, H3, PrimaryButton } from '../../../../../components';
@@ -20,7 +18,46 @@ import css from './EditListingAvailabilityExceptionForm.module.css';
 //////////////////////////////////////////
 // EditListingAvailabilityExceptionForm //
 //////////////////////////////////////////
+
+/**
+ * @typedef {Object} AvailabilityException
+ * @property {string} id
+ * @property {'availabilityException'} type 'availabilityException'
+ * @property {Object} attributes            API entity's attributes
+ * @property {Date} attributes.start        The start of availability exception (inclusive)
+ * @property {Date} attributes.end          The end of availability exception (exclusive)
+ * @property {Number} attributes.seats      The number of seats available (0 means 'unavailable')
+ */
+/**
+ * @typedef {Object} MonthlyExceptionQueryInfo
+ * @property {Object?} fetchExceptionsError
+ * @property {boolean} fetchExceptionsInProgress
+ */
+
+/**
+ * A Form that allows the creation of AvailabilityExceptions.
+ *
+ * @component
+ * @param {Object} props
+ * @param {string?} props.className
+ * @param {string?} props.rootClassName
+ * @param {string?} props.formId
+ * @param {Object.<string, MonthlyExceptionQueryInfo>?} props.monthlyExceptionQueries E.g. '2022-12': { fetchExceptionsError, fetchExceptionsInProgress }
+ * @param {Array<AvailabilityException>} props.allExceptions
+ * @param {Function} props.onSubmit
+ * @param {Object} props.listingTypeConfig
+ * @param {'oneSeat'|'multipleSeats'} props.listingTypeConfig.availabilityType
+ * @param {'hour'|'day'|'night'} props.unitType
+ * @param {boolean} props.useFullDays derivative info based on unitType
+ * @param {string} props.timeZone IANA time zone key
+ * @param {boolean} props.updateInProgress
+ * @param {Object} props.fetchErrors
+ * @param {Object|null} props.fetchErrors.updateListingError
+ * @param {Function} props.onFetchExceptions Redux Thunk function to fetch AvailabilityExceptions
+ * @returns {JSX.Element} containing form that allows adding availability exceptions
+ */
 const EditListingAvailabilityExceptionForm = props => {
+  const intl = useIntl();
   return (
     <FinalForm
       {...props}
@@ -33,12 +70,11 @@ const EditListingAvailabilityExceptionForm = props => {
           listingId,
           disabled,
           handleSubmit,
-          intl,
           invalid,
           onMonthChanged,
           pristine,
           monthlyExceptionQueries,
-          allExceptions,
+          allExceptions = [],
           onFetchExceptions,
           useFullDays,
           listingTypeConfig,
@@ -62,7 +98,7 @@ const EditListingAvailabilityExceptionForm = props => {
           seats,
         } = values;
         const hasMultipleSeatsInUSe =
-          listingTypeConfig.availabilityType === AVAILABILITY_MULTIPLE_SEATS;
+          listingTypeConfig?.availabilityType === AVAILABILITY_MULTIPLE_SEATS;
         const hasSeats = hasMultipleSeatsInUSe ? seats != null : availability;
 
         const { updateListingError } = fetchErrors || {};
@@ -109,7 +145,6 @@ const EditListingAvailabilityExceptionForm = props => {
                   formId={formId}
                   listingId={listingId}
                   intl={intl}
-                  formApi={formApi}
                   allExceptions={allExceptions}
                   monthlyExceptionQueries={monthlyExceptionQueries}
                   onFetchExceptions={onFetchExceptions}
@@ -162,34 +197,4 @@ const EditListingAvailabilityExceptionForm = props => {
   );
 };
 
-EditListingAvailabilityExceptionForm.defaultProps = {
-  className: null,
-  rootClassName: null,
-  fetchErrors: null,
-  formId: null,
-  monthlyExceptionQueries: null,
-  allExceptions: [],
-};
-
-EditListingAvailabilityExceptionForm.propTypes = {
-  className: string,
-  rootClassName: string,
-  formId: string,
-  monthlyExceptionQueries: object,
-  allExceptions: arrayOf(propTypes.availabilityException),
-  intl: intlShape.isRequired,
-  onSubmit: func.isRequired,
-  listingTypeConfig: shape({
-    availabilityType: oneOf(['oneSeat', 'multipleSeats']).isRequired,
-  }).isRequired,
-  unitType: string.isRequired,
-  useFullDays: bool.isRequired,
-  timeZone: string.isRequired,
-  updateInProgress: bool.isRequired,
-  fetchErrors: shape({
-    updateListingError: propTypes.error,
-  }),
-  onFetchExceptions: func.isRequired,
-};
-
-export default compose(injectIntl)(EditListingAvailabilityExceptionForm);
+export default EditListingAvailabilityExceptionForm;
