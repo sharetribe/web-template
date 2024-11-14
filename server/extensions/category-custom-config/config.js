3e857ba76f99c2a86cc152f173abb0eb51a27f48
minh-le-jh <minh.le@journeyh.io>
2024-11-14 11:37:02 +0700

    Update category custom config: only Location category has config now

--- server/extensions/category-custom-config/config.js
+++ server/extensions/category-custom-config/config.js
@@ -13,39 +13,9 @@ const CATEGORY_ID_SERVICES = 'services';
  **/
 const categoriesExtraConfig = {
   [CATEGORY_ID_LOCATION]: {
-    minimumPrice: 10000,
-    providerFlatFee: 1000,
-    providerCommissionPercentage: 10,
-    customerCommissionPercentage: 0,
-  },
-  [CATEGORY_ID_MACHINE]: {
-    minimumPrice: 10000,
-    providerFlatFee: 1500,
-    providerCommissionPercentage: 10,
-    customerCommissionPercentage: 5,
-  },
-  [CATEGORY_ID_LOCATION_MACHINE]: {
-    minimumPrice: 10000,
-    providerFlatFee: 1000,
-    providerCommissionPercentage: 10,
-    customerCommissionPercentage: 0,
-  },
-  [CATEGORY_ID_ATM_LOCATION]: {
-    minimumPrice: 10000,
-    providerFlatFee: 1000,
-    providerCommissionPercentage: 10,
-    customerCommissionPercentage: 0,
-  },
-  [CATEGORY_ID_PARTS]: {
-    minimumPrice: 10000,
-    providerFlatFee: 1000,
-    providerCommissionPercentage: 10,
-    customerCommissionPercentage: 0,
-  },
-  [CATEGORY_ID_SERVICES]: {
-    minimumPrice: 10000,
-    providerFlatFee: 1000,
-    providerCommissionPercentage: 10,
+    minimumPrice: 20000,
+    providerFlatFee: 10000,
+    providerCommissionPercentage: 3,
     customerCommissionPercentage: 0,
   },
 };
