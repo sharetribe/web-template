7395da8cf06a2a77578424c97e06a5ab31b9447a
Rafael Rodriguez <rafaerod89@gmail.com>
2024-11-15 17:29:14 +0100

    Secret manager integration test

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,7 @@
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 module.exports = {
   reactScriptsVersion: 'sharetribe-scripts',
   webpack: {
@@ -9,5 +11,11 @@ module.exports = {
         swDest: 'sw.js',
       }),
     ],
+  },
+  babel: {
+    plugins: [
+      // Enable React Refresh only in the development environment
+      isDevelopment && require.resolve('react-refresh/babel'),
+    ].filter(Boolean), // Filter out `false` values
   },
 };
