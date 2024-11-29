42534a0241c88ae99a763833c5b3b1fdf6409550
Rafael Rodriguez <rafaerod89@gmail.com>
2024-11-29 23:07:10 +0100

    ProfilePage changes v1

--- src/containers/ProfilePage/ProfilePage.js
+++ src/containers/ProfilePage/ProfilePage.js
@@ -2,18 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { bool, arrayOf, oneOfType } from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import classNames from 'classnames';
 
 import { useConfiguration } from '../../context/configurationContext';
-import { FormattedMessage, useIntl } from '../../util/reactIntl';
-import {
-  REVIEW_TYPE_OF_PROVIDER,
-  REVIEW_TYPE_OF_CUSTOMER,
-  SCHEMA_TYPE_MULTI_ENUM,
-  SCHEMA_TYPE_TEXT,
-  SCHEMA_TYPE_YOUTUBE,
-  propTypes,
-} from '../../util/types';
+import { useIntl } from '../../util/reactIntl';
+import { propTypes } from '../../util/types';
 import {
   NO_ACCESS_PAGE_USER_PENDING_APPROVAL,
   NO_ACCESS_PAGE_VIEW_LISTINGS,
@@ -25,252 +17,16 @@ import {
   isForbiddenError,
   isNotFoundError,
 } from '../../util/errors';
-import { pickCustomFieldProps } from '../../util/fieldHelpers';
-import { hasPermissionToViewData, isUserAuthorized } from '../../util/userHelpers';
-import { richText } from '../../util/richText';
+import { hasPermissionToViewData, isUserAuthorized, isCreativeSeller } from '../../util/userHelpers';
 
 import { isScrollingDisabled } from '../../ducks/ui.duck';
 import { getMarketplaceEntities } from '../../ducks/marketplaceData.duck';
-import {
-  Heading,
-  H2,
-  H4,
-  Page,
-  AvatarLarge,
-  NamedLink,
-  ListingCard,
-  Reviews,
-  ButtonTabNavHorizontal,
-  LayoutSideNavigation,
-  NamedRedirect,
-} from '../../components';
+import { Page, NamedRedirect } from '../../components';
 
-import TopbarContainer from '../../containers/TopbarContainer/TopbarContainer';
-import FooterContainer from '../../containers/FooterContainer/FooterContainer';
 import NotFoundPage from '../../containers/NotFoundPage/NotFoundPage';
 
-import css from './ProfilePage.module.css';
-import SectionDetailsMaybe from './SectionDetailsMaybe';
-import SectionTextMaybe from './SectionTextMaybe';
-import SectionMultiEnumMaybe from './SectionMultiEnumMaybe';
-import SectionYoutubeVideoMaybe from './SectionYoutubeVideoMaybe';
-
-const MAX_MOBILE_SCREEN_WIDTH = 768;
-const MIN_LENGTH_FOR_LONG_WORDS = 20;
-
-export const AsideContent = props => {
-  const { user, displayName, showLinkToProfileSettingsPage } = props;
-  return (
-    <div className={css.asideContent}>
-      <AvatarLarge className={css.avatar} user={user} disableProfileLink />
-      <H2 as="h1" className={css.mobileHeading}>
-        {displayName ? (
-          <FormattedMessage id="ProfilePage.mobileHeading" values={{ name: displayName }} />
-        ) : null}
-      </H2>
-      {showLinkToProfileSettingsPage ? (
-        <>
-          <NamedLink className={css.editLinkMobile} name="ProfileSettingsPage">
-            <FormattedMessage id="ProfilePage.editProfileLinkMobile" />
-          </NamedLink>
-          <NamedLink className={css.editLinkDesktop} name="ProfileSettingsPage">
-            <FormattedMessage id="ProfilePage.editProfileLinkDesktop" />
-          </NamedLink>
-        </>
-      ) : null}
-    </div>
-  );
-};
-
-export const ReviewsErrorMaybe = props => {
-  const { queryReviewsError } = props;
-  return queryReviewsError ? (
-    <p className={css.error}>
-      <FormattedMessage id="ProfilePage.loadingReviewsFailed" />
-    </p>
-  ) : null;
-};
-
-export const MobileReviews = props => {
-  const { reviews, queryReviewsError } = props;
-  const reviewsOfProvider = reviews.filter(r => r.attributes.type === REVIEW_TYPE_OF_PROVIDER);
-  const reviewsOfCustomer = reviews.filter(r => r.attributes.type === REVIEW_TYPE_OF_CUSTOMER);
-  return (
-    <div className={css.mobileReviews}>
-      <H4 as="h2" className={css.mobileReviewsTitle}>
-        <FormattedMessage
-          id="ProfilePage.reviewsFromMyCustomersTitle"
-          values={{ count: reviewsOfProvider.length }}
-        />
-      </H4>
-      <ReviewsErrorMaybe queryReviewsError={queryReviewsError} />
-      <Reviews reviews={reviewsOfProvider} />
-      <H4 as="h2" className={css.mobileReviewsTitle}>
-        <FormattedMessage
-          id="ProfilePage.reviewsAsACustomerTitle"
-          values={{ count: reviewsOfCustomer.length }}
-        />
-      </H4>
-      <ReviewsErrorMaybe queryReviewsError={queryReviewsError} />
-      <Reviews reviews={reviewsOfCustomer} />
-    </div>
-  );
-};
-
-export const DesktopReviews = props => {
-  const [showReviewsType, setShowReviewsType] = useState(REVIEW_TYPE_OF_PROVIDER);
-  const { reviews, queryReviewsError } = props;
-  const reviewsOfProvider = reviews.filter(r => r.attributes.type === REVIEW_TYPE_OF_PROVIDER);
-  const reviewsOfCustomer = reviews.filter(r => r.attributes.type === REVIEW_TYPE_OF_CUSTOMER);
-  const isReviewTypeProviderSelected = showReviewsType === REVIEW_TYPE_OF_PROVIDER;
-  const isReviewTypeCustomerSelected = showReviewsType === REVIEW_TYPE_OF_CUSTOMER;
-  const desktopReviewTabs = [
-    {
-      text: (
-        <Heading as="h3" rootClassName={css.desktopReviewsTitle}>
-          <FormattedMessage
-            id="ProfilePage.reviewsFromMyCustomersTitle"
-            values={{ count: reviewsOfProvider.length }}
-          />
-        </Heading>
-      ),
-      selected: isReviewTypeProviderSelected,
-      onClick: () => setShowReviewsType(REVIEW_TYPE_OF_PROVIDER),
-    },
-    {
-      text: (
-        <Heading as="h3" rootClassName={css.desktopReviewsTitle}>
-          <FormattedMessage
-            id="ProfilePage.reviewsAsACustomerTitle"
-            values={{ count: reviewsOfCustomer.length }}
-          />
-        </Heading>
-      ),
-      selected: isReviewTypeCustomerSelected,
-      onClick: () => setShowReviewsType(REVIEW_TYPE_OF_CUSTOMER),
-    },
-  ];
-
-  return (
-    <div className={css.desktopReviews}>
-      <div className={css.desktopReviewsWrapper}>
-        <ButtonTabNavHorizontal className={css.desktopReviewsTabNav} tabs={desktopReviewTabs} />
-
-        <ReviewsErrorMaybe queryReviewsError={queryReviewsError} />
-
-        {isReviewTypeProviderSelected ? (
-          <Reviews reviews={reviewsOfProvider} />
-        ) : (
-          <Reviews reviews={reviewsOfCustomer} />
-        )}
-      </div>
-    </div>
-  );
-};
-
-export const CustomUserFields = props => {
-  const { publicData, metadata, userFieldConfig } = props;
-
-  const shouldPickUserField = fieldConfig => fieldConfig?.showConfig?.displayInProfile !== false;
-  const propsForCustomFields =
-    pickCustomFieldProps(publicData, metadata, userFieldConfig, 'userType', shouldPickUserField) ||
-    [];
-
-  return (
-    <>
-      <SectionDetailsMaybe {...props} />
-      {propsForCustomFields.map(customFieldProps => {
-        const { schemaType, ...fieldProps } = customFieldProps;
-        return schemaType === SCHEMA_TYPE_MULTI_ENUM ? (
-          <SectionMultiEnumMaybe {...fieldProps} />
-        ) : schemaType === SCHEMA_TYPE_TEXT ? (
-          <SectionTextMaybe {...fieldProps} />
-        ) : schemaType === SCHEMA_TYPE_YOUTUBE ? (
-          <SectionYoutubeVideoMaybe {...fieldProps} />
-        ) : null;
-      })}
-    </>
-  );
-};
-
-export const MainContent = props => {
-  const {
-    userShowError,
-    bio,
-    displayName,
-    listings,
-    queryListingsError,
-    reviews,
-    queryReviewsError,
-    publicData,
-    metadata,
-    userFieldConfig,
-    intl,
-    hideReviews,
-  } = props;
-
-  const hasListings = listings.length > 0;
-  const hasMatchMedia = typeof window !== 'undefined' && window?.matchMedia;
-  const isMobileLayout = hasMatchMedia
-    ? window.matchMedia(`(max-width: ${MAX_MOBILE_SCREEN_WIDTH}px)`)?.matches
-    : true;
-
-  const hasBio = !!bio;
-  const bioWithLinks = richText(bio, {
-    linkify: true,
-    longWordMinLength: MIN_LENGTH_FOR_LONG_WORDS,
-    longWordClass: css.longWord,
-  });
-
-  const listingsContainerClasses = classNames(css.listingsContainer, {
-    [css.withBioMissingAbove]: !hasBio,
-  });
-
-  if (userShowError || queryListingsError) {
-    return (
-      <p className={css.error}>
-        <FormattedMessage id="ProfilePage.loadingDataFailed" />
-      </p>
-    );
-  }
-  return (
-    <div>
-      <H2 as="h1" className={css.desktopHeading}>
-        <FormattedMessage id="ProfilePage.desktopHeading" values={{ name: displayName }} />
-      </H2>
-      {hasBio ? <p className={css.bio}>{bioWithLinks}</p> : null}
-
-      {displayName ? (
-        <CustomUserFields
-          publicData={publicData}
-          metadata={metadata}
-          userFieldConfig={userFieldConfig}
-          intl={intl}
-        />
-      ) : null}
-
-      {hasListings ? (
-        <div className={listingsContainerClasses}>
-          <H4 as="h2" className={css.listingsTitle}>
-            <FormattedMessage id="ProfilePage.listingsTitle" values={{ count: listings.length }} />
-          </H4>
-          <ul className={css.listings}>
-            {listings.map(l => (
-              <li className={css.listing} key={l.id.uuid}>
-                <ListingCard listing={l} showAuthorInfo={false} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : null}
-      {hideReviews ? null : isMobileLayout ? (
-        <MobileReviews reviews={reviews} queryReviewsError={queryReviewsError} />
-      ) : (
-        <DesktopReviews reviews={reviews} queryReviewsError={queryReviewsError} />
-      )}
-    </div>
-  );
-};
+import BasicProfilePage from './BasicProfilePage';
+import SellerProfilePage from './SellerProfilePage';
 
 export const ProfilePageComponent = props => {
   const config = useConfiguration();
@@ -288,6 +44,10 @@ export const ProfilePageComponent = props => {
     useCurrentUser,
     userShowError,
     user,
+    reviews,
+    queryReviewsError,
+    listings,
+    queryListingsError,
     ...rest
   } = props;
   const isVariant = pathParams.variant?.length > 0;
@@ -307,8 +67,7 @@ export const ProfilePageComponent = props => {
 
   const isCurrentUser = currentUser?.id && currentUser?.id?.uuid === pathParams.id;
   const profileUser = useCurrentUser ? currentUser : user;
-  const { bio, displayName, publicData, metadata } = profileUser?.attributes?.profile || {};
-  const { userFields } = config.user;
+  const { displayName } = profileUser?.attributes?.profile || {};
   const isPrivateMarketplace = config.accessControl.marketplace.private === true;
   const isUnauthorizedUser = currentUser && !isUserAuthorized(currentUser);
   const isUnauthorizedOnPrivateMarketplace = isPrivateMarketplace && isUnauthorizedUser;
@@ -366,7 +125,11 @@ export const ProfilePageComponent = props => {
     // and the first pass on client-side should render the same UI.
     return null;
   }
+
   // This is rendering normal profile page (not preview for pending-approval)
+  const { publicData } = profileUser?.attributes?.profile || {};
+  const isSeller = isCreativeSeller(publicData?.userType);
+  const ProfilePageContent = isSeller ? SellerProfilePage : BasicProfilePage;
   return (
     <Page
       scrollingDisabled={scrollingDisabled}
@@ -377,30 +140,16 @@ export const ProfilePageComponent = props => {
         name: schemaTitle,
       }}
     >
-      <LayoutSideNavigation
-        sideNavClassName={css.aside}
-        topbar={<TopbarContainer />}
-        sideNav={
-          <AsideContent
-            user={profileUser}
-            showLinkToProfileSettingsPage={mounted && isCurrentUser}
-            displayName={displayName}
-          />
-        }
-        footer={<FooterContainer />}
-      >
-        <MainContent
-          bio={bio}
-          displayName={displayName}
-          userShowError={userShowError}
-          publicData={publicData}
-          metadata={metadata}
-          userFieldConfig={userFields}
-          hideReviews={hasNoViewingRightsOnPrivateMarketplace}
-          intl={intl}
-          {...rest}
-        />
-      </LayoutSideNavigation>
+      <ProfilePageContent
+        isCurrentUser={isCurrentUser}
+        profileUser={profileUser}
+        hideReviews={hasNoViewingRightsOnPrivateMarketplace}
+        reviews={reviews}
+        queryReviewsError={queryReviewsError}
+        userShowError={userShowError}
+        listings={listings}
+        queryListingsError={queryListingsError}
+      />
     </Page>
   );
 };
@@ -436,14 +185,13 @@ const mapStateToProps = state => {
     reviews,
     queryReviewsError,
   } = state.ProfilePage;
+  const listings = getMarketplaceEntities(state, userListingRefs);
   const userMatches = getMarketplaceEntities(state, [{ type: 'user', id: userId }]);
   const user = userMatches.length === 1 ? userMatches[0] : null;
-
   // Show currentUser's data if it's not approved yet
   const isCurrentUser = userId?.uuid === currentUser?.id?.uuid;
   const useCurrentUser =
     isCurrentUser && !(isUserAuthorized(currentUser) && hasPermissionToViewData(currentUser));
-
   return {
     scrollingDisabled: isScrollingDisabled(state),
     currentUser,
@@ -451,7 +199,7 @@ const mapStateToProps = state => {
     user,
     userShowError,
     queryListingsError,
-    listings: getMarketplaceEntities(state, userListingRefs),
+    listings,
     reviews,
     queryReviewsError,
   };
