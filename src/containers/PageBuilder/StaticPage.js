5729ec750eab10500c9a6ad1b3db0b6422c329dc
Vesa Luusua <vesa.luusua@gmail.com>
2024-12-10 16:08:17 +0200

    StaticPage: remove defaultProps (deprecated) and propTypes.

--- src/containers/PageBuilder/StaticPage.js
+++ src/containers/PageBuilder/StaticPage.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { node } from 'prop-types';
 import { connect } from 'react-redux';
 
 import { isScrollingDisabled } from '../../ducks/ui.duck.js';
 import { Page } from '../../components/index.js';
 
+/**
+ * This component returns a Page component which is connected to Redux store.
+ *
+ * @component
+ * @param {Object} props
+ * @param {JSX.Element} props.children
+ * @returns {JSX.Element} Page component with scrollingDisabled info from Redux state.
+ */
 const StaticPageComponent = props => {
   const { children, ...pageProps } = props;
   return <Page {...pageProps}>{children}</Page>;
-};
-
-StaticPageComponent.defaultProps = {
-  children: null,
-};
-
-StaticPageComponent.propTypes = {
-  children: node,
 };
 
 const mapStateToProps = state => {
