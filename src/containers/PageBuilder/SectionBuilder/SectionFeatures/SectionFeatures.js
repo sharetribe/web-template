4934fd688288ca86c5b16a664e92fcdfd0d25c67
Vesa Luusua <vesa.luusua@gmail.com>
2024-12-10 19:42:21 +0200

    SectionFeatures: remove defaultProps (deprecated) and propTypes.

--- src/containers/PageBuilder/SectionBuilder/SectionFeatures/SectionFeatures.js
+++ src/containers/PageBuilder/SectionBuilder/SectionFeatures/SectionFeatures.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { arrayOf, bool, func, node, object, shape, string } from 'prop-types';
 import classNames from 'classnames';
 
 import Field, { hasDataInFields } from '../../Field';
@@ -9,11 +8,47 @@ import SectionContainer from '../SectionContainer';
 
 import css from './SectionFeatures.module.css';
 
-// Section component that shows features.
-// Block content are shown in a row-like way:
-// [image] text
-// text [image]
-// [image] text
+/**
+ * @typedef {Object} BlockConfig
+ * @property {string} blockId
+ * @property {string} blockName
+ * @property {'defaultBlock' | 'footerBlock' | 'socialMediaLink'} blockType
+ */
+
+/**
+ * @typedef {Object} FieldComponentConfig
+ * @property {ReactNode} component
+ * @property {Function} pickValidProps
+ */
+
+/**
+ * Section component that shows features.
+ * Block content are shown in a row-like way:
+ * [image] text
+ * text [image]
+ * [image] text
+ *
+ * @component
+ * @param {Object} props
+ * @param {string?} props.className add more style rules in addition to components own css.root
+ * @param {string?} props.rootClassName overwrite components own css.root
+ * @param {Object} props.defaultClasses
+ * @param {string} props.defaultClasses.sectionDetails
+ * @param {string} props.defaultClasses.title
+ * @param {string} props.defaultClasses.description
+ * @param {string} props.defaultClasses.ctaButton
+ * @param {string} props.sectionId id of the section
+ * @param {'features'} props.sectionType
+ * @param {Object?} props.title
+ * @param {Object?} props.description
+ * @param {Object?} props.appearance
+ * @param {Object?} props.callToAction
+ * @param {Array<BlockConfig>?} props.blocks array of block configs
+ * @param {boolean?} props.isInsideContainer
+ * @param {Object} props.options extra options for the section component (e.g. custom fieldComponents)
+ * @param {Object<string,FieldComponentConfig>?} props.options.fieldComponents custom fields
+ * @returns {JSX.Element} Section for article content
+ */
 const SectionFeatures = props => {
   const {
     sectionId,
@@ -24,8 +59,8 @@ const SectionFeatures = props => {
     description,
     appearance,
     callToAction,
-    blocks,
-    isInsideContainer,
+    blocks = [],
+    isInsideContainer = false,
     options,
   } = props;
 
@@ -70,43 +105,6 @@ const SectionFeatures = props => {
       ) : null}
     </SectionContainer>
   );
-};
-
-const propTypeOption = shape({
-  fieldComponents: shape({ component: node, pickValidProps: func }),
-});
-
-SectionFeatures.defaultProps = {
-  className: null,
-  rootClassName: null,
-  defaultClasses: null,
-  textClassName: null,
-  title: null,
-  description: null,
-  appearance: null,
-  callToAction: null,
-  blocks: [],
-  isInsideContainer: false,
-  options: null,
-};
-
-SectionFeatures.propTypes = {
-  sectionId: string.isRequired,
-  className: string,
-  rootClassName: string,
-  defaultClasses: shape({
-    sectionDetails: string,
-    title: string,
-    description: string,
-    ctaButton: string,
-  }),
-  title: object,
-  description: object,
-  appearance: object,
-  callToAction: object,
-  blocks: arrayOf(object),
-  isInsideContainer: bool,
-  options: propTypeOption,
 };
 
 export default SectionFeatures;
