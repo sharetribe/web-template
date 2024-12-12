4d3a148fe273b0c5e4de0dfe34b488ac226118b4
Vesa Luusua <vesa.luusua@gmail.com>
2024-12-12 18:16:39 +0200

    components/NamedRedirect: remove defaultProps (deprecated) and propTypes.

--- src/components/NamedRedirect/NamedRedirect.js
+++ src/components/NamedRedirect/NamedRedirect.js
@@ -1,31 +1,27 @@
-/**
- * This component wraps React-Router's Redirect by providing name-based routing.
- * (Helps to narrow down the scope of possible format changes to routes.)
- */
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 
 import { useRouteConfiguration } from '../../context/routeConfigurationContext';
 import { pathByRouteName } from '../../util/routes';
 
+/**
+ * This component wraps React-Router's Redirect by providing name-based routing.
+ * (Helps to narrow down the scope of possible format changes to routes.)
+ *
+ * @component
+ * @param {Object} props
+ * @param {string} props.name Route's name
+ * @param {string?} props.search search params
+ * @param {Object?} props.state history's state
+ * @param {boolean?} props.push
+ * @param {Object} props.params Path params
+ * @returns {JSX.Element} redirect component to help navigation
+ */
 const NamedRedirect = props => {
   const routeConfiguration = useRouteConfiguration();
-  const { name, search, state, params, push } = props;
+  const { name, search, state = {}, params = {}, push = false } = props;
   const pathname = pathByRouteName(name, routeConfiguration, params);
   return <Redirect to={{ pathname, search, state }} push={push} />;
-};
-
-const { bool, object, string } = PropTypes;
-
-NamedRedirect.defaultProps = { search: '', state: {}, push: false, params: {} };
-
-NamedRedirect.propTypes = {
-  name: string.isRequired,
-  search: string,
-  state: object,
-  push: bool,
-  params: object,
 };
 
 export default NamedRedirect;
