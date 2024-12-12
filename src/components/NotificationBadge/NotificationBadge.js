d0ad922aa023ac198cac3cfd711948bc8f498113
Vesa Luusua <vesa.luusua@gmail.com>
2024-12-12 18:16:57 +0200

    components/NotificationBadge: remove defaultProps (deprecated) and propTypes.

--- src/components/NotificationBadge/NotificationBadge.js
+++ src/components/NotificationBadge/NotificationBadge.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import css from './NotificationBadge.module.css';
 
+/**
+ * Small badge/dot with number inside.
+ *
+ * @component
+ * @param {Object} props
+ * @param {string?} props.className add more style rules in addition to component's own css.root
+ * @param {string?} props.rootClassName overwrite components own css.root
+ * @param {number} props.count the number of notifications
+ * @returns {JSX.Element} badge component that shows how many notifications should be shown.
+ */
 const NotificationBadge = props => {
   const { className, rootClassName, count } = props;
   const classes = classNames(rootClassName || css.root, className);
 
   return <span className={classes}>{count}</span>;
-};
-
-const { number, string } = PropTypes;
-
-NotificationBadge.defaultProps = {
-  className: null,
-  rootClassName: null,
-};
-
-NotificationBadge.propTypes = {
-  count: number.isRequired,
-  className: string,
-  rootClassName: string,
 };
 
 export default NotificationBadge;
