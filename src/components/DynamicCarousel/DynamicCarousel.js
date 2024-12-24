4ad25d4b13f39a76f15dc113fe2ad8b458e56636
lcorsini <ludovico.corsini@certilogo.com>
2024-12-24 11:53:44 +0100

    added affect

--- src/components/DynamicCarousel/DynamicCarousel.js
+++ src/components/DynamicCarousel/DynamicCarousel.js
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './DynamicCarousel.module.css';
 
 const DynamicCarousel = () => {
-  const images = [
-    'https://picsum.photos/300/400?random=1',
+  const [images, setImages] = useState([
+    'https://picsum.photos/100/200?random=1',
     'https://picsum.photos/200/300?random=2',
     'https://picsum.photos/300/400?random=3',
     'https://picsum.photos/200/300?random=4',
     'https://picsum.photos/100/200?random=5',
-  ];
+  ]);
+
+  const [hovering, setHovering] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (hovering) {
+      interval = setInterval(() => {
+        setImages((prevImages) => {
+          const newImages = [...prevImages];
+          const shiftedImage = newImages.shift();
+          newImages.push(shiftedImage);
+          return newImages;
+        });
+      }, 300); // Adjust the interval speed here (in milliseconds)
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [hovering]);
+
+  const handleMouseEnter = () => setHovering(true);
+  const handleMouseLeave = () => setHovering(false);
 
   return (
-    <div className={css.carousel}>
-      <div className={`${css.image} ${css.leftFar}`}>
-        <img src={images[4]} alt="Left Far" />
-      </div>
-      <div className={`${css.image} ${css.left}`}>
-        <img src={images[1]} alt="Left" />
-      </div>
-      <div className={`${css.image} ${css.center}`}>
-        <img src={images[2]} alt="Center" />
-      </div>
-      <div className={`${css.image} ${css.right}`}>
-        <img src={images[3]} alt="Right" />
-      </div>
-      <div className={`${css.image} ${css.rightFar}`}>
-        <img src={images[0]} alt="Right Far" />
-      </div>
+    <div
+      className={css.carousel}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {images.map((src, index) => (
+        <div
+          key={index}
+          className={`${css.image} ${
+            index === 0
+              ? css.leftFar
+              : index === 1
+              ? css.left
+              : index === 2
+              ? css.center
+              : index === 3
+              ? css.right
+              : css.rightFar
+          }`}
+        >
+          <img src={src} alt={`Image ${index + 1}`} />
+        </div>
+      ))}
     </div>
   );
 };
