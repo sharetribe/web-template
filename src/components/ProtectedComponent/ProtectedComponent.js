15d75a7a0dd9cfca35e1c3b4b9aa28afc4048ff8
lcorsini <ludovico.corsini@certilogo.com>
2025-01-07 13:32:05 +0100

    protect torino events

--- src/components/ProtectedComponent/ProtectedComponent.js
+++ src/components/ProtectedComponent/ProtectedComponent.js
@@ -8,15 +8,15 @@ export function ProtectedComponent({ children }) {
   const location = useLocation();
 
   const ALLOWED_PATHS = [
-    '/l/test-gratis/672e005a-aafd-4440-a476-49d5c82296f3',
-    '/l/672e005a-aafd-4440-a476-49d5c82296f3',
+    '/l/letter-making-stations-tag-torino/6773d00a-bcbc-4a69-919c-76b2b5d6cb08',
+    '/l/6773d00a-bcbc-4a69-919c-76b2b5d6cb08',
   ];
 
   function isAllowedPath(path) {
     return ALLOWED_PATHS.includes(path);
   }
 
-  const CORRECT_PASSWORD = '123';
+  const CORRECT_PASSWORD = 'TAGTORINO';
 
   const handlePasswordSubmit = (e) => {
     e.preventDefault();
@@ -32,17 +32,17 @@ export function ProtectedComponent({ children }) {
   if (isProtected && !isAuthorized) {
     return (
       <div className={styles.container}>
-        <h2 className={styles.title}>Ciao questo è un listing protetto, Inserisci la password che ti è stata fornita :)</h2>
+        <h2 className={styles.title}>Evento riservato ai community members del Talent Garden Torino. Registrati gratis con il codice fornito! :)</h2>
         <form onSubmit={handlePasswordSubmit} className={styles.form}>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter password"
+            placeholder="Password"
             className={styles.input}
           />
           <button type="submit" className={styles.button}>
-            Invio
+          Partecipa!
           </button>
         </form>
       </div>
