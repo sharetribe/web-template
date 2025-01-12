1b90e6e9f06652538e06824785551afbaca8efd9
lcorsini <ludovico.corsini@certilogo.com>
2025-01-12 18:05:09 +0100

    adjusted reviews

--- src/components/TrustPilot/TrustPilot.js
+++ src/components/TrustPilot/TrustPilot.js
@@ -7,11 +7,11 @@ import r3 from '../../media/landing/r3.JPG';
 const reviews = [
   {
     text: 'Siamo un team di 30 persone, con età e background diversissimi. Siamo arrivati al workshop un po’ titubanti—chi pensava di essere negato, chi aveva mille scadenze in testa. Ma bastava vedere il sorriso di ognuno mentre ci si sporcava le mani per capire che stavamo vivendo qualcosa di unico. Per qualche ora, non c’erano ruoli, generazioni o gerarchie. Solo mani impiastricciate e idee che volavano.',
-    image: r1,
+    image: r2,
   },
   {
     text: 'Abbiamo trascorso una giornata incredibile, ricca di creatività e collaborazione. Non importa da dove venivamo o cosa facevamo prima—ci siamo uniti in un’esperienza che ci ha lasciati ispirati e motivati.',
-    image: r2,
+    image: r1,
   },
   {
     text: 'L’energia del team era contagiosa! Per un giorno ci siamo lasciati alle spalle le distrazioni e abbiamo trovato una nuova connessione con i colleghi, scoprendo idee e risate che non immaginavamo.',
