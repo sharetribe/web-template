0969f9d757da1bbb44be61d73a440c0666247a47
lcorsini <ludovico.corsini@certilogo.com>
2025-01-14 17:16:06 +0100

    switched css

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
-import 'react-datepicker/dist/react-datepicker-cssmodules.css'
+import "react-datepicker/dist/react-datepicker.css";
 import { v4 as uuidv4 } from 'uuid';
 
 const randomId = () => uuidv4();
