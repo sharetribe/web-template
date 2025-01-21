505e4b5c1f38f2d0cc2c754e5f093cc991d0d27f
lcorsini <ludovico.corsini@certilogo.com>
2025-01-21 22:04:31 +0100

    new csp

--- server/csp.js
+++ server/csp.js
@@ -8,15 +8,8 @@ const data = 'data:';
 const blob = 'blob:';
 const devImagesMaybe = dev ? ['*.localhost:8000'] : [];
 const baseUrl = process.env.REACT_APP_SHARETRIBE_SDK_BASE_URL || 'https://flex-api.sharetribe.com';
-// Asset Delivery API is using a different domain than other Sharetribe APIs
-// cdn.st-api.com
-// If assetCdnBaseUrl is used to initialize SDK (for proxy purposes), then that URL needs to be in CSP
 const assetCdnBaseUrl = process.env.REACT_APP_SHARETRIBE_SDK_ASSET_CDN_BASE_URL;
 
-// Default CSP whitelist.
-//
-// NOTE: Do not change these in the customizations, make custom
-// additions within the exported function in the bottom of this file.
 const defaultDirectives = {
   baseUri: [self],
   defaultSrc: [self],
@@ -32,8 +25,6 @@ const defaultDirectives = {
     'events.mapbox.com',
 
     // Google Analytics
-    // TODO: Signals support needs more work
-    // https://developers.google.com/tag-platform/security/guides/csp
     '*.google-analytics.com',
     '*.analytics.google.com',
     '*.googletagmanager.com',
@@ -106,10 +97,36 @@ const defaultDirectives = {
     'www.googleadservices.com',
     '*.g.doubleclick.net',
     'js.stripe.com',
+    
     // Plausible analytics
     'plausible.io',
   ],
   styleSrc: [self, unsafeInline, 'fonts.googleapis.com', 'api.mapbox.com'],
+};
+
+// Custom CSP directives for Facebook and other required domains
+const { imgSrc = [self] } = defaultDirectives;
+const facebookImgSrc = imgSrc.concat([
+  '*.facebook.com',
+  '*.fbcdn.net',
+  '*.fbsbx.com',
+]);
+
+const { scriptSrc = [self] } = defaultDirectives;
+const facebookScriptSrc = scriptSrc.concat([
+  '*.facebook.net',
+  '*.facebook.com',
+]);
+
+const { frameSrc = [self] } = defaultDirectives;
+const facebookFrameSrc = frameSrc.concat([
+  '*.facebook.com',
+]);
+
+const customDirectives = {
+  imgSrc: facebookImgSrc,
+  scriptSrc: facebookScriptSrc,
+  frameSrc: facebookFrameSrc,
 };
 
 /**
@@ -122,34 +139,13 @@ const defaultDirectives = {
  * reported to the report URL instead of blocked
  */
 module.exports = (reportUri, reportOnly) => {
-  // ================ START CUSTOM CSP URLs ================ //
-
-  // Add custom CSP whitelisted URLs here. See commented example
-  // below. For format specs and examples, see:
-  // https://content-security-policy.com/
-
-  // Example: extend default img directive with custom domain
-  // const { imgSrc = [self] } = defaultDirectives;
-  // const exampleImgSrc = imgSrc.concat('my-custom-domain.example.com');
-
-  const customDirectives = {
-    // Example: Add custom directive override
-    // imgSrc: exampleImgSrc,
-  };
-
-  // ================ END CUSTOM CSP URLs ================ //
-
-  // Helmet v4 expects every value to be iterable so strings or booleans are not supported directly
-  // If we want to add block-all-mixed-content directive we need to add empty array to directives
-  // See Helmet's default directives:
-  // https://github.com/helmetjs/helmet/blob/bdb09348c17c78698b0c94f0f6cc6b3968cd43f9/middlewares/content-security-policy/index.ts#L51
-
+  // Merging default and custom directives
   const directives = Object.assign({ reportUri: [reportUri] }, defaultDirectives, customDirectives);
+
   if (!reportOnly) {
     directives.upgradeInsecureRequests = [];
   }
 
-  // See: https://helmetjs.github.io/docs/csp/
   return helmet.contentSecurityPolicy({
     useDefaults: false,
     directives,
