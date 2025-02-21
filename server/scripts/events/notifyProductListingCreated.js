1b3d4f3515e5fddcb093ea6a0a75a7655ff861dc
Rafael Rodriguez <rafaerod89@gmail.com>
2025-02-21 19:05:14 +0100

    Improved the metadata of the storage-manager

--- server/scripts/events/notifyProductListingCreated.js
+++ server/scripts/events/notifyProductListingCreated.js
@@ -17,62 +17,15 @@ const filterEvents = event => {
   const { resourceType, eventType, resourceId, resource } = event.attributes;
   if (resourceType !== RESOURCE_TYPE || eventType !== EVENT_TYPES) return false;
   const { attributes: listing, relationships } = resource;
-  const userId = relationships?.author?.data?.id?.uuid;
+  const authorId = relationships?.author?.data?.id?.uuid;
   const listingId = resourceId?.uuid;
   const originalAssetUrl = listing?.privateData?.originalAssetUrl;
   const previewAssetUrl = listing?.privateData?.previewAssetUrl;
   const isProductListing = listing?.publicData?.listingType === LISTING_TYPES.PRODUCT;
-  if (!originalAssetUrl || !previewAssetUrl || !userId || !listingId || !isProductListing) {
+  if (!originalAssetUrl || !previewAssetUrl || !authorId || !listingId || !isProductListing) {
     return false;
   }
   return true;
-};
-
-const storageHandler = async events => {
-  const storageManagerClient = new StorageManagerClient();
-  const data = events.map(event => {
-    const { resourceId, resource } = event.attributes;
-    const { attributes: listing, relationships } = resource;
-    const userId = relationships?.author?.data?.id?.uuid;
-    const listingId = resourceId?.uuid;
-    const originalAssetUrl = listing?.privateData?.originalAssetUrl;
-    return {
-      userId,
-      relationId: listingId,
-      tempSslUrl: originalAssetUrl,
-      metadata: {},
-    };
-  });
-  return await storageManagerClient.uploadOriginalAssets(data);
-};
-
-const processEvent = async (integrationSdk, event, originalAssetData) => {
-  const { resourceId, resource } = event.attributes;
-  const { attributes: listing } = resource;
-  const listingId = resourceId?.uuid;
-  const previewAssetUrl = listing?.privateData?.previewAssetUrl;
-  try {
-    const imageStream = await httpFileUrlToStream(previewAssetUrl);
-    const promiseFn = async () => {
-      const { data: sdkImage } = await integrationSdk.images.upload({ image: imageStream });
-      await integrationSdk.listings.update(
-        {
-          id: listingId,
-          privateData: { originalAssetUrl: originalAssetData.source, previewAssetUrl: null },
-          images: [sdkImage.data.id],
-        },
-        { expand: true, include: ['images'] }
-      );
-    };
-    await retryAsync(promiseFn, RETRIES, RETRY_SDK_DELAY);
-    return true;
-  } catch (error) {
-    console.error(
-      `[notifyProductListingCreated] Error processing event | listingId: ${listingId} | Error:`,
-      error
-    );
-    return false;
-  }
 };
 
 const analyzeEventGroup = events => {
@@ -90,9 +43,64 @@ const analyzeEventGroup = events => {
 
 function script() {
   const integrationSdk = integrationSdkInit();
+
+  async function getAuthor(authorId) {
+    const result = await integrationSdk.users.show({ id: authorId });
+    const { profile } = result?.data?.data?.attributes || {};
+    return profile;
+  }
+
+  const storageHandler = async events => {
+    const storageManagerClient = new StorageManagerClient();
+    const data = await Promise.all(
+      events.map(async event => {
+        const { resourceId, resource } = event.attributes;
+        const { attributes: listing, relationships } = resource;
+        const authorId = relationships?.author?.data?.id?.uuid;
+        const author = await getAuthor(authorId);
+        const creator = `${author.firstName} ${author.lastName}`;
+        const listingId = resourceId?.uuid;
+        const originalAssetUrl = listing?.privateData?.originalAssetUrl;
+        return {
+          userId: authorId,
+          relationId: listingId,
+          tempSslUrl: originalAssetUrl,
+          metadata: { creator },
+        };
+      })
+    );
+    return await storageManagerClient.uploadOriginalAssets(data);
+  };
+
   const queryEvents = args => integrationSdk.events.query({ ...args, eventTypes: EVENT_TYPES });
-  const analyzeEvent = (event, originalAssetData) =>
-    processEvent(integrationSdk, event, originalAssetData);
+  const analyzeEvent = async (event, originalAssetData) => {
+    const { resourceId, resource } = event.attributes;
+    const { attributes: listing } = resource;
+    const listingId = resourceId?.uuid;
+    const previewAssetUrl = listing?.privateData?.previewAssetUrl;
+    try {
+      const imageStream = await httpFileUrlToStream(previewAssetUrl);
+      const promiseFn = async () => {
+        const { data: sdkImage } = await integrationSdk.images.upload({ image: imageStream });
+        await integrationSdk.listings.update(
+          {
+            id: listingId,
+            privateData: { originalAssetUrl: originalAssetData.source, previewAssetUrl: null },
+            images: [sdkImage.data.id],
+          },
+          { expand: true, include: ['images'] }
+        );
+      };
+      await retryAsync(promiseFn, RETRIES, RETRY_SDK_DELAY);
+      return true;
+    } catch (error) {
+      console.error(
+        `[notifyProductListingCreated] Error processing event | listingId: ${listingId} | Error:`,
+        error
+      );
+      return false;
+    }
+  };
 
   const analyzeEventsBatch = async events => {
     let successList = [];
@@ -101,19 +109,17 @@ function script() {
     try {
       const promiseFn = async () => await storageHandler(parsedEvents);
       const originalAssets = await retryAsync(promiseFn, RETRIES, RETRY_STORAGE_DELAY);
-      await Promise.all(
-        parsedEvents.map(async event => {
-          const { resourceId } = event.attributes;
-          const listingId = resourceId?.uuid;
-          const originalAssetData = originalAssets.find(asset => asset.id === listingId);
-          const success = await analyzeEvent(event, originalAssetData);
-          if (success) {
-            successList.push(listingId);
-          } else {
-            failList.push(listingId);
-          }
-        })
-      );
+      for (const event of parsedEvents) {
+        const { resourceId } = event.attributes;
+        const listingId = resourceId?.uuid;
+        const originalAssetData = originalAssets.find(asset => asset.id === listingId);
+        const success = await analyzeEvent(event, originalAssetData);
+        if (success) {
+          successList.push(listingId);
+        } else {
+          failList.push(listingId);
+        }
+      }
       const withErrors = !!failList.length;
       if (withErrors) {
         slackProductListingsErrorWorkflow(failList);
