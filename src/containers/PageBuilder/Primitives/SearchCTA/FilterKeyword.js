da83d823834d6baabc2a041d195fd36655285d58
Aleksi O'Connor <aleksi@sharetribe.com>
2025-03-14 13:44:49 +0200

    add keyword filter

--- src/containers/PageBuilder/Primitives/SearchCTA/FilterKeyword.js
+++ src/containers/PageBuilder/Primitives/SearchCTA/FilterKeyword.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import classNames from 'classnames';
 import { FieldTextInput } from '../../../../components';
+import { useIntl } from '../../../../util/reactIntl';
 
-const FilterKeyword = props => (
-  <div className={props.className}>
-    <FieldTextInput name={'keywords'} type="text" placeholder="Search listings" />
-  </div>
-);
+import css from './FilterKeyword.module.css';
+
+const FilterKeyword = props => {
+  const intl = useIntl();
+  const { className, rootClassName } = props;
+  const rootClass = rootClassName || css.root;
+  const classes = classNames(rootClass, className);
+
+  return (
+    <div className={classes}>
+      <FieldTextInput
+        className={css.customField}
+        name={'keywords'}
+        type="text"
+        placeholder={intl.formatMessage({
+          id: 'SearchCTA.keywordFilterPlaceholder',
+        })}
+      />
+    </div>
+  );
+};
+
 export default FilterKeyword;
