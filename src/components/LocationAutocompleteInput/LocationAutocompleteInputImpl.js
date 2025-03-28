c0176fc8972a0358012b73f0844d9edc5518fed9
Aleksi O'Connor <aleksi@sharetribe.com>
2025-03-28 16:19:30 +0200

    removed support for legacy places api

--- src/components/LocationAutocompleteInput/LocationAutocompleteInputImpl.js
+++ src/components/LocationAutocompleteInput/LocationAutocompleteInputImpl.js
@@ -11,7 +11,6 @@ import IconHourGlass from './IconHourGlass';
 import IconCurrentLocation from './IconCurrentLocation';
 import * as geocoderMapbox from './GeocoderMapbox';
 import * as geocoderGoogleMaps from './GeocoderGoogleMaps';
-import * as geocoderGoogleMapsNew from './GeocoderGoogleMapsNew';
 
 import css from './LocationAutocompleteInput.module.css';
 
@@ -35,17 +34,7 @@ const getTouchCoordinates = nativeEvent => {
 // Get correct geocoding variant: geocoderGoogleMaps or geocoderMapbox
 const getGeocoderVariant = mapProvider => {
   const isGoogleMapsInUse = mapProvider === 'googleMaps';
-
-  // Determine if the new version of the Google Places API is enabled
-  const useNewGooglePlacesAPI =
-    typeof window !== 'undefined' &&
-    typeof window?.useNewGooglePlacesAPI !== 'undefined' &&
-    window?.useNewGooglePlacesAPI;
-  return isGoogleMapsInUse && useNewGooglePlacesAPI
-    ? geocoderGoogleMapsNew
-    : isGoogleMapsInUse
-    ? geocoderGoogleMaps
-    : geocoderMapbox;
+  return isGoogleMapsInUse ? geocoderGoogleMaps : geocoderMapbox;
 };
 
 // Renders the autocompletion prediction results in a list
@@ -172,32 +161,6 @@ class LocationAutocompleteInputImplementation extends Component {
 
   componentDidMount() {
     this._isMounted = true;
-
-    // Check whether the new version of the Google Places API is in use.
-    // The useNewGooglePlacesAPI property indicates whether the updated
-    // Google Places API is available and being utilized. If the useNewGooglePlacesAPI
-    // property is undefined, it means we haven't determined the API version yet.
-    const googleMapsStatus = typeof window?.useNewGooglePlacesAPI !== 'undefined';
-    // Development against localhost:3000 aka Webpack Dev Server has a bit random loading order.
-    const hasGoogle = window?.google;
-
-    // If the map provider configured in the application is Google Maps and we
-    // haven't yet determined the API version, we make a test API call
-    // to Google Maps Places Autocomplete to check its behavior.
-    // The fetchAutocompleteSuggestions function is only supported by
-    // the newer version of Google Places API.
-    if (this.props.config.maps.mapProvider === 'googleMaps' && hasGoogle && !googleMapsStatus) {
-      window?.google.maps.places.AutocompleteSuggestion.fetchAutocompleteSuggestions({
-        input: 'test',
-      })
-        .then(response => {
-          // A response means that the new Places API is enabled
-          window.useNewGooglePlacesAPI = true;
-        })
-        .catch(e => {
-          window.useNewGooglePlacesAPI = false;
-        });
-    }
   }
 
   componentWillUnmount() {
