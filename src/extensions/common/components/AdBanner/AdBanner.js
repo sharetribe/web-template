42c721ca969479d20e62c2f25f300da9c7b2ebb2
Yoav Schwartz <yostar@gmail.com>
2025-03-31 14:14:50 -0400

    fixed pr feedback

--- src/extensions/common/components/AdBanner/AdBanner.js
+++ src/extensions/common/components/AdBanner/AdBanner.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import adImage from '../../../../assets/ad-banner-nayax.avif';
 
 const AdBanner = ({ link }) => (
   <section style={{ textAlign: 'center', maxWidth: '800px', margin: '2rem auto' }}>
     <a href={`${link}`}>
       <img 
-        src="/static/assets/ad-banner-nayax.avif"
+        src={adImage} 
         alt={`Nayax Banner`} 
         style={{ maxWidth: '90%', height: 'auto' }}
       />
