9dc6b8a2f050c2c903e4a35a51517979486d0707
Rafael Rodriguez <rafaerod89@gmail.com>
2025-04-04 15:33:00 +0200

    Require Payout details before allowing a user to publish or edit listings

--- src/containers/BatchEditListingPage/BatchEditListingPage.js
+++ src/containers/BatchEditListingPage/BatchEditListingPage.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { compose } from 'redux';
 import { useParams, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { injectIntl } from '../../util/reactIntl';
+
+import { FormattedMessage, injectIntl } from '../../util/reactIntl';
 import {
   NO_ACCESS_PAGE_POST_LISTINGS,
   NO_ACCESS_PAGE_USER_PENDING_APPROVAL,
 } from '../../util/urlHelpers';
 import { hasPermissionToPostListings, isUserAuthorized } from '../../util/userHelpers';
-import { NamedRedirect, Page } from '../../components';
+import { NamedRedirect, NamedLink, Page, Heading, LayoutSingleColumn } from '../../components';
 import TopbarContainer from '../../containers/TopbarContainer/TopbarContainer';
+import FooterContainer from '../../containers/FooterContainer/FooterContainer';
 
-import css from './BatchEditListingPage.module.css';
 import { PAGE_MODE_NEW } from './constants';
 import BatchEditListingWizard from './BatchEditListingWizard/BatchEditListingWizard';
 import { ProductsListingEditMode } from './ProductListingsEditMode/ProductsListingEditMode';
 
+import css from './BatchEditListingPage.module.css';
+
 export const BatchEditListingPageComponent = props => {
   const { currentUser, history, intl, params, page } = props;
+  const noPayoutDetails = !currentUser?.attributes?.stripeConnected;
   const hasPostingRights = hasPermissionToPostListings(currentUser);
   const shouldRedirectNoPostingRights = !!currentUser?.id && !hasPostingRights;
   const { listingFieldsOptions } = page;
@@ -40,27 +44,50 @@ export const BatchEditListingPageComponent = props => {
     );
   }
 
+  const EditPageContent = isNew ? BatchEditListingWizard : ProductsListingEditMode;
+  const editPageContentProps = isNew
+    ? {
+        id: 'EditListingWizard',
+        className: css.wizard,
+        params,
+        history,
+        currentUser,
+        listingFieldsOptions,
+      }
+    : {};
+
   return (
     <Page
       title={intl.formatMessage({ id: 'EditListingPage.titleCreateListing' })}
       scrollingDisabled={false}
     >
-      <TopbarContainer
-        mobileRootClassName={css.mobileTopbar}
-        desktopClassName={css.desktopTopbar}
-        mobileClassName={css.mobileTopbar}
-      />
-      {isNew ? (
-        <BatchEditListingWizard
-          id="EditListingWizard"
-          className={css.wizard}
-          params={params}
-          history={history}
-          currentUser={currentUser}
-          listingFieldsOptions={listingFieldsOptions}
-        />
+      {noPayoutDetails ? (
+        <LayoutSingleColumn topbar={<TopbarContainer />} footer={<FooterContainer />}>
+          <div className={css.root}>
+            <div className={css.content}>
+              <Heading as="h1" rootClassName={css.heading}>
+                <FormattedMessage id="BatchEditListingPage.noPayoutDetails.heading" />
+              </Heading>
+              <p className={css.description}>
+                <FormattedMessage id="BatchEditListingPage.noPayoutDetails.description" />
+              </p>
+              <p className={css.description}>
+                <NamedLink name="StripePayoutPage">
+                  <FormattedMessage id="BatchEditListingPage.noPayoutDetails.link" />
+                </NamedLink>
+              </p>
+            </div>
+          </div>
+        </LayoutSingleColumn>
       ) : (
-        <ProductsListingEditMode></ProductsListingEditMode>
+        <>
+          <TopbarContainer
+            mobileRootClassName={css.mobileTopbar}
+            desktopClassName={css.desktopTopbar}
+            mobileClassName={css.mobileTopbar}
+          />
+          <EditPageContent {...editPageContentProps} />
+        </>
       )}
     </Page>
   );
@@ -68,7 +95,6 @@ export const BatchEditListingPageComponent = props => {
 
 const mapStateToProps = state => {
   const page = state.BatchEditListingPage;
-
   return {
     currentUser: state.user.currentUser,
     page,
