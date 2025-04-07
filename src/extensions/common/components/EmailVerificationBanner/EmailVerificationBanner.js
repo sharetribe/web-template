ae72ab07b71bf435c5905b95e4be8c331ec1f2a4
Yoav Schwartz <yostar@gmail.com>
2025-04-07 09:06:38 -0400

    pr comment fixes

--- src/extensions/common/components/EmailVerificationBanner/EmailVerificationBanner.js
+++ src/extensions/common/components/EmailVerificationBanner/EmailVerificationBanner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom'; // Assuming React Router is used
 import { SecondaryButton } from '../../../../components';
 import { MailCheck } from 'lucide-react';
@@ -6,24 +6,17 @@ import { FormattedMessage } from '../../../../util/reactIntl';
 import css from './EmailVerificationBanner.module.css';
 
 const EmailVerificationBanner = () => {
-  const [isVisible, setIsVisible] = useState(true);
   const history = useHistory();
   const location = useLocation();
 
-  useEffect(() => {
-    if (location.pathname === '/account/contact-details' || location.pathname === '/signup') {
-      setIsVisible(false);
-    }
-  }, [location.pathname]);
+  if (location.pathname === '/account/contact-details' || location.pathname === '/signup') {
+    return null;
+  }
 
   const handleVerifyClick = () => {
     history.push('/account/contact-details');
-    setIsVisible(false);
   };
 
-  if (!isVisible) {
-    return null;
-  }
 
   return (
     <section className={css.root}>
