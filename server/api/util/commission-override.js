af4b4196a88dc5520dfc1f9a0271082102014688
Jaydeep Patil <patiljaydeep688@gmail.com>
2025-04-29 16:18:49 -0700

    Adding custom listing commission functionality(try and error)

--- server/api/util/commission-override.js
+++ server/api/util/commission-override.js
@@ -1,4 +1,12 @@
 const extractOverridingProviderCommissionPercent = (authorJoinedListing, globalProviderCommission) => {
+  
+  const listingAttributes = authorJoinedListing.data.data?.attributes;
+  const listingPublicData = listingAttributes?.publicData;
+
+  console.log("Listing Attributes:", listingAttributes);
+  console.log("Listing PublicData:", listingPublicData);
+  console.log("Listing Type (publicData.listingType):", listingPublicData?.listingType);
+  
   let overrideProviderCommissionPercent =
     Array.isArray(authorJoinedListing.data.included) && authorJoinedListing.data.included[0]?.attributes?.profile?.metadata?.provider_commission_percentage != null
       ? authorJoinedListing.data.included[0].attributes.profile.metadata.provider_commission_percentage
