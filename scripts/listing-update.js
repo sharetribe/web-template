1de58ef2544d44b488950b6bfd7f7d844e6a1b0b
mathiscode <code@mathis.network>
2025-04-29 21:58:56 -0500

    update script to revert fixed to hourly listings

--- scripts/listing-update.js
+++ scripts/listing-update.js
@@ -3,10 +3,13 @@ const fs = require('fs')
 const { getISdk } = require('../server/api-util/sdk')
 const sdk = getISdk()
 
-inPersonHourlyToFixed()
+inPersonHourlyToFixed(false)
+inPersonAndOnlineInstructorProfileFixedToHourly(true)
 
-// Update all hourly listings to fixed
-async function inPersonHourlyToFixed() {
+// Update all in-person hourly listings to fixed
+async function inPersonHourlyToFixed(activate = false) {
+  if (!activate) return
+
   let allListings = []
   let currentPage = 1
   let totalPages = 1
@@ -55,4 +58,56 @@ async function inPersonHourlyToFixed() {
   }
 
   console.log('Finished processing hourly listings.')
+}
+
+// Update all in-person and online instructor profile (calendar booking) listings to hourly
+async function inPersonAndOnlineInstructorProfileFixedToHourly(activate = false) {
+  if (!activate) return
+
+  let allListings = []
+  let currentPage = 1
+  let totalPages = 1
+  const perPage = 100 // Max allowed by the SDK
+
+  console.log('Fetching listings...')
+
+  do {
+    try {
+      const res = await sdk.listings.query({ page: currentPage, perPage })
+      if (res?.statusText !== 'OK') throw new Error(`Failed to get listings on page ${currentPage}`)
+
+      const listings = res.data.data
+      const meta = res.data.meta
+      allListings = allListings.concat(listings)
+      totalPages = meta.totalPages
+
+      console.log(`Fetched page ${currentPage}/${totalPages}, got ${listings.length} listings. Total fetched: ${allListings.length}`)
+
+      currentPage++
+    } catch (error) {
+      console.error('Error fetching listings:', error)
+      break
+    }
+  } while (currentPage <= totalPages)
+
+  console.log(`Finished fetching. Total listings: ${allListings.length}`)
+
+  fs.writeFileSync('listings.backup.live.all.json', JSON.stringify(allListings, null, 2))
+
+  const inPersonAndInstructorProfile = allListings.filter(listing => listing.attributes.publicData.listingType === 'in-person' || listing.attributes.publicData.listingType === 'instruction_and_consultation')
+  console.log(`Found ${inPersonAndInstructorProfile.length} in-person and instructor profile listings out of ${allListings.length} total`)
+
+  const fixedListings = inPersonAndInstructorProfile.filter(listing => listing.attributes.publicData.unitType === 'fixed')
+  console.log(`Found ${fixedListings.length} fixed in-person and instructor profile listings`)
+
+  for (const listing of fixedListings) {
+    console.log(`Processing listing ${listing.attributes.title} (${listing.id.uuid})`)
+    sdk.listings.update({
+      id: listing.id,
+      publicData: {
+        ...listing.attributes.publicData,
+        unitType: 'hour'
+      }
+    })
+  }
 }
