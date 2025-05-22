11fcc707805d7b7a85f7fd824ac36243d7ded389
mathiscode <code@mathis.network>
2025-05-21 23:23:35 -0500

    improve eslint config; tweak to reduce noise; cleanup/lintfix

--- src/containers/PageBuilder/SectionBuilder/SectionFooter/SectionFooter.js
+++ src/containers/PageBuilder/SectionBuilder/SectionFooter/SectionFooter.js
@@ -142,9 +142,7 @@ const SectionFooter = props => {
               </div>
             ) : null}
             {/* SKYFARER +version */}
-            {APP_VERSION && process.env.NODE_ENV === 'development' && (
-              <span style={{ fontSize: '12px', color: 'gray', marginRight: '5px' }}>{`v${APP_VERSION || '?.?.?'}`}</span>
-            )}
+            <span style={{ fontSize: '12px', color: 'gray', marginRight: '5px' }}>{`v${process.env.REACT_APP_VERSION || 'DEV'}`}</span>
             <Field data={copyright} className={css.copyright} />
           </div>
           <div className={classNames(css.grid, getGridCss(numberOfColumns))}>
