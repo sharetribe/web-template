72bac9df19bc80a5088560579eaf4b2adb47d7df
Amanda Kauppinen <amanda@sharetribe.com>
2025-09-03 13:43:42 +0300

    IconArrowHead: Added new 'tiny' param for to handle sort dropdown arrows. Created example.

--- src/components/IconArrowHead/IconArrowHead.js
+++ src/components/IconArrowHead/IconArrowHead.js
@@ -9,6 +9,7 @@ const DIRECTION_DOWN = 'down';
 const DIRECTION_UP = 'up';
 const SIZE_BIG = 'big';
 const SIZE_SMALL = 'small';
+const SIZE_TINY = 'tiny';
 
 /**
  * Icon with arrow head pointing to given direction and with given size.
@@ -18,12 +19,15 @@ const SIZE_SMALL = 'small';
  * @param {string?} props.className add more style rules in addition to components own css.root
  * @param {string?} props.rootClassName overwrite components own css.root
  * @param {('right' | 'left' | 'down' | 'up')} props.direction
- * @param {('small' | 'big')} props.size
+ * @param {('small' | 'big' | 'tiny')} props.size
  * @returns {JSX.Element} arrow head icon
  */
 const IconArrowHead = props => {
   const { className, rootClassName, direction, size = SIZE_SMALL } = props;
-  const classes = classNames(rootClassName || css.root, className);
+  const classes = classNames(
+    rootClassName ? rootClassName : size === SIZE_TINY ? css.rootTinyArrow : css.root,
+    className
+  );
 
   const isRight = direction === DIRECTION_RIGHT;
   const isLeft = direction === DIRECTION_LEFT;
@@ -31,6 +35,7 @@ const IconArrowHead = props => {
   const isUp = direction === DIRECTION_UP;
   const isBig = size === SIZE_BIG;
   const isSmall = size === SIZE_SMALL;
+  const isTiny = size === SIZE_TINY;
 
   if (isRight && isSmall) {
     return (
@@ -157,6 +162,70 @@ const IconArrowHead = props => {
         <path
           d="M13.5 8.9c0 .17-.065.34-.192.458a.508.508 0 0 1-.764-.07L7 2.028 1.455 9.29a.506.506 0 0 1-.763.07.644.644 0 0 1-.064-.846l5.958-7.8A.524.524 0 0 1 7 .502c.16 0 .31.077.414.212l5.958 7.8c.086.113.128.25.128.388"
           fillRule="evenodd"
+        />
+      </svg>
+    );
+  } else if (isRight && isTiny) {
+    return (
+      <svg
+        className={classes}
+        width="5"
+        height="8"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="none"
+      >
+        <path
+          fillRule="evenodd"
+          d="M3.903 3.763c.13.131.13.342 0 .473L1.238 6.902a.333.333 0 1 1-.47-.47l2.427-2.433L.767 1.57a.333.333 0 0 1 .471-.471l2.665 2.665Z"
+        />
+      </svg>
+    );
+  } else if (isLeft && isTiny) {
+    return (
+      <svg
+        className={classes}
+        width="5"
+        height="8"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="none"
+      >
+        <path
+          fillRule="evenodd"
+          d="M1.097 3.763a.335.335 0 0 0 0 .473l2.665 2.666a.333.333 0 1 0 .47-.47L1.806 3.998l2.428-2.43a.333.333 0 0 0-.471-.471L1.097 3.763Z"
+        />
+      </svg>
+    );
+  } else if (isDown && isTiny) {
+    return (
+      <svg
+        className={classes}
+        width="8"
+        height="5"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="none"
+      >
+        <path
+          fillRule="evenodd"
+          d="M3.764 4.236c.131.13.341.13.472 0l2.666-2.667a.333.333 0 1 0-.471-.471L4 3.528l-2.43-2.43a.333.333 0 1 0-.471.471l2.665 2.667Z"
+        />
+      </svg>
+    );
+  } else if (isUp && isTiny) {
+    return (
+      <svg
+        className={classes}
+        width="8"
+        height="5"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="none"
+      >
+        <path
+          fillRule="evenodd"
+          d="M3.764 1.098a.334.334 0 0 1 .472 0l2.666 2.667a.333.333 0 1 1-.471.47L4 1.807l-2.43 2.43a.333.333 0 1 1-.471-.471l2.665-2.667Z"
         />
       </svg>
     );
