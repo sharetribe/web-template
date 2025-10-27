f13e4a1315a33d1887f8630cba6203deaf9c3ef9
Vesa Luusua <vesa.luusua@gmail.com>
2025-10-27 17:46:07 +0200

    PasswordRecoverPage.duck.js: update to use Redux Toolkit

--- src/containers/PasswordRecoveryPage/PasswordRecoveryPage.duck.js
+++ src/containers/PasswordRecoveryPage/PasswordRecoveryPage.duck.js
@@ -1,81 +1,65 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { storableError } from '../../util/errors';
 
-// ================ Action types ================ //
+// ================ Async Thunks ================ //
 
-export const RECOVERY_REQUEST = 'app/PasswordRecoveryPage/RECOVERY_REQUEST';
-export const RECOVERY_SUCCESS = 'app/PasswordRecoveryPage/RECOVERY_SUCCESS';
-export const RECOVERY_ERROR = 'app/PasswordRecoveryPage/RECOVERY_ERROR';
-export const RETYPE_EMAIL = 'app/PasswordRecoveryPage/RETYPE_EMAIL';
-export const CLEAR_RECOVERY_ERROR = 'app/PasswordRecoveryPage/CLEAR_RECOVERY_ERROR';
-
-// ================ Reducer ================ //
-
-const initialState = {
-  initialEmail: null,
-  submittedEmail: null,
-  recoveryError: null,
-  recoveryInProgress: false,
-  passwordRequested: false,
-};
-
-export default function reducer(state = initialState, action = {}) {
-  const { type, payload } = action;
-  switch (type) {
-    case RECOVERY_REQUEST:
-      return {
-        ...state,
-        submittedEmail: null,
-        recoveryInProgress: true,
-        recoveryError: null,
-      };
-    case RECOVERY_SUCCESS:
-      return {
-        ...state,
-        submittedEmail: payload.email,
-        initialEmail: payload.email,
-        recoveryInProgress: false,
-        passwordRequested: true,
-      };
-    case RECOVERY_ERROR:
-      return {
-        ...state,
-        recoveryInProgress: false,
-        recoveryError: payload.error,
-        initialEmail: payload.email,
-      };
-    case RETYPE_EMAIL:
-      return {
-        ...state,
-        initialEmail: state.submittedEmail,
-        submittedEmail: null,
-        passwordRequested: false,
-      };
-    case CLEAR_RECOVERY_ERROR:
-      return { ...state, recoveryError: null };
-    default:
-      return state;
+export const recoverPassword = createAsyncThunk(
+  'passwordRecoveryPage/recoverPassword',
+  ({ email }, { rejectWithValue, extra: sdk }) => {
+    return sdk.passwordReset
+      .request({ email })
+      .then(() => ({ email }))
+      .catch(e => rejectWithValue({ error: storableError(e), email }));
   }
-}
+);
 
-// ================ Action creators ================ //
+// ================ Slice ================ //
 
-export const passwordRecoveryRequest = () => ({ type: RECOVERY_REQUEST });
-export const passwordRecoverySuccess = email => ({ type: RECOVERY_SUCCESS, payload: { email } });
-export const passwordRecoveryError = (error, email) => ({
-  type: RECOVERY_ERROR,
-  payload: { error, email },
-  error: true,
+const passwordRecoveryPageSlice = createSlice({
+  name: 'passwordRecoveryPage',
+  initialState: {
+    initialEmail: null,
+    submittedEmail: null,
+    recoveryError: null,
+    recoveryInProgress: false,
+    passwordRequested: false,
+  },
+  reducers: {
+    retypePasswordRecoveryEmail: state => {
+      state.initialEmail = state.submittedEmail;
+      state.submittedEmail = null;
+      state.passwordRequested = false;
+    },
+    clearPasswordRecoveryError: state => {
+      state.recoveryError = null;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(recoverPassword.pending, state => {
+        state.submittedEmail = null;
+        state.recoveryInProgress = true;
+        state.recoveryError = null;
+      })
+      .addCase(recoverPassword.fulfilled, (state, action) => {
+        const { email } = action.payload;
+        state.submittedEmail = email;
+        state.initialEmail = email;
+        state.recoveryInProgress = false;
+        state.passwordRequested = true;
+      })
+      .addCase(recoverPassword.rejected, (state, action) => {
+        const { error, email } = action.payload;
+        state.recoveryInProgress = false;
+        state.recoveryError = error;
+        state.initialEmail = email;
+      });
+  },
 });
-export const retypePasswordRecoveryEmail = () => ({ type: RETYPE_EMAIL });
-export const clearPasswordRecoveryError = () => ({ type: CLEAR_RECOVERY_ERROR });
 
-// ================ Thunks ================ //
+export const {
+  retypePasswordRecoveryEmail,
+  clearPasswordRecoveryError,
+} = passwordRecoveryPageSlice.actions;
 
-export const recoverPassword = email => (dispatch, getState, sdk) => {
-  dispatch(passwordRecoveryRequest());
-
-  return sdk.passwordReset
-    .request({ email })
-    .then(() => dispatch(passwordRecoverySuccess(email)))
-    .catch(e => dispatch(passwordRecoveryError(storableError(e), email)));
-};
+export default passwordRecoveryPageSlice.reducer;
