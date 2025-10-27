2ca7b7a36938c0c9b904ed8d392370b88a237f7b
Vesa Luusua <vesa.luusua@gmail.com>
2025-10-27 17:16:20 +0200

    CheckoutPage.duck.js: update to use Redux Toolkit

--- src/containers/CheckoutPage/CheckoutPage.test.js
+++ src/containers/CheckoutPage/CheckoutPage.test.js
@@ -19,7 +19,7 @@ import {
 
 import CheckoutPageWithPayment from './CheckoutPageWithPayment';
 import CheckoutPageWithInquiryProcess from './CheckoutPageWithInquiryProcess';
-import checkoutPageReducer, { SET_INITIAL_VALUES, setInitialValues } from './CheckoutPage.duck';
+import checkoutPageReducer, { setInitialValues } from './CheckoutPage.duck';
 
 const { Money } = sdkTypes;
 const { screen } = testingLibrary;
@@ -381,7 +381,7 @@ describe('CheckoutPage', () => {
       );
       const orderData = { quantity: 3, deliveryMethod: 'shipping' };
       const expectedAction = {
-        type: SET_INITIAL_VALUES,
+        type: 'CheckoutPage/setInitialValues',
         payload: { listing, orderData },
       };
 
@@ -408,7 +408,7 @@ describe('CheckoutPage', () => {
         expect(checkoutPageReducer(undefined, {})).toEqual(initialValues);
       });
 
-      it('should handle SET_INITIAL_VALUES', () => {
+      it('should handle setInitialValues', () => {
         const listing = createListing(
           '00000000-0000-0000-0000-000000000000',
           {},
@@ -419,7 +419,9 @@ describe('CheckoutPage', () => {
         const orderData = { quantity: 3, deliveryMethod: 'shipping' };
         const payload = { listing, orderData };
         const expected = { ...initialValues, ...payload };
-        expect(checkoutPageReducer({}, { type: SET_INITIAL_VALUES, payload })).toEqual(expected);
+        expect(checkoutPageReducer({}, { type: 'CheckoutPage/setInitialValues', payload })).toEqual(
+          expected
+        );
       });
     });
   });
