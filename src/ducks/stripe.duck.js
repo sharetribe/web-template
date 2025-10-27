1cae588ce67f59851d956fe02324bebfbdae1db6
Vesa Luusua <vesa.luusua@gmail.com>
2025-10-27 13:39:46 +0200

    ducks/stripe.duck.js: update to use Redux Toolkit

--- src/ducks/stripe.duck.js
+++ src/ducks/stripe.duck.js
@@ -1,213 +1,17 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import * as log from '../util/log';
 import { storableError } from '../util/errors';
 
 // https://stripe.com/docs/api/payment_intents/object#payment_intent_object-status
 const STRIPE_PI_HAS_PASSED_CONFIRM = ['processing', 'requires_capture', 'canceled', 'succeeded'];
 
-// ================ Action types ================ //
+// ================ Async thunks ================ //
 
-export const STRIPE_ACCOUNT_CLEAR_ERROR = 'app/stripe/STRIPE_ACCOUNT_CLEAR_ERROR';
-
-export const ACCOUNT_OPENER_CREATE_REQUEST = 'app/stripe/ACCOUNT_OPENER_CREATE_REQUEST';
-export const ACCOUNT_OPENER_CREATE_SUCCESS = 'app/stripe/ACCOUNT_OPENER_CREATE_SUCCESS';
-export const ACCOUNT_OPENER_CREATE_ERROR = 'app/stripe/ACCOUNT_OPENER_CREATE_ERROR';
-
-export const PERSON_CREATE_REQUEST = 'app/stripe/PERSON_CREATE_REQUEST';
-export const PERSON_CREATE_SUCCESS = 'app/stripe/PERSON_CREATE_SUCCESS';
-export const PERSON_CREATE_ERROR = 'app/stripe/PERSON_CREATE_ERROR';
-
-export const CLEAR_PAYMENT_TOKEN = 'app/stripe/CLEAR_PAYMENT_TOKEN';
-
-export const HANDLE_CARD_PAYMENT_REQUEST = 'app/stripe/HANDLE_CARD_PAYMENT_REQUEST';
-export const HANDLE_CARD_PAYMENT_SUCCESS = 'app/stripe/HANDLE_CARD_PAYMENT_SUCCESS';
-export const HANDLE_CARD_PAYMENT_ERROR = 'app/stripe/HANDLE_CARD_PAYMENT_ERROR';
-
-export const HANDLE_CARD_SETUP_REQUEST = 'app/stripe/HANDLE_CARD_SETUP_REQUEST';
-export const HANDLE_CARD_SETUP_SUCCESS = 'app/stripe/HANDLE_CARD_SETUP_SUCCESS';
-export const HANDLE_CARD_SETUP_ERROR = 'app/stripe/HANDLE_CARD_SETUP_ERROR';
-
-export const CLEAR_HANDLE_CARD_PAYMENT = 'app/stripe/CLEAR_HANDLE_CARD_PAYMENT';
-
-export const RETRIEVE_PAYMENT_INTENT_REQUEST = 'app/stripe/RETRIEVE_PAYMENT_INTENT_REQUEST';
-export const RETRIEVE_PAYMENT_INTENT_SUCCESS = 'app/stripe/RETRIEVE_PAYMENT_INTENT_SUCCESS';
-export const RETRIEVE_PAYMENT_INTENT_ERROR = 'app/stripe/RETRIEVE_PAYMENT_INTENT_ERROR';
-
-// ================ Reducer ================ //
-
-const initialState = {
-  confirmCardPaymentInProgress: false,
-  confirmCardPaymentError: null,
-  handleCardSetupInProgress: false,
-  handleCardSetupError: null,
-  paymentIntent: null,
-  setupIntent: null,
-  retrievePaymentIntentInProgress: false,
-  retrievePaymentIntentError: null,
-};
-
-export default function reducer(state = initialState, action = {}) {
-  const { type, payload } = action;
-  switch (type) {
-    case STRIPE_ACCOUNT_CLEAR_ERROR:
-      return { ...initialState };
-
-    case ACCOUNT_OPENER_CREATE_REQUEST:
-      return {
-        ...state,
-        createAccountOpenerError: null,
-        createAccountOpenerInProgress: true,
-      };
-    case ACCOUNT_OPENER_CREATE_SUCCESS:
-      return { ...state, createAccountOpenerInProgress: false, personAccountOpener: payload };
-    case ACCOUNT_OPENER_CREATE_ERROR:
-      console.error(payload);
-      return { ...state, createAccountOpenerError: payload, createAccountOpenerInProgress: false };
-
-    case PERSON_CREATE_REQUEST:
-      return {
-        ...state,
-        persons: [
-          ...state.persons,
-          {
-            ...payload,
-            createStripePersonError: null,
-            createStripePersonInProgress: true,
-          },
-        ],
-      };
-    case PERSON_CREATE_SUCCESS:
-      return {
-        ...state,
-        persons: state.persons.map(p => {
-          return p.personToken === payload.personToken
-            ? { ...payload, createStripePersonInProgress: false }
-            : p;
-        }),
-      };
-    case PERSON_CREATE_ERROR:
-      console.error(payload);
-      return {
-        ...state,
-        persons: state.persons.map(p => {
-          return p.personToken === payload.personToken
-            ? { ...p, createStripePersonInProgress: false, createStripePersonError: payload.error }
-            : p;
-        }),
-      };
-
-    case HANDLE_CARD_PAYMENT_REQUEST:
-      return {
-        ...state,
-        confirmCardPaymentError: null,
-        confirmCardPaymentInProgress: true,
-      };
-    case HANDLE_CARD_PAYMENT_SUCCESS:
-      return { ...state, paymentIntent: payload, confirmCardPaymentInProgress: false };
-    case HANDLE_CARD_PAYMENT_ERROR:
-      console.error(payload);
-      return { ...state, confirmCardPaymentError: payload, confirmCardPaymentInProgress: false };
-
-    case HANDLE_CARD_SETUP_REQUEST:
-      return {
-        ...state,
-        handleCardSetupError: null,
-        handleCardSetupInProgress: true,
-      };
-    case HANDLE_CARD_SETUP_SUCCESS:
-      return { ...state, setupIntent: payload, handleCardSetupInProgress: false };
-    case HANDLE_CARD_SETUP_ERROR:
-      console.error(payload);
-      return { ...state, handleCardSetupError: payload, handleCardSetupInProgress: false };
-
-    case CLEAR_HANDLE_CARD_PAYMENT:
-      return {
-        ...state,
-        confirmCardPaymentInProgress: false,
-        confirmCardPaymentError: null,
-        paymentIntent: null,
-      };
-
-    case RETRIEVE_PAYMENT_INTENT_REQUEST:
-      return {
-        ...state,
-        retrievePaymentIntentError: null,
-        retrievePaymentIntentInProgress: true,
-      };
-    case RETRIEVE_PAYMENT_INTENT_SUCCESS:
-      return { ...state, paymentIntent: payload, retrievePaymentIntentInProgress: false };
-    case RETRIEVE_PAYMENT_INTENT_ERROR:
-      console.error(payload);
-      return {
-        ...state,
-        retrievePaymentIntentError: payload,
-        retrievePaymentIntentInProgress: false,
-      };
-
-    default:
-      return state;
-  }
-}
-
-// ================ Action creators ================ //
-
-export const stripeAccountClearError = () => ({
-  type: STRIPE_ACCOUNT_CLEAR_ERROR,
-});
-
-export const confirmCardPaymentRequest = () => ({
-  type: HANDLE_CARD_PAYMENT_REQUEST,
-});
-
-export const confirmCardPaymentSuccess = payload => ({
-  type: HANDLE_CARD_PAYMENT_SUCCESS,
-  payload,
-});
-
-export const confirmCardPaymentError = payload => ({
-  type: HANDLE_CARD_PAYMENT_ERROR,
-  payload,
-  error: true,
-});
-
-export const handleCardSetupRequest = () => ({
-  type: HANDLE_CARD_SETUP_REQUEST,
-});
-
-export const handleCardSetupSuccess = payload => ({
-  type: HANDLE_CARD_SETUP_SUCCESS,
-  payload,
-});
-
-export const handleCardSetupError = payload => ({
-  type: HANDLE_CARD_SETUP_ERROR,
-  payload,
-  error: true,
-});
-
-export const initializeCardPaymentData = () => ({
-  type: CLEAR_HANDLE_CARD_PAYMENT,
-});
-
-export const retrievePaymentIntentRequest = () => ({
-  type: RETRIEVE_PAYMENT_INTENT_REQUEST,
-});
-
-export const retrievePaymentIntentSuccess = payload => ({
-  type: RETRIEVE_PAYMENT_INTENT_SUCCESS,
-  payload,
-});
-
-export const retrievePaymentIntentError = payload => ({
-  type: RETRIEVE_PAYMENT_INTENT_ERROR,
-  payload,
-  error: true,
-});
-
-// ================ Thunks ================ //
-
-export const retrievePaymentIntent = params => dispatch => {
+/////////////////////////////
+// Retrieve Payment Intent //
+/////////////////////////////
+const retrievePaymentIntentPayloadCreator = (params, { rejectWithValue }) => {
   const { stripe, stripePaymentIntentClientSecret } = params;
-  dispatch(retrievePaymentIntentRequest());
 
   return stripe
     .retrievePaymentIntent(stripePaymentIntentClientSecret)
@@ -215,14 +19,12 @@ export const retrievePaymentIntent = params => dispatch => {
       if (response.error) {
         return Promise.reject(response);
       } else {
-        dispatch(retrievePaymentIntentSuccess(response.paymentIntent));
         return response;
       }
     })
     .catch(err => {
       // Unwrap Stripe error.
       const e = err.error || storableError(err);
-      dispatch(retrievePaymentIntentError(e));
 
       // Log error
       const { code, doc_url, message, payment_intent } = err.error || {};
@@ -239,17 +41,26 @@ export const retrievePaymentIntent = params => dispatch => {
       log.error(loggableError, 'stripe-retrieve-payment-intent-failed', {
         stripeMessage: loggableError.message,
       });
-      throw err;
+      return rejectWithValue(e);
     });
 };
+export const retrievePaymentIntentThunk = createAsyncThunk(
+  'stripe/retrievePaymentIntent',
+  retrievePaymentIntentPayloadCreator
+);
+// Backward compatible wrapper function
+export const retrievePaymentIntent = params => dispatch => {
+  return dispatch(retrievePaymentIntentThunk(params)).unwrap();
+};
 
-export const confirmCardPayment = params => dispatch => {
+//////////////////////////
+// Confirm Card Payment //
+//////////////////////////
+const confirmCardPaymentPayloadCreator = (params, { rejectWithValue }) => {
   // It's required to use the same instance of Stripe as where the card has been created
   // so that's why Stripe needs to be passed here and we can't create a new instance.
   const { stripe, paymentParams, stripePaymentIntentClientSecret } = params;
   const transactionId = params.orderId;
-
-  dispatch(confirmCardPaymentRequest());
 
   // When using default payment method paymentParams.payment_method is
   // already set Marketplace API side, when request-payment transition is made
@@ -263,7 +74,6 @@ export const confirmCardPayment = params => dispatch => {
       if (response.error) {
         return Promise.reject(response);
       } else {
-        dispatch(confirmCardPaymentSuccess(response));
         return { ...response, transactionId };
       }
     });
@@ -277,7 +87,6 @@ export const confirmCardPayment = params => dispatch => {
         return Promise.reject(response);
       } else if (STRIPE_PI_HAS_PASSED_CONFIRM.includes(response?.paymentIntent?.status)) {
         // Payment Intent has been confirmed already, move forward.
-        dispatch(confirmCardPaymentSuccess(response));
         return { ...response, transactionId };
       } else {
         // If payment intent has not been confirmed yet, confirm it.
@@ -287,7 +96,6 @@ export const confirmCardPayment = params => dispatch => {
     .catch(err => {
       // Unwrap Stripe error.
       const e = err.error || storableError(err);
-      dispatch(confirmCardPaymentError(e));
 
       // Log error
       const containsPaymentIntent = err.error && err.error.payment_intent;
@@ -303,16 +111,25 @@ export const confirmCardPayment = params => dispatch => {
       log.error(loggableError, 'stripe-handle-card-payment-failed', {
         stripeMessage: loggableError.message,
       });
-      throw e;
+      return rejectWithValue(e);
     });
 };
+export const confirmCardPaymentThunk = createAsyncThunk(
+  'stripe/confirmCardPayment',
+  confirmCardPaymentPayloadCreator
+);
+// Backward compatible wrapper function
+export const confirmCardPayment = params => dispatch => {
+  return dispatch(confirmCardPaymentThunk(params)).unwrap();
+};
 
-export const handleCardSetup = params => dispatch => {
+///////////////////////
+// Handle Card Setup //
+///////////////////////
+const handleCardSetupPayloadCreator = (params, { rejectWithValue }) => {
   // It's required to use the same instance of Stripe as where the card has been created
   // so that's why Stripe needs to be passed here and we can't create a new instance.
   const { stripe, card, setupIntentClientSecret, paymentParams } = params;
-
-  dispatch(handleCardSetupRequest());
 
   return stripe
     .handleCardSetup(setupIntentClientSecret, card, paymentParams)
@@ -320,14 +137,12 @@ export const handleCardSetup = params => dispatch => {
       if (response.error) {
         return Promise.reject(response);
       } else {
-        dispatch(handleCardSetupSuccess(response));
         return response;
       }
     })
     .catch(err => {
       // Unwrap Stripe error.
       const e = err.error || storableError(err);
-      dispatch(handleCardSetupError(e));
 
       // Log error
       const containsSetupIntent = err.error && err.error.setup_intent;
@@ -343,6 +158,99 @@ export const handleCardSetup = params => dispatch => {
       log.error(loggableError, 'stripe-handle-card-setup-failed', {
         stripeMessage: loggableError.message,
       });
-      throw e;
+      return rejectWithValue(e);
     });
 };
+export const handleCardSetupThunk = createAsyncThunk(
+  'stripe/handleCardSetup',
+  handleCardSetupPayloadCreator
+);
+// Backward compatible wrapper function
+export const handleCardSetup = params => dispatch => {
+  return dispatch(handleCardSetupThunk(params)).unwrap();
+};
+
+// ================ Slice ================ //
+
+const stripeSlice = createSlice({
+  name: 'stripe',
+  initialState: {
+    confirmCardPaymentInProgress: false,
+    confirmCardPaymentError: null,
+    handleCardSetupInProgress: false,
+    handleCardSetupError: null,
+    paymentIntent: null,
+    setupIntent: null,
+    retrievePaymentIntentInProgress: false,
+    retrievePaymentIntentError: null,
+  },
+  reducers: {
+    stripeAccountClearError: state => {
+      return {
+        confirmCardPaymentInProgress: false,
+        confirmCardPaymentError: null,
+        handleCardSetupInProgress: false,
+        handleCardSetupError: null,
+        paymentIntent: null,
+        setupIntent: null,
+        retrievePaymentIntentInProgress: false,
+        retrievePaymentIntentError: null,
+      };
+    },
+    initializeCardPaymentData: state => {
+      state.confirmCardPaymentInProgress = false;
+      state.confirmCardPaymentError = null;
+      state.paymentIntent = null;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      // Retrieve Payment Intent cases
+      .addCase(retrievePaymentIntentThunk.pending, state => {
+        state.retrievePaymentIntentError = null;
+        state.retrievePaymentIntentInProgress = true;
+      })
+      .addCase(retrievePaymentIntentThunk.fulfilled, (state, action) => {
+        state.paymentIntent = action.payload.paymentIntent;
+        state.retrievePaymentIntentInProgress = false;
+      })
+      .addCase(retrievePaymentIntentThunk.rejected, (state, action) => {
+        console.error(action.payload);
+        state.retrievePaymentIntentError = action.payload;
+        state.retrievePaymentIntentInProgress = false;
+      })
+      // Confirm Card Payment cases
+      .addCase(confirmCardPaymentThunk.pending, state => {
+        state.confirmCardPaymentError = null;
+        state.confirmCardPaymentInProgress = true;
+      })
+      .addCase(confirmCardPaymentThunk.fulfilled, (state, action) => {
+        state.paymentIntent = action.payload;
+        state.confirmCardPaymentInProgress = false;
+      })
+      .addCase(confirmCardPaymentThunk.rejected, (state, action) => {
+        console.error(action.payload);
+        state.confirmCardPaymentError = action.payload;
+        state.confirmCardPaymentInProgress = false;
+      })
+      // Handle Card Setup cases
+      .addCase(handleCardSetupThunk.pending, state => {
+        state.handleCardSetupError = null;
+        state.handleCardSetupInProgress = true;
+      })
+      .addCase(handleCardSetupThunk.fulfilled, (state, action) => {
+        state.setupIntent = action.payload;
+        state.handleCardSetupInProgress = false;
+      })
+      .addCase(handleCardSetupThunk.rejected, (state, action) => {
+        console.error(action.payload);
+        state.handleCardSetupError = action.payload;
+        state.handleCardSetupInProgress = false;
+      });
+  },
+});
+
+// Export the action creators
+export const { stripeAccountClearError, initializeCardPaymentData } = stripeSlice.actions;
+
+export default stripeSlice.reducer;
