310c8957a8af421a393aba36617665d1abfd55d3
Vesa Luusua <vesa.luusua@gmail.com>
2025-12-08 15:29:56 +0200

    FilterPlain: add keyboard listeners for Enter, ArrowDown, ArrowUp, Home, End, Escape Pass the Clear button inside the form and keep the focus on in over async data load.

--- src/containers/SearchPage/FilterPlain/FilterPlain.js
+++ src/containers/SearchPage/FilterPlain/FilterPlain.js
@@ -5,8 +5,51 @@ import { FormattedMessage, injectIntl, intlShape } from '../../../util/reactIntl
 
 import IconPlus from '../IconPlus/IconPlus';
 import FilterForm from '../FilterForm/FilterForm';
+import KeyboardListener from '../KeyboardListener/KeyboardListener';
 
 import css from './FilterPlain.module.css';
+
+const FOCUSABLE_ELEMENTS = 'button, [tabindex], input, select, textarea';
+
+const moveFocusToFirstFocusableElement = formId => {
+  const form = document.getElementById(formId);
+  const focusableElements = form.querySelectorAll(FOCUSABLE_ELEMENTS);
+  if (focusableElements.length > 0) {
+    const firstFocusableElement = Array.from(focusableElements).find(
+      element => !(element.tabIndex < 0 || element.disabled)
+    );
+    firstFocusableElement?.focus();
+  }
+};
+const moveFocusToLastFocusableElement = formId => {
+  const form = document.getElementById(formId);
+  const focusableElements = form.querySelectorAll(FOCUSABLE_ELEMENTS);
+  if (focusableElements.length > 0) {
+    const lastFocusableElement = Array.from(focusableElements)
+      .reverse()
+      .find(element => !(element.tabIndex < 0 || element.disabled));
+    lastFocusableElement?.focus();
+  }
+};
+
+const moveFocusToNextFocusableElement = (formId, direction = 'next') => {
+  const form = document.getElementById(formId);
+  const focusableElements = form.querySelectorAll(FOCUSABLE_ELEMENTS);
+  if (focusableElements.length > 0) {
+    const currentIndex = Array.from(focusableElements).findIndex(
+      element => element === document.activeElement
+    );
+    const targetIndex =
+      direction === 'next' && currentIndex < focusableElements.length - 1
+        ? currentIndex + 1
+        : direction === 'next'
+        ? 0
+        : direction === 'previous' && currentIndex > 0
+        ? currentIndex - 1
+        : focusableElements.length - 1;
+    focusableElements[targetIndex]?.focus();
+  }
+};
 
 /**
  * FilterPlain component
@@ -42,10 +85,14 @@ class FilterPlainComponent extends Component {
   handleChange(values) {
     const { onSubmit } = this.props;
     onSubmit(values);
+    window.__focusedElementId__ = null;
   }
 
   handleClear() {
-    const { onSubmit, onClear } = this.props;
+    const { id, onSubmit, onClear } = this.props;
+
+    // Due to async API call, focused element is not guaranteed to be the clear button.
+    window.__focusedElementId__ = `${id}.form.clear`;
 
     if (onClear) {
       onClear();
@@ -54,8 +101,16 @@ class FilterPlainComponent extends Component {
     onSubmit(null);
   }
 
-  toggleIsOpen() {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  toggleIsOpen(options = {}) {
+    const { callback, enforcedState } = options || {};
+    const cb = callback || (() => {});
+    if (enforcedState !== undefined) {
+      this.setState({ isOpen: enforcedState }, cb);
+    } else {
+      this.setState(prevState => {
+        return { isOpen: !prevState.isOpen };
+      }, cb);
+    }
   }
 
   render() {
@@ -72,16 +127,122 @@ class FilterPlainComponent extends Component {
       initialValues,
       keepDirtyOnReinitialize = false,
       ariaLabel,
+      containerId, //TODO
     } = this.props;
     const formId = `${id}.form`;
     const classes = classNames(rootClassName || css.root, className);
+    const inertMaybe = this.state.isOpen ? {} : { inert: '' };
 
     return (
-      <div className={classes}>
+      <KeyboardListener
+        className={classes}
+        containerId={containerId}
+        keyMap={{
+          Enter: {
+            action: 'toggle',
+            callback: (...args) => {
+              const [event, containerId, action] = args;
+              if (event.target.id === `${id}.toggle`) {
+                const isOpen = this.state.isOpen;
+                event.preventDefault();
+                event.stopPropagation();
+                this.toggleIsOpen({
+                  callback: () => {
+                    if (!isOpen) {
+                      moveFocusToFirstFocusableElement(formId);
+                    }
+                  },
+                });
+              }
+            },
+          },
+          ArrowDown: {
+            action: 'down',
+            callback: (...args) => {
+              const [event, containerId, action] = args;
+              if (event.target.id === `${id}.toggle`) {
+                event.preventDefault();
+                event.stopPropagation();
+                if (!this.state.isOpen) {
+                  this.toggleIsOpen({
+                    callback: () => moveFocusToFirstFocusableElement(formId),
+                    enforcedState: true,
+                  });
+                }
+              } else if (this.filterContent.contains(event.target)) {
+                event.preventDefault();
+                event.stopPropagation();
+                moveFocusToNextFocusableElement(formId, 'next');
+              }
+            },
+          },
+          ArrowUp: {
+            action: 'close',
+            callback: (...args) => {
+              const [event, containerId, action] = args;
+              if (event.target.id === `${id}.toggle`) {
+                event.preventDefault();
+                event.stopPropagation();
+                if (this.state.isOpen) {
+                  this.toggleIsOpen({ enforcedState: false });
+                }
+              } else if (this.filterContent.contains(event.target)) {
+                event.preventDefault();
+                event.stopPropagation();
+                moveFocusToNextFocusableElement(formId, 'previous');
+              }
+            },
+          },
+          Home: {
+            action: 'first',
+            callback: (...args) => {
+              const [event, containerId, action] = args;
+              if (!this.state.isOpen && event.target.id === `${id}.toggle`) {
+                event.preventDefault();
+                event.stopPropagation();
+                this.toggleIsOpen({
+                  callback: () => moveFocusToFirstFocusableElement(formId),
+                  enforcedState: true,
+                });
+              }
+            },
+          },
+          End: {
+            action: 'last',
+            callback: (...args) => {
+              const [event, containerId, action] = args;
+              if (!this.state.isOpen && event.target.id === `${id}.toggle`) {
+                event.preventDefault();
+                event.stopPropagation();
+                this.toggleIsOpen({
+                  callback: () => moveFocusToLastFocusableElement(formId),
+                  enforcedState: true,
+                });
+              }
+            },
+          },
+          Escape: {
+            action: 'close',
+            callback: (...args) => {
+              const [event, containerId, action] = args;
+              if (this.state.isOpen) {
+                event.preventDefault();
+                event.stopPropagation();
+                const button = document.getElementById(`${id}.toggle`);
+                if (button) {
+                  button.focus();
+                }
+                this.toggleIsOpen({ enforcedState: false });
+              }
+            },
+          },
+        }}
+      >
         <div className={css.filterHeader}>
           <button
+            id={`${id}.toggle`}
             className={css.labelButton}
-            onClick={this.toggleIsOpen}
+            onClick={() => this.toggleIsOpen()}
             aria-label={ariaLabel}
             aria-expanded={this.state.isOpen}
             aria-controls={this.state.isOpen ? formId : ''}
@@ -108,6 +269,7 @@ class FilterPlainComponent extends Component {
           ref={node => {
             this.filterContent = node;
           }}
+          {...inertMaybe}
         >
           <FilterForm
             id={formId}
@@ -115,14 +277,21 @@ class FilterPlainComponent extends Component {
             onChange={this.handleChange}
             initialValues={initialValues}
             keepDirtyOnReinitialize={keepDirtyOnReinitialize}
-            clearButton={(<button type="button" className={css.clearButton} onClick={this.handleClear}>
-              <FormattedMessage id={'FilterPlain.clear'} />
-            </button>)}
+            clearButton={
+              <button
+                id={`${formId}.clear`}
+                type="button"
+                className={css.clearButton}
+                onClick={this.handleClear}
+              >
+                <FormattedMessage id={'FilterPlain.clear'} />
+              </button>
+            }
           >
             {children}
           </FilterForm>
         </div>
-      </div>
+      </KeyboardListener>
     );
   }
 }
