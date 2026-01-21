7ed605647ae51feb0f422382c3c50e0577049445
nouakil63 <normanouakil63@gmail.com>
2026-01-21 16:23:12 +0100

    Update SectionAuthorMaybe.js

--- src/containers/ListingPage/SectionAuthorMaybe.js
+++ src/containers/ListingPage/SectionAuthorMaybe.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { FormattedMessage } from '../../util/reactIntl';
 import { INQUIRY_PROCESS_NAME, resolveLatestProcessName } from '../../transactions/transaction';
-
 import { Heading, Modal } from '../../components';
 import UserCard from './UserCard/UserCard';
 import InquiryForm from './InquiryForm/InquiryForm';
-
 import css from './ListingPage.module.css';
 
 const CONTACT_USER_LINK = 'inquiryModalContactUserLink';
@@ -29,6 +27,12 @@ const SectionAuthorMaybe = props => {
     return null;
   }
 
+  // Récupérer les données publiques de l'auteur
+  const author = listing.author;
+  const { publicData } = author.attributes.profile || {};
+  const phoneNumber = publicData?.publicPhone || null;
+  const email = publicData?.publicEmail || null;
+
   const transactionProcessAlias = listing?.attributes?.publicData?.transactionProcessAlias || '';
   const processName = resolveLatestProcessName(transactionProcessAlias.split('/')[0]);
   const isInquiryProcess = processName === INQUIRY_PROCESS_NAME;
@@ -45,6 +49,25 @@ const SectionAuthorMaybe = props => {
         showContact={!isInquiryProcess}
         contactLinkId={CONTACT_USER_LINK}
       />
+
+      {/* Coordonnées de l'annonceur */}
+      {(email || phoneNumber) && (
+        <div className={css.authorContact}>
+          {email && (
+            <div className={css.contactItem}>
+              <span className={css.contactLabel}>Email : </span>
+              <a href={`mailto:${email}`} className={css.contactLink}>{email}</a>
+            </div>
+          )}
+          {phoneNumber && (
+            <div className={css.contactItem}>
+              <span className={css.contactLabel}>Téléphone : </span>
+              <a href={`tel:${phoneNumber}`} className={css.contactLink}>{phoneNumber}</a>
+            </div>
+          )}
+        </div>
+      )}
+
       <Modal
         id="ListingPage.inquiry"
         contentClassName={css.inquiryModalContent}
