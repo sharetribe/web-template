0572c3f9ee266c612f3cbfcfbea8bef44bf84818
Sari Saariaho <sari@sharetribe.com>
2026-03-27 16:15:02 +0200

    Implement upload and verification progress status tracking

--- src/util/fileHelpers.js
+++ src/util/fileHelpers.js
@@ -1,5 +1,7 @@
 const KILO = 1024;
 
+export const MAX_FILE_SIZE = KILO * KILO * KILO; // real max size
+
 export const calculateFileSize = size => {
   const kbSize = Math.ceil(size / KILO);
   const mbSize = (kbSize / KILO).toFixed(1);
