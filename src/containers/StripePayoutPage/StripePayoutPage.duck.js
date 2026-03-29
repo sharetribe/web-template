077528898b9ec1c293c5113a31e69299fdd47837
Edgar Bongkishiy <biz@bongkishiy.de>
2026-03-29 01:02:56 +0100

    Made the payout page to be manual with Mpesa and bank account, amongst other changes

--- src/containers/StripePayoutPage/StripePayoutPage.duck.js
+++ src/containers/StripePayoutPage/StripePayoutPage.duck.js
@@ -1,59 +1,51 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-
 import { pick } from '../../util/common';
-import {
-  createStripeAccount,
-  updateStripeAccount,
-  fetchStripeAccount,
-} from '../../ducks/stripeConnectAccount.duck';
 import { fetchCurrentUser } from '../../ducks/user.duck';
 
 // ================ Async thunks ================ //
 
 const savePayoutDetailsPayloadCreator = (
-  { values, isUpdateCall },
+  { values },
   { dispatch, extra: sdk, rejectWithValue }
 ) => {
-  const upsertThunk = isUpdateCall ? updateStripeAccount : createStripeAccount;
-
-  return dispatch(upsertThunk(values, { expand: true }))
-    .then(response => {
-      return response;
-    })
-    .catch(() => {
-      return rejectWithValue('Failed to save payout details');
-    });
+  return sdk.currentUser
+    .updateProfile(
+      { privateData: { manualPayoutDetails: values } },
+      { expand: true }
+    )
+    .then(() => dispatch(fetchCurrentUser()))
+    .catch(() => rejectWithValue('Failed to save payout details'));
 };
+
 export const savePayoutDetailsThunk = createAsyncThunk(
   'StripePayoutPage/savePayoutDetails',
   savePayoutDetailsPayloadCreator
 );
-// Backward compatible wrapper function
-export const savePayoutDetails = (values, isUpdateCall) => dispatch => {
-  return dispatch(savePayoutDetailsThunk({ values, isUpdateCall })).unwrap();
-};
+
+export const savePayoutDetails = values => dispatch =>
+  dispatch(savePayoutDetailsThunk({ values })).unwrap();
 
 // ================ Slice ================ //
 
 const initialState = {
   payoutDetailsSaveInProgress: false,
   payoutDetailsSaved: false,
-  fromReturnURL: false,
 };
 
 const stripePayoutPageSlice = createSlice({
   name: 'StripePayoutPage',
   initialState,
   reducers: {
-    setInitialValues: (state, action) => {
-      return { ...initialState, ...pick(action.payload, Object.keys(initialState)) };
-    },
+    setInitialValues: (state, action) => ({
+      ...initialState,
+      ...pick(action.payload, Object.keys(initialState)),
+    }),
   },
   extraReducers: builder => {
     builder
-      // Save Payout Details cases
       .addCase(savePayoutDetailsThunk.pending, state => {
         state.payoutDetailsSaveInProgress = true;
+        state.payoutDetailsSaved = false;
       })
       .addCase(savePayoutDetailsThunk.fulfilled, state => {
         state.payoutDetailsSaveInProgress = false;
@@ -65,27 +57,12 @@ const stripePayoutPageSlice = createSlice({
   },
 });
 
-// Export the action creators
 export const { setInitialValues } = stripePayoutPageSlice.actions;
-
 export default stripePayoutPageSlice.reducer;
 
 // ================ Load Data ================ //
 
-export const loadData = () => (dispatch, getState, sdk) => {
-  // Clear state so that previously loaded data is not visible
-  // in case this page load fails.
+export const loadData = () => (dispatch) => {
   dispatch(setInitialValues());
-  const fetchCurrentUserOptions = {
-    updateHasListings: false,
-    updateNotifications: false,
-  };
-
-  return dispatch(fetchCurrentUser(fetchCurrentUserOptions)).then(response => {
-    const currentUser = getState().user.currentUser;
-    if (currentUser && currentUser.stripeAccount) {
-      dispatch(fetchStripeAccount());
-    }
-    return response;
-  });
+  return dispatch(fetchCurrentUser({ updateHasListings: false, updateNotifications: false }));
 };
