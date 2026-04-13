7a9181cd106ce457a97edbb711e4c9a7537edc34
Sari Saariaho <sari@sharetribe.com>
2026-04-13 16:05:30 +0300

    Update message action styling

--- src/containers/TransactionPage/SendMessageForm/SendMessageForm.js
+++ src/containers/TransactionPage/SendMessageForm/SendMessageForm.js
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 import { FormattedMessage, injectIntl, intlShape } from '../../../util/reactIntl';
 import { propTypes } from '../../../util/types';
 
-import { Form, FieldTextInput, FileUpload, SecondaryButtonInline } from '../../../components';
+import { Form, FieldTextInput, FileUpload, Button } from '../../../components';
 
 import css from './SendMessageForm.module.css';
 
@@ -39,21 +39,20 @@ const FieldAddFile = props => {
   );
 };
 
-const IconSendMessage = () => {
+const IconAttachFile = () => {
   return (
     <svg
-      className={css.sendIcon}
-      width="14"
-      height="14"
-      viewBox="0 0 14 14"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       role="none"
     >
-      <g className={css.strokeMatter} fill="none" fillRule="evenodd" strokeLinejoin="round">
-        <path d="M12.91 1L0 7.003l5.052 2.212z" />
-        <path d="M10.75 11.686L5.042 9.222l7.928-8.198z" />
-        <path d="M5.417 8.583v4.695l2.273-2.852" />
-      </g>
+      <path
+        d="M12.309 6.03636C12.6497 6.37765 12.6497 6.93102 12.309 7.27233L7.99042 11.598C7.64969 11.9392 7.64969 12.4926 7.99042 12.8339C8.33114 13.1752 8.88358 13.1752 9.22428 12.8339L14.1598 7.89025C15.1819 6.86638 15.1819 5.20637 14.1598 4.1825C13.1376 3.15863 11.4803 3.15863 10.4581 4.1825L5.52267 9.12617C3.81905 10.8326 3.81905 13.5993 5.52267 15.3058C7.22629 17.0122 9.9884 17.0122 11.692 15.3058L16.0106 10.98C16.3513 10.6387 16.9037 10.6387 17.2445 10.98C17.5852 11.3213 17.5852 11.8746 17.2445 12.2159L12.9259 16.5417C10.5408 18.9307 6.67387 18.9307 4.2888 16.5417C1.90373 14.1526 1.90373 10.2793 4.2888 7.89025L9.22428 2.94658C10.9279 1.24014 13.6901 1.24014 15.3937 2.94658C17.0973 4.65303 17.0973 7.41972 15.3937 9.12617L10.4581 14.0699C9.43603 15.0937 7.77872 15.0937 6.75655 14.0699C5.73437 13.046 5.73437 11.386 6.75655 10.3621L11.0751 6.03638C11.4158 5.69509 11.9682 5.69508 12.309 6.03636Z"
+        fill="currentColor"
+      />
     </svg>
   );
 };
@@ -133,6 +132,12 @@ class SendMessageFormComponent extends Component {
           const submitDisabled =
             invalid || submitInProgress || !hasMessage || isAnyFileInProgress || hasAnyFileErrors;
           const showFileLink = !files || files?.length < 10; // TODO add access control logic
+          const addFileLabel = (
+            <>
+              <IconAttachFile />
+              <FormattedMessage id="TransactionPage.attachFile" />
+            </>
+          );
 
           return (
             <Form className={classes} onSubmit={values => handleSubmit(values, form)}>
@@ -165,23 +170,22 @@ class SendMessageFormComponent extends Component {
                   <FieldAddFile
                     id="addFile"
                     name="addFile"
-                    label={<FormattedMessage id="TransactionPage.attachFile" />}
+                    label={addFileLabel}
                     type="file"
                     onFileUpload={onFileUpload}
                     formApi={form}
                     className={css.fileLink}
                     showFileLink={showFileLink}
                   />
-                  <SecondaryButtonInline
+                  <Button
                     className={css.submitButton}
                     inProgress={submitInProgress}
                     disabled={submitDisabled}
                     onFocus={this.handleFocus}
                     onBlur={this.handleBlur}
                   >
-                    <IconSendMessage />
                     <FormattedMessage id="SendMessageForm.sendMessage" />
-                  </SecondaryButtonInline>
+                  </Button>
                 </div>
               </div>
             </Form>
