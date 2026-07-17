e5bf201cde90d1f71de4253219b2d0008c4f6af7
Vesa Luusua <vesa.luusua@gmail.com>
2026-07-17 13:27:41 +0300

    SavedCardDetails: allow more payment methods to be passed for the component (We'll rename this component on later commit)

--- src/components/SavedCardDetails/SavedCardDetails.js
+++ src/components/SavedCardDetails/SavedCardDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { useIntl } from '../../util/reactIntl';
 import {
@@ -18,6 +18,7 @@ import css from './SavedCardDetails.module.css';
 
 const DEFAULT_CARD = 'defaultCard';
 const REPLACE_CARD = 'replaceCard';
+const ONETIME_CARD_PAYMENT = 'onetimeCardPayment';
 const DELETE_CARD_BUTTON_ID = 'deleteCardButton';
 
 /**
@@ -27,7 +28,7 @@ const DELETE_CARD_BUTTON_ID = 'deleteCardButton';
  * @param {Object} props
  * @param {string} [props.rootClassName] - Custom class that overrides the default class for the root element
  * @param {string} [props.className] - Custom class that extends the default class for the root element
- * @param {Object} props.card - The card to render
+ * @param {Object} [props.card] - The card to render (omit for options-only mode)
  * @param {string} props.card.brand - The brand of the card
  * @param {number} props.card.expirationMonth - The expiration month of the card
  * @param {number} props.card.expirationYear - The expiration year of the card
@@ -36,12 +37,13 @@ const DELETE_CARD_BUTTON_ID = 'deleteCardButton';
  * @param {function} [props.onDeleteCard] - The function to call when the card is deleted
  * @param {function} [props.onManageDisableScrolling] - The function to call when the modal is opened
  * @param {boolean} [props.deletePaymentMethodInProgress] - Whether the delete payment method is in progress
+ * @param {Array<{ value: string, label: string }>} [props.additionalPaymentOptions] - Extra menu options
+ * @param {string} [props.selectedValue] - Controlled selected menu value
  * @returns {JSX.Element}
  */
 const SavedCardDetails = props => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-  const [active, setActive] = useState(DEFAULT_CARD);
   const [hasBeenOpened, setHasBeenOpened] = useState(false);
   const intl = useIntl();
 
@@ -53,10 +55,24 @@ const SavedCardDetails = props => {
     onDeleteCard,
     onManageDisableScrolling,
     deletePaymentMethodInProgress,
+    additionalPaymentOptions = [],
+    selectedValue,
   } = props;
 
   const { last4Digits, expirationMonth, expirationYear, brand } = card || {};
+  const hasSavedCard = !!(card && last4Digits);
+  const newCardValue = hasSavedCard ? REPLACE_CARD : ONETIME_CARD_PAYMENT;
   const classes = classNames(rootClassName || css.root, className);
+
+  const [active, setActive] = useState(
+    selectedValue || (hasSavedCard ? DEFAULT_CARD : newCardValue)
+  );
+
+  useEffect(() => {
+    if (selectedValue != null) {
+      setActive(selectedValue);
+    }
+  }, [selectedValue]);
 
   const paymentMethodPlaceholderDesktop = intl.formatMessage(
     { id: 'SavedCardDetails.savedPaymentMethodPlaceholderDesktop' },
@@ -107,7 +123,7 @@ const SavedCardDetails = props => {
   const isCardExpired =
     expirationMonth && expirationYear && isExpired(expirationMonth, expirationYear);
 
-  const defaultCard = (
+  const defaultCard = hasSavedCard ? (
     <div className={css.savedPaymentMethod}>
       <IconCard brand={brand} className={css.cardIcon} />
       {paymentMethodPlaceholder}
@@ -115,7 +131,7 @@ const SavedCardDetails = props => {
         {expirationMonth}/{expirationYear.toString().substring(2)}
       </span>
     </div>
-  );
+  ) : null;
 
   const handleClick = item => e => {
     // Clicking buttons inside a form will call submit
@@ -152,14 +168,27 @@ const SavedCardDetails = props => {
   const removeCard = intl.formatMessage({ id: 'SavedCardDetails.removeCard' });
   const deletePaymentMethod = intl.formatMessage({ id: 'SavedCardDetails.deletePaymentMethod' });
 
-  const showExpired = isCardExpired && active === DEFAULT_CARD;
+  const selectedAdditionalOption = additionalPaymentOptions.find(option => option.value === active);
+  const isNewCardSelected = active === REPLACE_CARD || active === ONETIME_CARD_PAYMENT;
+  const menuLabelContent =
+    active === DEFAULT_CARD && defaultCard ? (
+      defaultCard
+    ) : isNewCardSelected ? (
+      replaceCard
+    ) : selectedAdditionalOption ? (
+      <span className={css.additionalPaymentOptionLabel}>{selectedAdditionalOption.label}</span>
+    ) : (
+      replaceCard
+    );
+
+  const showExpired = isCardExpired && active === DEFAULT_CARD && hasSavedCard;
 
   return (
     <div className={classes}>
       <Menu className={css.menu} isOpen={menuOpen} onToggleActive={onToggleActive} useArrow={false}>
         <MenuLabel className={css.menuLabel}>
           <div className={showExpired ? css.menuLabelWrapperExpired : css.menuLabelWrapper}>
-            {active === DEFAULT_CARD ? defaultCard : replaceCard}
+            {menuLabelContent}
           </div>
         </MenuLabel>
 
@@ -168,35 +197,56 @@ const SavedCardDetails = props => {
             [css.menuContentTransition]: hasBeenOpened,
           })}
         >
-          <MenuItem key="first item" className={css.menuItem}>
+          {hasSavedCard ? (
+            <MenuItem key="default-card" className={css.menuItem}>
+              <IconCheckmark
+                className={active === DEFAULT_CARD ? css.iconCheckmark : css.iconCheckmarkHidden}
+                size="small"
+              />
+              <InlineTextButton className={css.menuText} onClick={handleClick(DEFAULT_CARD)}>
+                {defaultCard}
+              </InlineTextButton>
+            </MenuItem>
+          ) : null}
+          {hasSavedCard ? (
+            <MenuItem key="divider" className={css.menuDivider}>
+              {replaceCardTitle}
+            </MenuItem>
+          ) : null}
+          <MenuItem key="new-card" className={css.menuItem}>
             <IconCheckmark
-              className={active === DEFAULT_CARD ? css.iconCheckmark : css.iconCheckmarkHidden}
-              size="small"
-            />
-            <InlineTextButton className={css.menuText} onClick={handleClick(DEFAULT_CARD)}>
-              {defaultCard}
-            </InlineTextButton>
-          </MenuItem>
-          <MenuItem key="divider" className={css.menuDivider}>
-            {replaceCardTitle}
-          </MenuItem>
-          <MenuItem key="second item" className={css.menuItem}>
-            <IconCheckmark
-              className={active === REPLACE_CARD ? css.iconCheckmark : css.iconCheckmarkHidden}
+              className={isNewCardSelected ? css.iconCheckmark : css.iconCheckmarkHidden}
               size="small"
             />
             <InlineTextButton
               className={css.menuTextReplaceCard}
-              onClick={handleClick(REPLACE_CARD)}
+              onClick={handleClick(newCardValue)}
             >
               {replaceCard}
             </InlineTextButton>
           </MenuItem>
+          {additionalPaymentOptions.map(option => {
+            const isSelected = active === option.value;
+            return (
+              <MenuItem key={option.value} className={css.menuItem}>
+                <IconCheckmark
+                  className={isSelected ? css.iconCheckmark : css.iconCheckmarkHidden}
+                  size="small"
+                />
+                <InlineTextButton
+                  className={css.menuTextReplaceCard}
+                  onClick={handleClick(option.value)}
+                >
+                  <span className={css.additionalPaymentOptionLabel}>{option.label}</span>
+                </InlineTextButton>
+              </MenuItem>
+            );
+          })}
         </MenuContent>
       </Menu>
       {showExpired && !menuOpen ? expiredText : null}
 
-      {onDeleteCard && active !== REPLACE_CARD ? (
+      {onDeleteCard && active === DEFAULT_CARD ? (
         <InlineTextButton
           id={DELETE_CARD_BUTTON_ID}
           onClick={handleDeleteCard}
