64001aee52e1065b984b1404e6e6c0d76ae9a596
Sari Saariaho <sari@sharetribe.com>
2026-08-12 14:20:32 +0300

    Add a custom select component

--- src/components/index.js
+++ src/components/index.js
@@ -102,6 +102,7 @@ export { default as FieldNumber } from './FieldNumber/FieldNumber';
 export { default as FieldRadioButton } from './FieldRadioButton/FieldRadioButton';
 export { default as FieldReviewRating } from './FieldReviewRating/FieldReviewRating';
 export { default as FieldSelect } from './FieldSelect/FieldSelect';
+export { default as FieldSelectPopup } from './FieldSelectPopup/FieldSelectPopup';
 export { default as FieldSelectTree } from './FieldSelectTree/FieldSelectTree';
 export { default as FieldTextInput } from './FieldTextInput/FieldTextInput';
 
